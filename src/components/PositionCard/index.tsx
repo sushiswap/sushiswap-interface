03d71b66be5e77f758fd133f2847d4b002620b1b
Matthew Lilley <hello@matthewlilley.com>
2021-02-08 08:24:20 +0000

    Update fee %

--- src/components/PositionCard/index.tsx
+++ src/components/PositionCard/index.tsx
@@ -150,8 +150,8 @@ export function MinimalPositionCard({ pair, showUnwrapped = false, border }: Pos
             <span role="img" aria-label="wizard-icon">
               ⭐️
             </span>{' '}
-            By adding liquidity you&apos;ll earn 0.3% of all trades on this pair proportional to your share of the pool.
-            Fees are added to the pool, accrue in real time and can be claimed by withdrawing your liquidity.
+            By adding liquidity you&apos;ll earn 0.25% of all trades on this pair proportional to your share of the
+            pool. Fees are added to the pool, accrue in real time and can be claimed by withdrawing your liquidity.
           </TYPE.subHeader>
         </LightCard>
       )}
