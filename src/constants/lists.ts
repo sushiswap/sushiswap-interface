a457cdbdf99313fc1913d282940eb34adca7fc26
omakasebar <omakasebar@gmail.com>
2021-02-14 13:32:16 -0600

    reduce tokenlists to reduce checksum errors in dev

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -5,36 +5,36 @@
 export const UNSUPPORTED_LIST_URLS: string[] = []
 
 const SUSHI_LIST = 'https://raw.githubusercontent.com/sushiswapclassic/token-list/master/sushiswap.tokenlist.json'
-const COMPOUND_LIST = 'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json'
-const UMA_LIST = 'https://umaproject.org/uma.tokenlist.json'
-const AAVE_LIST = 'tokenlist.aave.eth'
-const SYNTHETIX_LIST = 'synths.snx.eth'
-const WRAPPED_LIST = 'wrapped.tokensoft.eth'
-const SET_LIST = 'https://raw.githubusercontent.com/SetProtocol/uniswap-tokenlist/main/set.tokenlist.json'
-const OPYN_LIST = 'https://raw.githubusercontent.com/opynfinance/opyn-tokenlist/master/opyn-v1.tokenlist.json'
-const ROLL_LIST = 'https://app.tryroll.com/tokens.json'
+//const COMPOUND_LIST = 'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json'
+//const UMA_LIST = 'https://umaproject.org/uma.tokenlist.json'
+//const AAVE_LIST = 'tokenlist.aave.eth'
+//const SYNTHETIX_LIST = 'synths.snx.eth'
+//const WRAPPED_LIST = 'wrapped.tokensoft.eth'
+//const SET_LIST = 'https://raw.githubusercontent.com/SetProtocol/uniswap-tokenlist/main/set.tokenlist.json'
+//const OPYN_LIST = 'https://raw.githubusercontent.com/opynfinance/opyn-tokenlist/master/opyn-v1.tokenlist.json'
+//const ROLL_LIST = 'https://app.tryroll.com/tokens.json'
 const COINGECKO_LIST = 'https://tokens.coingecko.com/uniswap/all.json'
-const CMC_ALL_LIST = 'defi.cmc.eth'
-const CMC_STABLECOIN = 'stablecoin.cmc.eth'
-const KLEROS_LIST = 't2crtokens.eth'
-const GEMINI_LIST = 'https://www.gemini.com/uniswap/manifest.json'
+//const CMC_ALL_LIST = 'defi.cmc.eth'
+//const CMC_STABLECOIN = 'stablecoin.cmc.eth'
+//const KLEROS_LIST = 't2crtokens.eth'
+//const GEMINI_LIST = 'https://www.gemini.com/uniswap/manifest.json'
 
 // lower index == higher priority for token import
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   SUSHI_LIST,
-  COMPOUND_LIST,
-  AAVE_LIST,
-  SYNTHETIX_LIST,
-  UMA_LIST,
-  WRAPPED_LIST,
-  SET_LIST,
-  OPYN_LIST,
-  ROLL_LIST,
+  // COMPOUND_LIST,
+  // AAVE_LIST,
+  // SYNTHETIX_LIST,
+  // UMA_LIST,
+  // WRAPPED_LIST,
+  // SET_LIST,
+  // OPYN_LIST,
+  // ROLL_LIST,
   COINGECKO_LIST,
-  CMC_ALL_LIST,
-  CMC_STABLECOIN,
-  KLEROS_LIST,
-  GEMINI_LIST,
+  // CMC_ALL_LIST,
+  // CMC_STABLECOIN,
+  // KLEROS_LIST,
+  // GEMINI_LIST,
   ...UNSUPPORTED_LIST_URLS // need to load unsupported tokens as well
 ]
 
