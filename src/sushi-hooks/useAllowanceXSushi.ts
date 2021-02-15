baf2cd48b667841eeb5a3494a4531ab95d05c56b
omakasebar <omakasebar@gmail.com>
2021-02-14 23:36:09 -0600

    add useAllowanceXsushi, remove logging from useAllowance

--- src/sushi-hooks/useAllowanceXSushi.ts
+++ src/sushi-hooks/useAllowanceXSushi.ts
@@ -2,16 +2,14 @@ import { useCallback, useEffect, useState } from 'react'
 import { Contract } from 'ethers'
 import { BigNumber } from '@ethersproject/bignumber'
 import { useActiveWeb3React } from '../hooks'
-import { isAddress } from '../utils'
-import ERC20_ABI from '../constants/abis/erc20.json'
-import { useContract, useMasterChefContract } from './useContract'
+import { useSushiContract, useSushiBarContract } from './useContract'
 
-const useAllowance = (lpAddress: string) => {
+const useXSushiAllowanceStaking = () => {
   const [allowance, setAllowance] = useState(BigNumber.from(0))
   const { account } = useActiveWeb3React()
-  const masterChefContract = useMasterChefContract()
-  const lpAddressChecksum = isAddress(lpAddress)
-  const lpContract = useContract(lpAddressChecksum, ERC20_ABI, false)
+
+  const sushiContract = useSushiContract()
+  const barContract = useSushiBarContract()
 
   const getAllowance = async (
     contract: Contract | null,
@@ -27,19 +25,21 @@ const useAllowance = (lpAddress: string) => {
   }
 
   const fetchAllowance = useCallback(async () => {
-    const allowance = await getAllowance(lpContract, account, masterChefContract?.address)
-    setAllowance(BigNumber.from(allowance))
-  }, [account, lpContract, masterChefContract?.address])
+    if (account) {
+      const allowance = await getAllowance(sushiContract, account, barContract?.address)
+      setAllowance(BigNumber.from(allowance))
+    }
+  }, [account, barContract, sushiContract])
 
   useEffect(() => {
-    if (account && masterChefContract && lpContract) {
+    if (account && barContract && sushiContract) {
       fetchAllowance()
     }
     const refreshInterval = setInterval(fetchAllowance, 10000)
     return () => clearInterval(refreshInterval)
-  }, [account, masterChefContract, lpContract, fetchAllowance])
+  }, [account, barContract, fetchAllowance, sushiContract])
 
   return allowance
 }
 
-export default useAllowance
+export default useXSushiAllowanceStaking
