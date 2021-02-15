9483e51ef90e0f023b001bca50b496ab8f877a3b
Matthew Lilley <hello@matthewlilley.com>
2021-02-15 13:02:08 +0000

    Group SushiBar functions by domain, useSushiBar returns enter/leave callbacks

---
+++ src/sushi-hooks/useSushiBar.ts
@@ -0,0 +1,46 @@
+import { useCallback } from 'react'
+import { BigNumber } from '@ethersproject/bignumber'
+//import { useActiveWeb3React } from '../hooks'
+import { useSushiBarContract } from './useContract'
+import { useTransactionAdder } from '../state/transactions/hooks'
+
+const useSushiBar = () => {
+  const addTransaction = useTransactionAdder()
+  const barContract = useSushiBarContract(true)
+
+  const enter = useCallback(
+    async (amount: string) => {
+      try {
+        const tx = await barContract?.enter(
+          BigNumber.from(amount)
+            .mul(BigNumber.from(10).pow(18))
+            .toString()
+        )
+        return addTransaction(tx, { summary: 'Enter SushiBar' })
+      } catch (e) {
+        return e
+      }
+    },
+    [addTransaction, barContract]
+  )
+
+  const leave = useCallback(
+    async (amount: string) => {
+      try {
+        const tx = await barContract?.leave(
+          BigNumber.from(amount)
+            .mul(BigNumber.from(10).pow(18))
+            .toString()
+        )
+        return addTransaction(tx, { summary: 'Leave SushiBar' })
+      } catch (e) {
+        return e
+      }
+    },
+    [addTransaction, barContract]
+  )
+
+  return { enter, leave }
+}
+
+export default useSushiBar
