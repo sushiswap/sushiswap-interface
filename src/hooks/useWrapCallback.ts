65f8fbb2fb302d5999b40151620b0b192fcf09f6
Matthew Lilley <hello@matthewlilley.com>
2021-03-02 10:37:21 +0000

    feat: show native currency symbol for wrapped native

--- src/hooks/useWrapCallback.ts
+++ src/hooks/useWrapCallback.ts
@@ -44,13 +44,13 @@ export default function useWrapCallback(
             ? async () => {
                 try {
                   const txReceipt = await wethContract.deposit({ value: `0x${inputAmount.raw.toString(16)}` })
-                  addTransaction(txReceipt, { summary: `Wrap ${inputAmount.toSignificant(6)} ETH to WETH` })
+                  addTransaction(txReceipt, { summary: `Wrap ${inputAmount.toSignificant(6)} ${Currency.getNativeCurrencySymbol(chainId)} to W${Currency.getNativeCurrencySymbol(chainId)}` })
                 } catch (error) {
                   console.error('Could not deposit', error)
                 }
               }
             : undefined,
-        inputError: sufficientBalance ? undefined : 'Insufficient ETH balance'
+        inputError: sufficientBalance ? undefined : `Insufficient ${Currency.getNativeCurrencySymbol(chainId)} balance`
       }
     } else if (currencyEquals(WETH[chainId], inputCurrency) && outputCurrency === ETHER) {
       return {
@@ -60,7 +60,7 @@ export default function useWrapCallback(
             ? async () => {
                 try {
                   const txReceipt = await wethContract.withdraw(`0x${inputAmount.raw.toString(16)}`)
-                  addTransaction(txReceipt, { summary: `Unwrap ${inputAmount.toSignificant(6)} WETH to ETH` })
+                  addTransaction(txReceipt, { summary: `Unwrap ${inputAmount.toSignificant(6)} W${Currency.getNativeCurrencySymbol(chainId)} to ${Currency.getNativeCurrencySymbol(chainId)}` })
                 } catch (error) {
                   console.error('Could not withdraw', error)
                 }
