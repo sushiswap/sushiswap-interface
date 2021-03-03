41f0b1344dc7052e90ec43365252e16882fd0721
omakasebar <omakasebar@gmail.com>
2021-03-03 02:15:56 -0600

    update gradient colors based on feedback

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -47,18 +47,18 @@ const sheen = keyframes`{
 export const ButtonPrimary = styled(Base)`
   /* background-color: ${({ theme }) => theme.primary1}; */
   overflow:hidden;
-  background: linear-gradient(to right, #2172e5 , #ff007a);
+  background: linear-gradient(to right, #0094ec , #f537c3);
   background-origin: border-box;
   color: white;
   &:focus {
     box-shadow: 0 0 0 1pt ${({ theme }) => darken(0.05, theme.primary1)};
     /*background: ${({ theme }) => darken(0.05, theme.primary1)};*/
-    background: linear-gradient(to right, #2172e5 , #ff007a);
+    background: linear-gradient(to right, #0094ec , #f537c3);
   }
   &:active {
     box-shadow: 0 0 0 1pt ${({ theme }) => darken(0.1, theme.primary1)};
     /*background: ${({ theme }) => darken(0.1, theme.primary1)};*/
-    background: linear-gradient(to right, #2172e5 , #ff007a);
+    background: linear-gradient(to right, #0094ec , #f537c3);
   }
   &:disabled {
     pointer-events: none;
@@ -74,7 +74,7 @@ export const ButtonPrimary = styled(Base)`
   }
   &:hover {
     /*background: ${({ theme }) => darken(0.05, theme.primary1)};*/
-    background: linear-gradient(to right, #2172e5 , #ff007a);
+    background: linear-gradient(to right, #0094ec , #f537c3);
     background-origin: border-box;
     &::after {
       animation: ${sheen} 0.5s forwards;
--- src/components/Header/UniBalanceContent.tsx
+++ src/components/Header/UniBalanceContent.tsx
@@ -23,7 +23,7 @@ const ContentWrapper = styled(AutoColumn)`
 
 const ModalUpper = styled(DataCard)`
   box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
-  background: radial-gradient(76.02% 75.41% at 1.84% 0%, #ff007a 0%, #021d43 100%);
+  background: radial-gradient(76.02% 75.41% at 1.84% 0%, #f537c3 0%, #021d43 100%);
   padding: 0.5rem;
 `
 
