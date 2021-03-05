e1e1f132c946f457f8e33a5fcb5c585827f931fc
Matthew Lilley <hello@matthewlilley.com>
2021-03-05 00:09:16 +0000

    feat: drop infura for alchemy

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -9,18 +9,12 @@ import { FortmaticConnector } from './Fortmatic'
 import { NetworkConnector } from './NetworkConnector'
 import { ChainId } from '@sushiswap/sdk'
 
-const INFURA_API_KEY = process.env.REACT_APP_INFURA_API_KEY
-
-if (!INFURA_API_KEY) {
-  throw new Error(`INFURA_API_KEY ${INFURA_API_KEY} must be a defined environment variable`)
-}
-
 const RPC = {
-  [ChainId.MAINNET]: `https://mainnet.infura.io/v3/${INFURA_API_KEY}`,
-  [ChainId.ROPSTEN]: `https://ropsten.infura.io/v3/${INFURA_API_KEY}`,
-  [ChainId.RINKEBY]: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
-  [ChainId.GÖRLI]: `https://goerli.infura.io/v3/${INFURA_API_KEY}`,
-  [ChainId.KOVAN]: `https://kovan.infura.io/v3/${INFURA_API_KEY}`,
+  [ChainId.MAINNET]: 'https://eth-mainnet.alchemyapi.io/v2/q1gSNoSMEzJms47Qn93f9-9Xg5clkmEC',
+  [ChainId.ROPSTEN]: 'https://eth-ropsten.alchemyapi.io/v2/cidKix2Xr-snU3f6f6Zjq_rYdalKKHmW',
+  [ChainId.RINKEBY]: 'https://eth-rinkeby.alchemyapi.io/v2/XVLwDlhGP6ApBXFz_lfv0aZ6VmurWhYD',
+  [ChainId.GÖRLI]: 'https://eth-goerli.alchemyapi.io/v2/Dkk5d02QjttYEoGmhZnJG37rKt8Yl3Im',
+  [ChainId.KOVAN]: 'https://eth-kovan.alchemyapi.io/v2/6OVAa_B_rypWWl9HqtiYK26IRxXiYqER',
   [ChainId.FANTOM]: 'https://rpcapi.fantom.network',
   [ChainId.FANTOM_TESTNET]: 'https://rpc.testnet.fantom.network',
   [ChainId.MATIC]: 'https://rpc-mainnet.maticvigil.com',
