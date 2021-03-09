3a036de4c186acecb7cf0e6bcdc38b7fe7ba4d56
codingsh <codingsh@pm.me>
2021-03-09 05:07:38 +0000

    feat(WalletConnect): add support to torus wallet

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -4,6 +4,7 @@ import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
 import { WalletLinkConnector } from '@web3-react/walletlink-connector'
 import { PortisConnector } from '@web3-react/portis-connector'
 import { LatticeConnector } from '@web3-react/lattice-connector'
+import { TorusConnector } from '@web3-react/torus-connector'
 
 import { FortmaticConnector } from './Fortmatic'
 import { NetworkConnector } from './NetworkConnector'
