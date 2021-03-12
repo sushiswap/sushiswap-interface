ea02330d5dadb4fa4162d1e58fa54ce716e2c524
Matthew Lilley <hello@matthewlilley.com>
2021-03-12 05:40:19 +0000

    fix: exports

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,7 +6,7 @@ import { BigNumber } from '@ethersproject/bignumber'
 import { abi as IUniswapV2Router02ABI } from '@uniswap/v2-periphery/build/IUniswapV2Router02.json'
 import { ChainId, JSBI, Percent, Token, CurrencyAmount, Currency, ETHER, ROUTER_ADDRESS } from '@sushiswap/sdk'
 import { TokenAddressMap } from '../state/lists/hooks'
-import ethers from 'ethers'
+import { ethers } from 'ethers'
 
 import Fraction from '../constants/Fraction'
 
