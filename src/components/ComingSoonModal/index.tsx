c4e980399f6dcb8b3c5e1e3e8ef32ef4569e3044
Lint Action <lint-action@samuelmeuli.com>
2021-03-12 09:46:20 +0000

    Fix code style issues with ESLint

--- src/components/ComingSoonModal/index.tsx
+++ src/components/ComingSoonModal/index.tsx
@@ -18,8 +18,8 @@ const Wrapper = styled.div`
 const ModalUpper = styled(Column)`
   padding: 3rem 1rem 2rem;
   position: relative;
-  background-color: #18212E;
-  background-image: linear-gradient(-55deg, #18212E 50%, #202D3E 50%);
+  background-color: #18212e;
+  background-image: linear-gradient(-55deg, #18212e 50%, #202d3e 50%);
   z-index: 0;
 `
 
@@ -27,7 +27,6 @@ const ModalLower = styled(Column)`
   padding: 3rem 1rem;
   background: #222636;
 `
-
 
 const StyledClose = styled(X)`
   position: absolute;
@@ -72,29 +71,40 @@ export default function ComingSoonModal() {
           <StyledPlus />
           <AutoColumn gap="md">
             <RowBetween>
-              <div><img width="66px" height="auto" src={BentoBox} /><TYPE.mediumHeader>BentoBox</TYPE.mediumHeader></div>
-              <div><img width="80px" height="auto" src={Kashi} /></div>
+              <div>
+                <img width="66px" height="auto" src={BentoBox} />
+                <TYPE.mediumHeader>BentoBox</TYPE.mediumHeader>
+              </div>
+              <div>
+                <img width="80px" height="auto" src={Kashi} />
+              </div>
             </RowBetween>
           </AutoColumn>
-          <StyledClose stroke="gray" onClick={() => {
-            localStorage.setItem("seenComingSoon", "true")
-            setIsOpen(false)
-          }}/>
+          <StyledClose
+            stroke="gray"
+            onClick={() => {
+              localStorage.setItem('seenComingSoon', 'true')
+              setIsOpen(false)
+            }}
+          />
           <StyledComingSoon src={ComingSoonImage} />
         </ModalUpper>
         <ModalLower>
-        <AutoRow gap="8px" justify="center">
-          <TYPE.largeHeader>
-            Kash Margin Trading <br/>
-            in BentoBox App
-          </TYPE.largeHeader>
-          <TYPE.subHeader fontWeight={600} fontSize={16} color="gray">
-            BentoBox is a reveolutionary way from SUSHI<br/> to interact with apps on L1 in a highly<br/> gas-efficient manner.
-          </TYPE.subHeader>
-          <TYPE.white fontWeight={400}>
-            The first app in the BentoBox system is Kashi, a<br/> gas-optimised margin trading platform with<br/> restricted lending pairs and short positions.
-          </TYPE.white>
-        </AutoRow>
+          <AutoRow gap="8px" justify="center">
+            <TYPE.largeHeader>
+              Kash Margin Trading <br />
+              in BentoBox App
+            </TYPE.largeHeader>
+            <TYPE.subHeader fontWeight={600} fontSize={16} color="gray">
+              BentoBox is a reveolutionary way from SUSHI
+              <br /> to interact with apps on L1 in a highly
+              <br /> gas-efficient manner.
+            </TYPE.subHeader>
+            <TYPE.white fontWeight={400}>
+              The first app in the BentoBox system is Kashi, a<br /> gas-optimised margin trading platform with
+              <br /> restricted lending pairs and short positions.
+            </TYPE.white>
+          </AutoRow>
         </ModalLower>
       </Wrapper>
     </Modal>
