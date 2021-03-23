7ddf335005e7b05b5e15fce9fc696c7f5f8554dc
Matthew Lilley <hello@matthewlilley.com>
2021-03-23 06:39:35 +0000

    background overlay

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -255,16 +255,11 @@ export const ThemedGlobalStyle = createGlobalStyle`
 html {
   color: ${({ theme }) => theme.text1};
   background-color: ${({ theme }) => theme.bg2};
+  background-image: url("data:image/svg+xml,%3Csvg width='42' height='44' viewBox='0 0 42 44' xmlns='http://www.w3.org/2000/svg'%3E%3Cg id='Page-1' fill='none' fill-rule='evenodd'%3E%3Cg id='brick-wall' fill='%239C92AC' fill-opacity='0.01'%3E%3Cpath d='M0 0h42v44H0V0zm1 1h40v20H1V1zM0 23h20v20H0V23zm22 0h20v20H22V23z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
 }
 
 body {
   min-height: 100vh;
-  /*background-image: ${({ theme }) =>
-    theme.darkMode
-      ? `radial-gradient(rgba(29, 45, 65, 1), rgba(29, 45, 65, 0.8), rgba(29, 45, 65, 0.2)), url(${BrickWallBackground});`
-      : `none`}; 
-  background-repeat: repeat */
-  /* background-size: cover; */
 }
 
 body::after {    
