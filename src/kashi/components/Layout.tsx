f0430d50a9776c4675b5efffb250c826cb7e7ecf
omakasebar <omakasebar@gmail.com>
2021-03-24 12:19:05 -0500

    polish:revert layout headers

--- src/kashi/components/Layout.tsx
+++ src/kashi/components/Layout.tsx
@@ -20,23 +20,19 @@ export default function Layout({ left = undefined, children = undefined, right =
             <div className="font-semibold text-2xl">Kashi</div>
           </div>
         </div>
-        <div className="flex items-end col-span-10 lg:col-span-8">
-          <div className="flex justify-center col-span-10 lg:col-span-2 lg:justify-start">
+        <div className="flex col-span-8 items-end">
+          <div className="w-full flex justify-center lg:justify-between pb-2 px-6">
             <div className="hidden lg:block">
               <MarketsNavigation />
-            </div>
-            <div className="flex lg:hidden items-center pb-2">
-              <img src={KashiLogo} className="block w-14 h-10 mr-2" />
-              <div className="hidden sm:block font-semibold text-2xl">Kashi</div>
             </div>
             <Navigation />
           </div>
         </div>
       </div>
-      <div className={`md:px-4 grid grid-cols-10 gap-4`}>
-        {left && <div className={`col-span-10 lg:col-span-2`}>{left}</div>}
-        <TeardropCard className={`col-span-10 ${right ? 'lg:col-span-5' : 'lg:col-span-8'}`}>{children}</TeardropCard>
-        {right && <Card className="col-span-10 lg:col-span-3">{right}</Card>}
+      <div className={`md:px-4 grid grid-cols-1 lg:grid-cols-10 gap-4`}>
+        {left && <div className={`col-span-12 lg:col-span-2`}>{left}</div>}
+        <TeardropCard className={`col-span-12 ${right ? 'lg:col-span-5' : 'lg:col-span-8'}`}>{children}</TeardropCard>
+        {right && <Card className="col-span-12 lg:col-span-3">{right}</Card>}
       </div>
     </>
   )
