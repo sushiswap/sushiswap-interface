db599a0f23f7365929dd482ed7cd78ed223f08c0
omakasebar <omakasebar@gmail.com>
2021-03-23 20:47:44 -0500

    polish:clean up kashi title font size, mobile responsiveness

--- src/kashi/components/Layout.tsx
+++ src/kashi/components/Layout.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { TeardropCard, Navigation } from '.'
-import { TYPE } from 'theme'
 import KashiLogo from 'assets/images/kashi-kanji-wires.png'
 import MarketsNavigation from './MarketsNavigation'
 import Card from './Card'
@@ -15,18 +14,20 @@ export default function Layout({ left = undefined, children = undefined, right =
   return (
     <>
       <div className={`md:px-4 grid grid-cols-1 lg:grid-cols-10 gap-4`}>
-        <div className="flex col-span-2 justify-center lg:justify-start">
-          <div className="flex items-center pb-3">
-            <img src={KashiLogo} className="w-10 y-10 sm:w-20 sm:y-20 lg:w-28 lg:y-28" />
-            <TYPE.extraLargeHeader color="extraHighEmphesisText" lineHeight={1}>
-              Kashi
-            </TYPE.extraLargeHeader>
+        <div className="hidden lg:flex col-span-2 justify-center lg:justify-start">
+          <div className="flex items-center pb-3 px-4">
+            <img src={KashiLogo} className="w-10 y-10 sm:w-14 sm:y-14 lg:w-18 lg:y-18 mx-2" />
+            <div className="font-semibold text-2xl">Kashi</div>
           </div>
         </div>
         <div className="flex col-span-8 items-end">
-          <div className="w-full flex justify-center lg:justify-between pb-2 px-6">
+          <div className="w-full flex justify-between px-6 md:px-4">
             <div className="hidden lg:block">
               <MarketsNavigation />
+            </div>
+            <div className="flex lg:hidden items-center pb-2">
+              <img src={KashiLogo} className="block w-14 h-10 mr-2" />
+              <div className="hidden sm:block font-semibold text-2xl">Kashi</div>
             </div>
             <Navigation />
           </div>
