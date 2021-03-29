2e050d90bb9749472d4da9567fd35f86760da7c9
omakasebar <omakasebar@gmail.com>
2021-03-29 09:02:02 -0500

    clean up warning deps

--- src/kashi/components/Alert.tsx
+++ src/kashi/components/Alert.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styled from 'styled-components'
+//import styled from 'styled-components'
 
 // export const Warning = styled.div<{ predicate: boolean }>`
 //   display: ${({ predicate }) => (predicate ? 'block' : 'none')}
