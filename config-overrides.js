d5fb1195d014c78d37d2910fa507a517dce7b483
Matthew Lilley <hello@matthewlilley.com>
2021-03-31 04:24:27 +0100

    config: overrides

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,8 @@
 const rewireReactHotLoader = require('react-app-rewire-hot-loader')
+const rewirePostCss = require('react-app-rewire-postcss')
 
 module.exports = function override (config, env) {
   config = rewireReactHotLoader(config, env)
+  config = rewirePostCss(config, env)
   return config
 }
