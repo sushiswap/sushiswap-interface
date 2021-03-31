c91d56df45c5c853e5ede0e059de3c986444073b
Matthew Lilley <hello@matthewlilley.com>
2021-03-31 05:29:57 +0100

    config: start to define design system in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,41 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      ...defaultTheme.colors,
+      blue: '#27B0E6',
+      pink: '#FA52A0',
+      purple: '#A755DD',
+      background: '#0D0415'
+    },
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
     extend: {
-      
+      fontFamily: {
+        sans: ['DM Sans',...defaultTheme.fontFamily.sans]
+      },
+      borderRadius: {
+        'none': '0',
+        'sm': '0.313',
+        DEFAULT: '0.625rem',
+      },
+      textColor: {
+        ...defaultTheme.textColor,
+        lowEmphesis: '#575757',
+        primary: '#BFBFBF',
+        secondary: '#7F7F7F',
+        highEmphesis: '#E3E3E3',
+      },
+      backgroundColor: {
+        ...defaultTheme.backgroundColor,
+        primary: '#0D0415'
+      }
     }
   },
   variants: {
@@ -10,7 +43,7 @@ module.exports = {
   },
   plugins: [],
   corePlugins: {
-    fontFamily: false,
+    fontFamily: true,
     preflight: true
   },
   purge: process.env.NODE_ENV !== 'development' ? ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'] : false
