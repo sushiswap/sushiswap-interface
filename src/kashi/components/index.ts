a25f0e386778216241cf0c6d5220f24169514d63
BoringCrypto <b@rtje.net>
2021-03-31 20:26:36 +1100

    fix: missing Header file

--- src/kashi/components/index.ts
+++ src/kashi/components/index.ts
@@ -1,5 +1,4 @@
 export { Warning } from './Alert'
-export { default as Header } from './Header'
 export { default as SectionHeader } from './SectionHeader'
 export { default as Search } from './Search'
 export { PrimaryTabs, SecondaryTabs } from './Tabs'
