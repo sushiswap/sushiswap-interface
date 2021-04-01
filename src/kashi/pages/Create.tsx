f4c0786f164986bc50f78363d846c48e0b09a083
Matthew Lilley <hello@matthewlilley.com>
2021-04-01 10:55:09 +0100

    clean: create page boilerplate

--- src/kashi/pages/Create.tsx
+++ src/kashi/pages/Create.tsx
@@ -1,60 +1,13 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
+import React from 'react'
 import { useActiveWeb3React } from 'hooks'
 import useTheme from 'hooks/useTheme'
 import DepositGraphic from 'assets/kashi/deposit-graphic.png'
 import { Card, Layout, LendCardHeader, BackButton, ListBox } from '../components'
-
-import getTokenIcon from 'kashi/functions/getTokenIcon'
-
-import { Input as NumericalInput } from 'components/NumericalInput'
-
-export const InputRow = styled.div`
-  ${({ theme }) => theme.flexRowNoWrap}
-  align-items: center;
-  border-radius: 10px;
-  // background-color: #2e3348;
-  // padding: 0.75rem 0.5rem 0.75rem 1rem;
-`
-
-export const StyledBalanceMax = styled.button`
-  height: 28px;
-  padding-right: 8px;
-  padding-left: 8px;
-  background-color: ${({ theme }) => theme.primary5};
-  border: 1px solid ${({ theme }) => theme.primary5};
-  border-radius: ${({ theme }) => theme.borderRadius};
-  border-radius: 6px;
-  font-size: 0.875rem;
-  font-weight: 500;
-  cursor: pointer;
-  margin-right: 0.5rem;
-  color: ${({ theme }) => theme.primaryText1};
-  :hover {
-    border: 1px solid ${({ theme }) => theme.primary1};
-  }
-  :focus {
-    border: 1px solid ${({ theme }) => theme.primary1};
-    outline: none;
-  }
-  ${({ theme }) => theme.mediaWidth.upToExtraSmall`
-    margin-right: 0.5rem;
-  `};
-`
-
-export const LabelRow = styled.div`
-  ${({ theme }) => theme.flexRowNoWrap}
-  align-items: center;
-  color: ${({ theme }) => theme.mediumEmphesisText};
-  font-size: 0.75rem;
-  line-height: 1rem;
-  padding: 0.75rem 0;
-`
+import { getTokenIcon } from 'kashi/functions'
 
 const CreatePair = () => {
   const { account, chainId } = useActiveWeb3React()
   const theme = useTheme()
-
   return (
     <Layout
       left={
