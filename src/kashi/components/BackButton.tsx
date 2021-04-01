d91771c1ff75cde6a6cd7301c950e83b52edb3d1
Matthew Lilley <hello@matthewlilley.com>
2021-04-01 10:55:30 +0100

    chore: remove unused imports

--- src/kashi/components/BackButton.tsx
+++ src/kashi/components/BackButton.tsx
@@ -1,6 +1,5 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import { useHistory } from 'react-router-dom'
-
 import { ChevronLeft } from 'react-feather'
 
 const BackButton = ({ defaultRoute }: any) => {
