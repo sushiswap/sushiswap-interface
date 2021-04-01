6171899088756cd7a28488e86aac2abb1c00bc44
Matthew Lilley <hello@matthewlilley.com>
2021-04-01 02:34:14 +0100

    feat: card header variants

--- src/kashi/components/CardHeader.tsx
+++ src/kashi/components/CardHeader.tsx
@@ -1,14 +1,26 @@
 import styled from 'styled-components'
 
-const CardHeader = styled.div<{ border?: boolean; market?: string }>`
-  display: flex;
-  align-items: center;
-  background: ${({ theme }) => theme.extraDarkPurple};
-  border-radius: 10px 10px 0 0;
-  padding: 32px 32px 26px;
-  border-bottom: 6px solid
-    ${({ market, border, theme }) =>
-      border ? `${market === 'Supply' ? theme.primaryBlue : theme.primaryPink}` : 'transparent'};
-`
+import React from 'react'
 
-export default CardHeader
+// const CardHeader = styled.div<{ border?: boolean; market?: string }>`
+//   display: flex;
+//   align-items: center;
+//   background: ${({ theme }) => theme.extraDarkPurple};
+//   border-radius: 10px 10px 0 0;
+//   padding: 32px 32px 26px;
+//   border-bottom: 6px solid
+//     ${({ market, border, theme }) =>
+//       border ? `${market === 'Supply' ? theme.primaryBlue : theme.primaryPink}` : 'transparent'};
+// `
+
+export default function CardHeader({ className, children }: any) {
+  return <div className={`${className} flex items-center rounded-t p-8`}>{children}</div>
+}
+
+export function BorrowCardHeader({ children }: any) {
+  return <CardHeader className="bg-kashi-borrow-header border-b-8 border-pink">{children}</CardHeader>
+}
+
+export function LendCardHeader({ children }: any) {
+  return <CardHeader className="bg-kashi-lend-header border-b-8 border-blue">{children}</CardHeader>
+}
