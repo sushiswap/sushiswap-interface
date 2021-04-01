cc48491a6c41fe45ac47b8d12b9afb67dcb03405
omakasebar <omakasebar@gmail.com>
2021-04-01 03:32:20 -0500

    feat: add ListBox generic

---
+++ src/kashi/components/ListBox.tsx
@@ -0,0 +1,90 @@
+import React, { useState } from 'react'
+import { Listbox, Transition } from '@headlessui/react'
+
+const tokens = ['Wade Cooper']
+
+export default function ListBox({ label }: any) {
+  const [selectedToken, setSelectedToken] = useState(tokens[0])
+
+  return (
+    <div className="flex items-center justify-center p-12">
+      <div className="w-full">
+        <Listbox as="div" className="space-y-1" value={selectedToken} onChange={setSelectedToken}>
+          {({ open }) => (
+            <>
+              <Listbox.Label className="block text-base leading-5 font-medium text-gray-700 pb-2">
+                {label}
+              </Listbox.Label>
+              <div className="relative">
+                <span className="inline-block w-full rounded-md shadow-sm">
+                  <Listbox.Button className="cursor-default relative w-full rounded-md border bg-background border-none py-4 px-6 text-left focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition ease-in-out duration-150 sm:text-sm sm:leading-5">
+                    <span className="block truncate">{selectedToken}</span>
+                    <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                      <svg className="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="none" stroke="currentColor">
+                        <path
+                          d="M7 7l3-3 3 3m0 6l-3 3-3-3"
+                          strokeWidth="1.5"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        />
+                      </svg>
+                    </span>
+                  </Listbox.Button>
+                </span>
+
+                <Transition
+                  show={open}
+                  leave="transition ease-in duration-100"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0"
+                  className="absolute mt-1 w-full rounded-md bg-background shadow-lg"
+                >
+                  <Listbox.Options
+                    static
+                    className="max-h-60 rounded-md py-1 text-base leading-6 shadow-xs overflow-auto focus:outline-none sm:text-sm sm:leading-5"
+                  >
+                    {tokens.map(person => (
+                      <Listbox.Option key={person} value={person}>
+                        {({ selected, active }) => (
+                          <div
+                            className={`${
+                              active ? 'text-white bg-blue-600' : 'text-gray-900'
+                            } cursor-default select-none relative py-2 pl-8 pr-4`}
+                          >
+                            <span className={`${selected ? 'font-semibold' : 'font-normal'} block truncate`}>
+                              {person}
+                            </span>
+                            {selected && (
+                              <span
+                                className={`${
+                                  active ? 'text-white' : 'text-blue-600'
+                                } absolute inset-y-0 left-0 flex items-center pl-1.5`}
+                              >
+                                <svg
+                                  className="h-5 w-5"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  viewBox="0 0 20 20"
+                                  fill="currentColor"
+                                >
+                                  <path
+                                    fillRule="evenodd"
+                                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                                    clipRule="evenodd"
+                                  />
+                                </svg>
+                              </span>
+                            )}
+                          </div>
+                        )}
+                      </Listbox.Option>
+                    ))}
+                  </Listbox.Options>
+                </Transition>
+              </div>
+            </>
+          )}
+        </Listbox>
+      </div>
+    </div>
+  )
+}
--- src/kashi/pages/Create.tsx
+++ src/kashi/pages/Create.tsx
@@ -3,7 +3,9 @@ import styled from 'styled-components'
 import { useActiveWeb3React } from 'hooks'
 import useTheme from 'hooks/useTheme'
 import DepositGraphic from 'assets/kashi/deposit-graphic.png'
-import { Card, Layout, LendCardHeader, BackButton } from '../components'
+import { Card, Layout, LendCardHeader, BackButton, ListBox } from '../components'
+
+import getTokenIcon from 'kashi/functions/getTokenIcon'
 
 import { Input as NumericalInput } from 'components/NumericalInput'
 
@@ -81,10 +83,18 @@ const CreatePair = () => {
         }
       >
         <div className="space-y-6">
+          <ListBox label={'Collateral (LONG)'} />
           <label className="block pb-4">
             <span className="text-gray-700 pb-2">Collateral (LONG)</span>
             <select className="form-select block w-full mt-1 rounded bg-background border-none py-4 px-6">
-              <option>Option 1</option>
+              <option className="flex">
+                <img
+                  src={getTokenIcon('0x6B3595068778DD592e39A122f4f5a5cF09C90fE2')}
+                  className="block w-10 h-10 sm:w-12 sm:h-12 rounded-lg"
+                  alt=""
+                />
+                Option 1
+              </option>
               <option>Option 2</option>
             </select>
           </label>
