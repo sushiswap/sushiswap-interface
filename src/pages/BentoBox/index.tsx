da6f2292a11007404f6dd9f3b3c4eed8b59ff5a7
Matthew Lilley <hello@matthewlilley.com>
2021-04-03 21:44:27 +0100

    style: bg

--- src/pages/BentoBox/index.tsx
+++ src/pages/BentoBox/index.tsx
@@ -32,17 +32,12 @@ function BentoBox(): JSX.Element {
     <div>
       <div className="absolute bg-dark-1000 top-0 right-0 bottom-0 left-0" />
       <div
+        className="absolute -top-32 right-0 bottom-0 left-0 bg-bentobox-hero bg-bottom bg-cover opacity-30 bg-no-repeat"
         style={{
-          position: 'absolute',
-          top: '-130px',
-          right: 0,
-          bottom: 0,
-          left: 0
+          maxHeight: '700px'
         }}
-        className="bg-bentobox-hero bg-top bg-auto opacity-30 bg-no-repeat"
       />
-
-      <div className="relative pt-48">
+      <div className="relative pt-48 w-full">
         <img alt="" src={BentoBoxLogo} className="w-80 h-auto -mt-52 mx-auto" />
         <div className="container mx-auto max-w-3xl">
           <div className="flex flex-col justify-center">
@@ -53,43 +48,43 @@ function BentoBox(): JSX.Element {
             manner. In order to use any one of the decentralized apps below you&apos;ll need to first enable them and
             deposit any ERC20 asset to your BentoBox balance.
           </div>
-          {/* List of Apps */}
         </div>
-        <div className="container mx-auto px-6 max-w-5xl">
-          <div className="grid gap-12 grid-flow-auto grid-cols-4">
-            <Card className="col-span-4 sm:col-span-2 md:col-span-1 w-full bg-dark-800 hover:bg-dark-900 cursor-pointer rounded shadow-pink-glow hover:shadow-pink-glow-hovered">
-              <div className="relative w-full">
-                <img alt="" src={KashiNeonSign} className="block m-auto w-full h-auto mb-4" />
-                {account ? (
-                  kashiApproved && kashiApproved === true ? (
-                    <Link to={'/bento/kashi/borrow'}>
-                      <GradientButton className="w-full rounded text-base text-high-emphesis px-4 py-3">
-                        Enter
-                      </GradientButton>
-                    </Link>
-                  ) : (
-                    <GradientButton
-                      className="w-full rounded text-base text-high-emphesis px-4 py-3"
-                      onClick={handleApprove}
-                    >
-                      Enable Kashi
+      </div>
+
+      <div className="container mx-auto px-6 max-w-5xl">
+        <div className="grid gap-12 grid-flow-auto grid-cols-4">
+          <Card className="col-span-4 sm:col-span-2 md:col-span-1 w-full bg-dark-800 hover:bg-dark-900 cursor-pointer rounded shadow-pink-glow hover:shadow-pink-glow-hovered">
+            <div className="relative w-full">
+              <img alt="" src={KashiNeonSign} className="block m-auto w-full h-auto mb-4" />
+              {account ? (
+                kashiApproved && kashiApproved === true ? (
+                  <Link to={'/bento/kashi/borrow'}>
+                    <GradientButton className="w-full rounded text-base text-high-emphesis px-4 py-3">
+                      Enter
                     </GradientButton>
-                  )
+                  </Link>
                 ) : (
-                  <Web3Status />
-                )}
-              </div>
-            </Card>
-            <Card className="flex items-center justify-center col-span-4 sm:col-span-2 md:col-span-1  bg-dark-800 hover:bg-dark-900 cursor-pointer shadow-blue-glow hover:shadow-blue-glow-hovered transition-colors">
-              <img src={ComingSoon} alt="Coming Soon" />
-            </Card>
-            <Card className="flex items-center justify-center col-span-4 sm:col-span-2 md:col-span-1 bg-dark-800 hover:bg-dark-900 cursor-pointer shadow-pink-glow hover:shadow-pink-glow-hovered transition-colors">
-              <img src={ComingSoon} alt="Coming Soon" />
-            </Card>
-            <Card className="flex items-center justify-center col-span-4 sm:col-span-2 md:col-span-1 bg-dark-800 hover:bg-dark-900 cursor-pointer shadow-blue-glow hover:shadow-blue-glow-hovered transition-colors">
-              <img src={ComingSoon} alt="Coming Soon" />
-            </Card>
-          </div>
+                  <GradientButton
+                    className="w-full rounded text-base text-high-emphesis px-4 py-3"
+                    onClick={handleApprove}
+                  >
+                    Enable Kashi
+                  </GradientButton>
+                )
+              ) : (
+                <Web3Status />
+              )}
+            </div>
+          </Card>
+          <Card className="flex items-center justify-center col-span-4 sm:col-span-2 md:col-span-1  bg-dark-800 hover:bg-dark-900 cursor-pointer shadow-blue-glow hover:shadow-blue-glow-hovered transition-colors">
+            <img src={ComingSoon} alt="Coming Soon" />
+          </Card>
+          <Card className="flex items-center justify-center col-span-4 sm:col-span-2 md:col-span-1 bg-dark-800 hover:bg-dark-900 cursor-pointer shadow-pink-glow hover:shadow-pink-glow-hovered transition-colors">
+            <img src={ComingSoon} alt="Coming Soon" />
+          </Card>
+          <Card className="flex items-center justify-center col-span-4 sm:col-span-2 md:col-span-1 bg-dark-800 hover:bg-dark-900 cursor-pointer shadow-blue-glow hover:shadow-blue-glow-hovered transition-colors">
+            <img src={ComingSoon} alt="Coming Soon" />
+          </Card>
         </div>
       </div>
     </div>
