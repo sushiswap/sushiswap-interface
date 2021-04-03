6d6e9f32af55ad3e3f89115cbb28315030e41751
Matthew Lilley <hello@matthewlilley.com>
2021-04-03 22:01:19 +0100

    refactor: background

--- src/pages/BentoBox/index.tsx
+++ src/pages/BentoBox/index.tsx
@@ -7,6 +7,7 @@ import useKashi from 'kashi/hooks/useKashi'
 import KashiNeonSign from '../../assets/kashi/kashi-neon.png'
 import ComingSoon from '../../assets/kashi/coming-soon.svg'
 import BentoBoxLogo from '../../assets/kashi/bentobox-logo.svg'
+import BentoBoxHero from '../../assets/kashi/bentobox-hero.png'
 function BentoBox(): JSX.Element {
   const { account } = useActiveWeb3React()
   const { kashiApproved, approveMaster } = useKashi()
@@ -32,17 +33,19 @@ function BentoBox(): JSX.Element {
     <div>
       <div className="absolute bg-dark-1000 top-0 right-0 bottom-0 left-0" />
       <div
-        className="absolute -top-32 right-0 bottom-0 left-0 bg-bentobox-hero bg-bottom bg-cover opacity-30 bg-no-repeat"
+        className="absolute -inset-32"
         style={{
           maxHeight: '700px'
         }}
-      />
-      <div className="relative pt-48 w-full">
-        <img alt="" src={BentoBoxLogo} className="w-80 h-auto -mt-52 mx-auto" />
+      >
+        <img className="h-full w-full object-cover object-bottom opacity-50" src={BentoBoxHero} alt="" />
+      </div>
+
+      <div className="relative flex flex-col items-center pt-48">
+        <img alt="" src={BentoBoxLogo} className="w-80 h-auto -mt-52" />
+
         <div className="container mx-auto max-w-3xl">
-          <div className="flex flex-col justify-center">
-            <div className="font-bold text-6xl text-high-emphesis text-center">BentoBox Apps</div>
-          </div>
+          <div className="font-bold text-center text-6xl text-high-emphesis">BentoBox Apps</div>
           <div className="font-medium text-center text-high-emphesis mt-4 mb-8 p-4">
             BentoBox is a revolutionary new way from SUSHI to interact with dapps on L1 in a highly gas efficient
             manner. In order to use any one of the decentralized apps below you&apos;ll need to first enable them and
