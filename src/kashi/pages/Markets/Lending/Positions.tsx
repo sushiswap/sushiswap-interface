72a1d110cde100d95c48a0404165b778880f87cb
Matthew Lilley <hello@matthewlilley.com>
2021-04-05 18:58:51 +0100

    feat: lending position borrowed usd & utilisation

--- src/kashi/pages/Markets/Lending/Positions.tsx
+++ src/kashi/pages/Markets/Lending/Positions.tsx
@@ -6,16 +6,17 @@ import { formattedPercent, formattedNum } from 'utils'
 function Positions({ pairs }: any): JSX.Element | null {
   return (
     <div>
-      <div className="pb-4 px-4 grid grid-cols-4 sm:grid-cols-4 text-sm font-semibold text-gray-500">
-        <div className="hover:text-gray-400 col-span-2 sm:col-span-2">Your Positions</div>
-        <div className="text-right pl-4 hover:text-gray-400">Lent</div>
+      <div className="grid grid-cols-4 md:grid-cols-5 pb-4 px-4 text-sm font-semibold text-gray-500">
+        <div className="hover:text-gray-400 col-span-1 md:col-span-2">Your Positions</div>
+        <div className="text-right hover:text-gray-400">Lent</div>
+        <div className="text-right hover:text-gray-400">Borrowed</div>
         <div className="text-right hover:text-gray-400">APR</div>
       </div>
       {pairs.map((pair: any) => {
         return (
           <div key={pair.address}>
             <Link to={'/bento/kashi/pair/' + pair.address + '/lend'} className="block text-high-emphesis">
-              <div className="mb-2 py-4 px-4 items-center align-center grid grid-cols-4 text-sm font-semibold rounded bg-dark-800">
+              <div className="grid grid-cols-4 md:grid-cols-5 mb-2 py-4 px-4 items-center align-center text-sm font-semibold rounded bg-dark-800">
                 <div className="hidden space-x-2 md:flex">
                   <img
                     src={getTokenIcon(pair.collateral.address)}
@@ -24,7 +25,7 @@ function Positions({ pairs }: any): JSX.Element | null {
                   />
                   <img src={getTokenIcon(pair.asset.address)} className="w-10 h-10 sm:w-12 sm:h-12 rounded-lg" alt="" />
                 </div>
-                <div className="col-span-2 md:col-span-1 sm:block">
+                <div className="col-span-1 sm:block md:col-span-1">
                   <div>
                     {pair.collateral.symbol} / {pair.asset.symbol}
                   </div>
@@ -35,6 +36,10 @@ function Positions({ pairs }: any): JSX.Element | null {
                     {formattedNum(pair.userAssetAmount.string, false)} {pair.asset.symbol}
                   </div>
                   <div className="text-gray-500 text-sm">{formattedNum(pair.userAssetAmount.usd, true)}</div>
+                </div>
+                <div className="text-right">
+                  <div>{formattedPercent(pair.utilization.string)}</div>
+                  <div className="text-gray-500">{formattedNum(pair.currentUserBorrowAmount.usd, true)}</div>
                 </div>
                 <div className="text-right">{formattedPercent(pair.currentSupplyAPR.value)}</div>
               </div>
