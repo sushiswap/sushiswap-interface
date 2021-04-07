10c3563686e6e1d09ac22b168599172049c7855a
Matthew Lilley <hello@matthewlilley.com>
2021-04-07 02:58:41 +0100

    feat: add icon

--- src/kashi/components/MarketHeader.tsx
+++ src/kashi/components/MarketHeader.tsx
@@ -5,6 +5,7 @@ import { LendCardHeader, BorrowCardHeader } from '.'
 import { BigNumber } from '@ethersproject/bignumber'
 import { formattedNum } from 'utils'
 import { Search } from 'react-feather'
+import { ReactComponent as AddIcon } from 'assets/svg/add.svg'
 
 function MarketHeader({ type = 'Borrow', children, search, term }: any) {
   const pairs = useKashiPairs()
@@ -15,7 +16,11 @@ function MarketHeader({ type = 'Borrow', children, search, term }: any) {
   return (
     <Header>
       <div className="flex flex-col md:flex-row items-center justify-between w-full">
-        <div className=" text-3xl text-high-emphesis">{type}</div>
+        <div className="flex items-center">
+          <div className="text-3xl text-high-emphesis mr-2">{type}</div>
+          <AddIcon className="fill-current w-5 h-5" />
+        </div>
+
         <div className="flex justify-end w-full py-4 md:py-0">
           <div className="relative w-full max-w-md">
             <input
