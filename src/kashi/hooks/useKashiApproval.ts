07170970fb24f3a98baeec124d1eb8304b49b125
Matthew Lilley <hello@matthewlilley.com>
2021-04-07 13:03:17 +0100

    bentobox master contract approval

--- src/kashi/hooks/useKashiApproval.ts
+++ src/kashi/hooks/useKashiApproval.ts
@@ -1,11 +1,12 @@
-import { useBentoMasterContractAllowed } from "data/Allowances"
-import { ethers } from "ethers"
-import { useActiveWeb3React } from "hooks"
-import { KASHI_ADDRESS } from "kashi/constants"
-import { signMasterContractApproval } from "kashi/entities/KashiCooker"
-import { useCallback, useMemo } from "react"
-import { useTransactionAdder } from "state/transactions/hooks"
-import { useBentoBoxContract } from "sushi-hooks/useContract"
+import { useBentoMasterContractAllowed } from 'data/Allowances'
+import { ethers } from 'ethers'
+import { useActiveWeb3React } from 'hooks'
+import { KASHI_ADDRESS } from 'kashi/constants'
+import { signMasterContractApproval } from 'kashi/entities/KashiCooker'
+import { useCallback, useMemo } from 'react'
+import { useTransactionAdder } from 'state/transactions/hooks'
+import { useBentoBoxContract } from 'sushi-hooks/useContract'
+import { TransactionResponse } from '@ethersproject/providers'
 
 export enum ApprovalState {
   UNKNOWN,
@@ -16,9 +17,7 @@ export enum ApprovalState {
 }
 
 // returns a variable indicating the state of the approval and a function which approves if necessary or early returns
-export function useKashiApproveCallback(
-    masterContract: string
-): [ApprovalState, () => Promise<void>] {
+export function useKashiApproveCallback(masterContract: string): [ApprovalState, () => Promise<void>] {
   const { account, library, chainId } = useActiveWeb3React()
   const pendingApproval = false
   const currentAllowed = useBentoMasterContractAllowed(KASHI_ADDRESS, account || ethers.constants.AddressZero)
@@ -29,7 +28,7 @@ export function useKashiApproveCallback(
     if (pendingApproval) return ApprovalState.PENDING
 
     return currentAllowed ? ApprovalState.APPROVED : ApprovalState.NOT_APPROVED
-  }, [currentAllowed, masterContract])
+  }, [currentAllowed, masterContract, pendingApproval])
 
   const bentoBoxContract = useBentoBoxContract()
   const addTransaction = useTransactionAdder()
@@ -43,6 +42,12 @@ export function useKashiApproveCallback(
       console.error('no token')
       return
     }
+
+    if (!bentoBoxContract) {
+      console.error('no bentobox contract')
+      return
+    }
+
     if (!account) {
       console.error('no account')
       return
@@ -51,8 +56,28 @@ export function useKashiApproveCallback(
       console.error('no library')
       return
     }
-    
-    await signMasterContractApproval(bentoBoxContract, KASHI_ADDRESS, account, library, true, chainId)
+
+    const signature = await signMasterContractApproval(bentoBoxContract, KASHI_ADDRESS, account, library, true, chainId)
+
+    const { v, r, s } = ethers.utils.splitSignature(signature)
+
+    // const options = {
+    //   gasLimit: calculateGasMargin(estimatedGas)
+    // }
+
+    return bentoBoxContract
+      .setMasterContractApproval(account, masterContract, true, v, r, s)
+      .then((response: TransactionResponse) => {
+        addTransaction(response, {
+          summary: 'Approve BentoBox'
+          // summary: 'Approve ' + amountToApprove.currency.symbol,
+          // approval: { tokenAddress: token.address, spender: spender }
+        })
+      })
+      .catch((error: Error) => {
+        console.debug('Failed to approve token', error)
+        throw error
+      })
 
     /*return bentoBoxContract
       .approve(spender, useExact ? amountToApprove.raw.toString() : MaxUint256, {
@@ -68,7 +93,7 @@ export function useKashiApproveCallback(
         console.debug('Failed to approve token', error)
         throw error
       })*/
-  }, [approvalState, account, library, chainId, bentoBoxContract, addTransaction])
+  }, [approvalState, account, library, chainId, bentoBoxContract, masterContract, addTransaction])
 
   return [approvalState, approve]
 }
