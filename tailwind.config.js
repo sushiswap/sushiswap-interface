da1203e2c3cb0e1b057dd0d440f334764671760c
Matthew Lilley <hello@matthewlilley.com>
2021-04-12 09:57:27 +0100

    style: disabled checkbox & coming soon badge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,81 +1,81 @@
-const defaultTheme = require("tailwindcss/defaultTheme")
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  darkMode: false, // or 'media' or 'class'
-  important: true,
-  theme: {
-    colors: {
-      ...defaultTheme.colors,
-      blue: '#27B0E6',
-      pink: '#FA52A0',
-      purple: '#A755DD',
-      background: '#0D0415'
+    darkMode: false, // or 'media' or 'class'
+    important: true,
+    theme: {
+        colors: {
+            ...defaultTheme.colors,
+            blue: '#27B0E6',
+            pink: '#FA52A0',
+            purple: '#A755DD',
+            background: '#0D0415',
+            'dark-1000': '#0D0415',
+            'dark-900': '#161522',
+            'dark-800': '#202231',
+            'dark-pink': '#221825',
+            'dark-blue': '#0F182A'
+        },
+        screens: {
+            sm: '480px',
+            md: '768px',
+            lg: '976px',
+            xl: '1440px'
+        },
+        extend: {
+            backgroundImage: theme => ({
+                ...defaultTheme.backgroundImage,
+                'bentobox-hero': "url('/src/assets/kashi/bentobox-hero.jpg')",
+                'bentobox-logo': "url('/src/assets/kashi/bentobox-logo.png')"
+            }),
+            fontFamily: {
+                sans: ['DM Sans', ...defaultTheme.fontFamily.sans]
+            },
+            borderRadius: {
+                none: '0',
+                sm: '0.313rem',
+                DEFAULT: '0.625rem'
+            },
+            textColor: {
+                ...defaultTheme.textColor,
+                'low-emphesis': '#575757',
+                primary: '#BFBFBF',
+                secondary: '#7F7F7F',
+                'high-emphesis': '#E3E3E3'
+            },
+            backgroundColor: {
+                ...defaultTheme.backgroundColor,
+                input: '#2E3348'
+            },
+            boxShadow: {
+                ...defaultTheme.boxShadow,
+                'pink-glow': '0px 57px 90px -47px rgba(250, 82, 160, 0.15)',
+                'blue-glow': '0px 57px 90px -47px rgba(39, 176, 230, 0.17)',
+                'pink-glow-hovered': '0px 57px 90px -47px rgba(250, 82, 160, 0.30)',
+                'blue-glow-hovered': '0px 57px 90px -47px rgba(39, 176, 230, 0.34)'
+            },
+            ringWidth: {
+                ...defaultTheme.ringWidth,
+                DEFAULT: '1px'
+            }
+        }
     },
-    screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
+    variants: {
+        extend: {
+            opacity: ['disabled'],
+            backgroundColor: ['checked'],
+            borderColor: ['checked']
+        }
     },
-    extend: {
-      backgroundImage: theme => ({
-        ...defaultTheme.backgroundImage,
-        'bentobox-hero': "url('/src/assets/kashi/bentobox-hero.jpg')",
-        'bentobox-logo': "url('/src/assets/kashi/bentobox-logo.png')",
-       }),
-      fontFamily: {
-        sans: ['DM Sans', ...defaultTheme.fontFamily.sans]
-      },
-      borderRadius: {
-        'none': '0',
-        'sm': '0.313rem',
-        DEFAULT: '0.625rem'
-      },
-      textColor: {
-        ...defaultTheme.textColor,
-        'low-emphesis': '#575757',
-        primary: '#BFBFBF',
-        secondary: '#7F7F7F',
-        'high-emphesis': '#E3E3E3'
-      },
-      backgroundColor: {
-        ...defaultTheme.backgroundColor,
-        'dark-1000': '#0D0415',
-        'dark-900': '#161522',
-        'dark-800': '#202231',
-        'dark-pink': '#221825',
-        'dark-blue': '#0F182A',
-        'input': "#2E3348"
-      },
-      boxShadow: {
-        ...defaultTheme.boxShadow,
-        'pink-glow': '0px 57px 90px -47px rgba(250, 82, 160, 0.15)',
-        'blue-glow': '0px 57px 90px -47px rgba(39, 176, 230, 0.17)',
-        'pink-glow-hovered': '0px 57px 90px -47px rgba(250, 82, 160, 0.30)',
-        'blue-glow-hovered': '0px 57px 90px -47px rgba(39, 176, 230, 0.34)'
-      },
-      ringWidth: {
-        ...defaultTheme.ringWidth,
-        'DEFAULT': '1px',
-      }
-    }
-  },
-  variants: {
-    extend: {
-      opacity: ['disabled'],
-      backgroundColor: ['checked'],
-      borderColor: ['checked'],
-    }
-  },
-  plugins: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/line-clamp'),
-    require('@tailwindcss/aspect-ratio'),
-  ],
-  corePlugins: {
-    fontFamily: true,
-    preflight: true
-  },
-  purge: process.env.NODE_ENV !== 'development' ? ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'] : false
+    plugins: [
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/line-clamp'),
+        require('@tailwindcss/aspect-ratio')
+    ],
+    corePlugins: {
+        fontFamily: true,
+        preflight: true
+    },
+    purge: process.env.NODE_ENV !== 'development' ? ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'] : false
 }
