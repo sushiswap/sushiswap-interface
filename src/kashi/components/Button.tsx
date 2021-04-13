ee8305f6135252df03492a4e5ff582d1831d0194
Matthew Lilley <hello@matthewlilley.com>
2021-04-13 23:30:35 +0100

    feat: button

--- src/kashi/components/Button.tsx
+++ src/kashi/components/Button.tsx
@@ -2,15 +2,21 @@ import React from 'react'
 import { ChevronLeft } from 'react-feather'
 import { useHistory } from 'react-router-dom'
 
-const FILLED = {
+const SIZE = {
     default: '',
+    small: '',
+    large: ''
+}
+
+const FILLED = {
+    default: 'bg-transparent',
     blue: 'bg-blue bg-opacity-80 w-full rounded text-base text-high-emphesis px-4 py-3 hover:bg-opacity-100',
     pink: 'bg-pink bg-opacity-80 w-full rounded text-base text-high-emphesis px-4 py-3 hover:bg-opacity-100',
     gradient: 'bg-gradient-to-r from-blue to-pink'
 }
 
 const OUTLINED = {
-    default: '',
+    default: 'bg-transparent',
     blue: 'bg-blue bg-opacity-20 outline-blue rounded text-xs text-blue px-2 py-1 hover:bg-opacity-40',
     pink: 'bg-pink bg-opacity-20 outline-pink rounded text-xs text-pink px-2 py-1 hover:bg-opacity-40',
     gradient: 'bg-gradient-to-r from-blue to-pink'
@@ -19,16 +25,18 @@ const OUTLINED = {
 const VARIANT = {
     outlined: OUTLINED,
     filled: FILLED
-    // gradient: 'bg-gradient-to-r from-blue to-pink'
 }
 
 export type ButtonColor = 'blue' | 'pink' | 'gradient' | 'default'
+
+export type ButtonSize = 'small' | 'large' | 'default'
 
 export type ButtonVariant = 'outlined' | 'filled'
 
 export interface ButtonProps {
     children?: React.ReactChild | React.ReactChild[]
     color?: ButtonColor
+    size?: ButtonSize
     variant?: ButtonVariant
 }
 
@@ -36,12 +44,13 @@ function Button({
     children,
     className,
     color = 'default',
+    size = 'default',
     variant = 'filled',
     ...rest
 }: ButtonProps & React.ButtonHTMLAttributes<HTMLButtonElement>): JSX.Element {
     return (
         <button
-            className={`${VARIANT[variant][color]} focus:outline-none focus:ring disabled:opacity-50 ${className}`}
+            className={`${VARIANT[variant][color]} ${SIZE[size]} focus:outline-none focus:ring disabled:opacity-50 ${className}`}
             {...rest}
         >
             {children}
