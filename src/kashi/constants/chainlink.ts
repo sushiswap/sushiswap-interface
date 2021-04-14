bc50d65885967eaed4dc45133911401a2eb6a8f1
BoringCrypto <b@rtje.net>
2021-04-14 17:42:58 +1000

    feat: xSUSHI support

--- src/kashi/constants/chainlink.ts
+++ src/kashi/constants/chainlink.ts
@@ -138,6 +138,7 @@ export const CHAINLINK_TOKENS: { [chainId in ChainId]?: ChainlinkToken[] } = {
         { symbol: 'WAVES', name: 'WAVES', address: '0x1cF4592ebfFd730c7dc92c1bdFFDfc3B9EfCf29a', decimals: 18 },
         { symbol: 'WBTC', name: 'Wrapped BTC', address: '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599', decimals: 8 },
         { symbol: 'wNXM', name: 'Wrapped NXM', address: '0x0d438F3b5175Bebc262bF23753C1E53d03432bDE', decimals: 18 },
+        { symbol: 'xSUSHI', name: 'SushiBar', address: '0x8798249c2E607446EfB7Ad49eC89dD1865Ff4272', decimals: 18 },
         { symbol: 'YFI', name: 'yearn.finance', address: '0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e', decimals: 18 },
         { symbol: 'YFII', name: 'YFII.finance', address: '0xa1d0E215a23d7030842FC67cE582a6aFa3CCaB83', decimals: 18 },
         {
@@ -721,6 +722,13 @@ export const CHAINLINK_MAPPING: { [chainId in ChainId]?: ChainlinkMappingList } 
         },
         '0xe572CeF69f43c2E488b33924AF04BDacE19079cf': {
             from: '0x6B3595068778DD592e39A122f4f5a5cF09C90fE2',
+            to: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+            decimals: 18,
+            fromDecimals: 18,
+            toDecimals: 18
+        },
+        '0xAE51d1f913eDB0f80562F270017806f3e9566029': {
+            from: '0x8798249c2E607446EfB7Ad49eC89dD1865Ff4272',
             to: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
             decimals: 18,
             fromDecimals: 18,
