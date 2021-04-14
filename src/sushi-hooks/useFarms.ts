368807fed66cdf56602643c020f67a2b481d867c
omakasebar <omakasebar@gmail.com>
2021-04-13 22:05:40 -0500

    feat: add kashiPair in yield

--- src/sushi-hooks/useFarms.ts
+++ src/sushi-hooks/useFarms.ts
@@ -23,14 +23,17 @@ const useFarms = () => {
     const fetchAllFarms = useCallback(async () => {
         const results = await Promise.all([
             masterchef.query({
+                // results[0]
                 query: poolsQuery
             }),
             exchange.query({
+                // results[1]
                 query: liquidityPositionSubsetQuery,
                 variables: { user: '0xc2edad668740f1aa35e4d8f227fb8e17dca888cd' }
             }),
-            getAverageBlockTime(),
-            sushiData.sushi.priceUSD()
+            getAverageBlockTime(), // results[2]
+            sushiData.sushi.priceUSD(), // results[3]
+            sushiData.bentobox.kashiStakedInfo() //results[4]
         ])
         const pools = results[0]?.data.pools
         const pairAddresses = pools
@@ -44,49 +47,67 @@ const useFarms = () => {
         })
 
         const liquidityPositions = results[1]?.data.liquidityPositions
-        const pairs = pairsQuery?.data.pairs
         const averageBlockTime = results[2]
         const sushiPrice = results[3]
+        const kashiPairs = Array(results[4])
+        const pairs = pairsQuery?.data.pairs
+
+        const KASHI_PAIRS = [200] // kashiPair pids
+
+        console.log('kashiPairs:', kashiPairs, pools)
 
         const farms = pools
             .filter((pool: any) => {
-                return !POOL_DENY.includes(pool.id) && pairs.find((pair: any) => pair?.id === pool.pair)
+                return (
+                    !POOL_DENY.includes(pool.id) &&
+                    (pairs.find((pair: any) => pair?.id === pool.pair) || KASHI_PAIRS.includes(pool.id))
+                )
             })
             .map((pool: any) => {
-                const pair = pairs.find((pair: any) => pair.id === pool.pair)
-                const liquidityPosition = liquidityPositions.find(
-                    (liquidityPosition: any) => liquidityPosition.pair.id === pair.id
-                )
-                const blocksPerHour = 3600 / averageBlockTime
-                const balance = Number(pool.balance / 1e18) > 0 ? Number(pool.balance / 1e18) : 0.1
-                const totalSupply = pair.totalSupply > 0 ? pair.totalSupply : 0.1
-                const reserveUSD = pair.reserveUSD > 0 ? pair.reserveUSD : 0.1
-                const balanceUSD = (balance / Number(totalSupply)) * Number(reserveUSD)
-                const rewardPerBlock =
-                    ((pool.allocPoint / pool.owner.totalAllocPoint) * pool.owner.sushiPerBlock) / 1e18
-                const roiPerBlock = (rewardPerBlock * sushiPrice) / balanceUSD
-                const roiPerHour = roiPerBlock * blocksPerHour
-                const roiPerDay = roiPerHour * 24
-                const roiPerMonth = roiPerDay * 30
-                const roiPerYear = roiPerMonth * 12
+                if (KASHI_PAIRS.includes(pool.id)) {
+                    const pair = kashiPairs.find((pair: any) => pair.id === pool.pair)
+                    return {
+                        ...pool,
+                        ...pair,
+                        type: 'kashiPair'
+                    }
+                } else {
+                    const pair = pairs.find((pair: any) => pair.id === pool.pair)
+                    const liquidityPosition = liquidityPositions.find(
+                        (liquidityPosition: any) => liquidityPosition.pair.id === pair.id
+                    )
+                    const blocksPerHour = 3600 / averageBlockTime
+                    const balance = Number(pool.balance / 1e18) > 0 ? Number(pool.balance / 1e18) : 0.1
+                    const totalSupply = pair.totalSupply > 0 ? pair.totalSupply : 0.1
+                    const reserveUSD = pair.reserveUSD > 0 ? pair.reserveUSD : 0.1
+                    const balanceUSD = (balance / Number(totalSupply)) * Number(reserveUSD)
+                    const rewardPerBlock =
+                        ((pool.allocPoint / pool.owner.totalAllocPoint) * pool.owner.sushiPerBlock) / 1e18
+                    const roiPerBlock = (rewardPerBlock * sushiPrice) / balanceUSD
+                    const roiPerHour = roiPerBlock * blocksPerHour
+                    const roiPerDay = roiPerHour * 24
+                    const roiPerMonth = roiPerDay * 30
+                    const roiPerYear = roiPerMonth * 12
 
-                return {
-                    ...pool,
-                    symbol: pair.token0.symbol + '-' + pair.token1.symbol,
-                    name: pair.token0.name + ' ' + pair.token1.name,
-                    pid: Number(pool.id),
-                    pairAddress: pair.id,
-                    slpBalance: pool.balance,
-                    liquidityPair: pair,
-                    roiPerBlock,
-                    roiPerHour,
-                    roiPerDay,
-                    roiPerMonth,
-                    roiPerYear,
-                    rewardPerThousand: 1 * roiPerDay * (1000 / sushiPrice),
-                    tvl: liquidityPosition?.liquidityTokenBalance
-                        ? (pair.reserveUSD / pair.totalSupply) * liquidityPosition.liquidityTokenBalance
-                        : 0.1
+                    return {
+                        ...pool,
+                        type: 'slp',
+                        symbol: pair.token0.symbol + '-' + pair.token1.symbol,
+                        name: pair.token0.name + ' ' + pair.token1.name,
+                        pid: Number(pool.id),
+                        pairAddress: pair.id,
+                        slpBalance: pool.balance,
+                        liquidityPair: pair,
+                        roiPerBlock,
+                        roiPerHour,
+                        roiPerDay,
+                        roiPerMonth,
+                        roiPerYear,
+                        rewardPerThousand: 1 * roiPerDay * (1000 / sushiPrice),
+                        tvl: liquidityPosition?.liquidityTokenBalance
+                            ? (pair.reserveUSD / pair.totalSupply) * liquidityPosition.liquidityTokenBalance
+                            : 0.1
+                    }
                 }
             })
 
