64096a887f80b86d2939268abf3bd5726547fa89
Matthew Lilley <hello@matthewlilley.com>
2021-04-16 19:41:38 +0100

    feat: link component

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import * as H from 'history'
+import React, { HTMLProps, useCallback } from 'react'
+import ReactGA from 'react-ga'
+import {
+    Link as ReactRouterLink,
+    LinkProps as ReactRouterLinkProps,
+    NavLink as ReactRouterNavLink,
+    NavLinkProps as ReactRouterNavLinkProps
+} from 'react-router-dom'
 
-// interface LinkProps {}
-
-function Link({
+function Link<S = H.LocationState>({
     href = '#',
     children,
-    className = 'text-baseline text-primary hover:text-high-emphesis',
+    className = 'text-baseline text-primary hover:text-high-emphesis focus:text-high-emphesis p-2 md:p-3',
     ...rest
-}: React.AnchorHTMLAttributes<HTMLAnchorElement>): JSX.Element {
+}: React.PropsWithoutRef<ReactRouterLinkProps<S>> & React.RefAttributes<HTMLAnchorElement>): JSX.Element {
     return (
         <a href={href} className={className} {...rest}>
             {children}
@@ -16,3 +22,55 @@ function Link({
 }
 
 export default Link
+
+export function NavLink<S = H.LocationState>({
+    href = '#',
+    children,
+    className = 'text-baseline text-primary hover:text-high-emphesis focus:text-high-emphesis p-2 md:p-3',
+    ...rest
+}: React.PropsWithoutRef<ReactRouterNavLinkProps<S>> & React.RefAttributes<HTMLAnchorElement>): JSX.Element {
+    return (
+        <ReactRouterNavLink href={href} className={className} activeClassName="text-high-emphesis" {...rest}>
+            {children}
+        </ReactRouterNavLink>
+    )
+}
+
+export function ExternalLink({
+    target = '_blank',
+    href,
+    children,
+    rel = 'noopener noreferrer',
+    ...rest
+}: Omit<HTMLProps<HTMLAnchorElement>, 'as' | 'ref' | 'onClick'> & { href: string }): JSX.Element {
+    const handleClick = useCallback(
+        (event: React.MouseEvent<HTMLAnchorElement>) => {
+            // don't prevent default, don't redirect if it's a new tab
+            if (target === '_blank' || event.ctrlKey || event.metaKey) {
+                ReactGA.outboundLink({ label: href }, () => {
+                    console.debug('Fired outbound link event', href)
+                })
+            } else {
+                event.preventDefault()
+                // send a ReactGA event and then trigger a location change
+                ReactGA.outboundLink({ label: href }, () => {
+                    window.location.href = href
+                })
+            }
+        },
+        [href, target]
+    )
+
+    return (
+        <a
+            target={target}
+            rel={rel}
+            href={href}
+            onClick={handleClick}
+            className="text-baseline text-primary hover:text-high-emphesis focus:text-high-emphesis p-2 md:p-3"
+            {...rest}
+        >
+            {children}
+        </a>
+    )
+}
