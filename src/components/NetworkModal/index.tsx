003cec1cf28c82e270323c4096e33a758dd9ada4
Matthew Lilley <hello@matthewlilley.com>
2021-04-16 04:06:20 +0100

    feat: wire up networks to switch

--- src/components/NetworkModal/index.tsx
+++ src/components/NetworkModal/index.tsx
@@ -6,22 +6,91 @@ import { useModalOpen, useNetworkModalToggle } from '../../state/application/hoo
 import { NETWORK_ICON, NETWORK_LABEL } from '../../constants/networks'
 import Modal from '../Modal'
 
+const PARAMS: {
+    [chainId in ChainId]?: {
+        chainId: string
+        chainName: string
+        nativeCurrency: {
+            name: string
+            symbol: string
+            decimals: string
+        }
+        rpcUrls: string[]
+        blockExplorerUrls: string[]
+    }
+} = {
+    [ChainId.FANTOM]: {
+        chainId: '0xfa',
+        chainName: 'Fantom',
+        nativeCurrency: {
+            name: 'Fantom',
+            symbol: 'FTM',
+            decimals: '18'
+        },
+        rpcUrls: ['https://rpcapi.fantom.network'],
+        blockExplorerUrls: ['https://ftmscan.com']
+    },
+    [ChainId.BSC]: {
+        chainId: '0x38',
+        chainName: 'Binance Smart Chain',
+        nativeCurrency: {
+            name: 'Binance Coin',
+            symbol: 'BNB',
+            decimals: '18'
+        },
+        rpcUrls: ['https://bsc-dataseed.binance.org'],
+        blockExplorerUrls: ['https://bscscan.com']
+    },
+    [ChainId.MATIC]: {
+        chainId: '0x89',
+        chainName: 'Matic',
+        nativeCurrency: {
+            name: 'Matic',
+            symbol: 'MATIC',
+            decimals: '18'
+        },
+        rpcUrls: ['https://rpc-mainnet.maticvigil.com'],
+        blockExplorerUrls: ['https://explorer-mainnet.maticvigil.com']
+    },
+    [ChainId.HECO]: {
+        chainId: '0x80',
+        chainName: 'Heco',
+        nativeCurrency: {
+            name: 'Heco Token',
+            symbol: 'HT',
+            decimals: '18'
+        },
+        rpcUrls: ['https://http-mainnet.hecochain.com'],
+        blockExplorerUrls: ['https://hecoinfo.com']
+    }
+}
+
 export default function NetworkModal(): JSX.Element | null {
-    const { chainId } = useActiveWeb3React()
+    const { chainId, library, account } = useActiveWeb3React()
     const networkModalOpen = useModalOpen(ApplicationModal.NETWORK)
     const toggleNetworkModal = useNetworkModalToggle()
+
     if (!chainId) return null
+
     return (
         <Modal isOpen={networkModalOpen} onDismiss={toggleNetworkModal}>
             <div className="text-2xl font-bold mb-3 text-high-emphesis">Select a Network</div>
 
             <div className="text-lg text-primary mb-6">
-                You are currently browsing <span className="font-bold">SUSHI</span>
-                <br /> on the <span className="font-bold">{NETWORK_LABEL[chainId]}</span> network
+                You are currently browsing <span className="font-bold text-pink">SUSHI</span>
+                <br /> on the <span className="font-bold text-blue">{NETWORK_LABEL[chainId]}</span> network
             </div>
 
             <div className="flex flex-col space-y-5 overflow-y-auto">
-                {[ChainId.MAINNET, ChainId.FANTOM, ChainId.BSC, ChainId.MATIC].map((key: ChainId, i: number) => {
+                {[
+                    ChainId.MAINNET,
+                    ChainId.FANTOM,
+                    ChainId.BSC,
+                    ChainId.MATIC,
+                    ChainId.HECO,
+                    ChainId.XDAI,
+                    ChainId.HARMONY
+                ].map((key: ChainId, i: number) => {
                     if (chainId === key) {
                         return (
                             <div className="bg-gradient-to-r from-blue to-pink w-full rounded p-px">
@@ -39,6 +108,11 @@ export default function NetworkModal(): JSX.Element | null {
                     return (
                         <div
                             key={i}
+                            onClick={() => {
+                                toggleNetworkModal()
+                                const params = PARAMS[key]
+                                library?.send('wallet_addEthereumChain', [params, account])
+                            }}
                             className="flex items-center bg-dark-800 hover:bg-dark-700 w-full rounded p-3 cursor-pointer"
                         >
                             <img src={NETWORK_ICON[key]} alt="Switch Network" className="rounded-md mr-2 w-8 h-8" />
