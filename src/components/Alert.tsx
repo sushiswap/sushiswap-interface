4d53b883119fc8ff07a4ca60962fd9b012dcfe84
Matthew Lilley <hello@matthewlilley.com>
2021-04-20 01:45:40 +0100

    style: add/remove/create lp

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const TYPE = {
     information: {
-        color: 'bg-purple bg-opacity-20 text-high-emphesis'
+        color: 'bg-purple bg-opacity-20 text-high-emphesis',
+        icon: (
+            <svg
+                width="33"
+                height="33"
+                className="text-purple"
+                viewBox="0 0 33 33"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                aria-hidden="true"
+            >
+                <path
+                    d="M16.5 0C7.40184 0 0 7.40184 0 16.5C0 25.5982 7.40184 33 16.5 33C25.5982 33 33 25.5982 33 16.5C33 7.40184 25.5982 0 16.5 0ZM16.5 25.9909C15.5747 25.9909 14.8245 25.2407 14.8245 24.3154C14.8245 23.39 15.5747 22.6398 16.5 22.6398C17.4253 22.6398 18.1755 23.39 18.1755 24.3154C18.1755 25.2407 17.4253 25.9909 16.5 25.9909ZM18.1755 17.3898C18.1755 18.3152 17.4253 19.0654 16.5 19.0654C15.5747 19.0654 14.8245 18.3152 14.8245 17.3898V8.56534C14.8245 7.63999 15.5747 6.8898 16.5 6.8898C17.4253 6.8898 18.1755 7.63999 18.1755 8.56534V17.3898Z"
+                    fill="#A755DD"
+                />
+            </svg>
+        )
     },
     warning: {
         color: 'bg-yellow-400 bg-opacity-25 text-high-emphesis',
@@ -43,14 +59,18 @@ const TYPE = {
 }
 
 export interface AlertProps {
-    message?: string
-    type?: 'warning' | 'error'
+    title?: string
+    message?: string | React.ReactChild | React.ReactChild[]
+    type?: 'warning' | 'error' | 'information'
+    showIcon?: boolean
 }
 
 export default function Alert({
+    title,
     message,
     type = 'warning',
-    className = ''
+    className = '',
+    showIcon = true
 }: AlertProps & React.HTMLAttributes<HTMLDivElement>): JSX.Element | null {
     if (!message) {
         return null
@@ -58,9 +78,10 @@ export default function Alert({
     const { color, icon } = TYPE[type]
     return (
         <div className={`block w-full rounded text-sm p-4 ${color} ${className}`}>
-            <div className="flex">
-                <div className="flex-shrink-0">{icon}</div>
-                <div className="ml-3">
+            {title && <div className="text-2xl font-medium mb-1">{title}</div>}
+            <div className="flex items-center">
+                {showIcon && <div className="flex-shrink-0">{icon}</div>}
+                <div className={`${!showIcon ? 'ml-0' : 'ml-3'}`}>
                     <p>{message}</p>
                 </div>
             </div>
