de50631f9a5372dd8066acdc385b02b34b6c24bb
Matthew Lilley <hello@matthewlilley.com>
2021-04-20 03:59:13 +0100

    fix: translations backend

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,10 +1,12 @@
 import i18next from 'i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
 import XHR from 'i18next-xhr-backend'
+import Backend from 'i18next-http-backend'
 import { initReactI18next } from 'react-i18next'
 
 i18next
-    .use(XHR)
+    // .use(XHR)
+    .use(Backend)
     .use(LanguageDetector)
     .use(initReactI18next)
     .init({
