50a1ca3eb82429279ecbb7cc8171818660c22f18
Matthew Lilley <hello@matthewlilley.com>
2021-04-21 06:41:46 +0100

    refactor: hooks entrypoint

---
+++ src/hooks/useActiveWeb3React.ts
@@ -0,0 +1,13 @@
+import { Web3Provider } from '@ethersproject/providers'
+import { ChainId } from '@sushiswap/sdk'
+import { useWeb3React as useWeb3ReactCore } from '@web3-react/core'
+import { Web3ReactContextInterface } from '@web3-react/core/dist/types'
+import { NetworkContextName } from '../constants'
+
+function useActiveWeb3React(): Web3ReactContextInterface<Web3Provider> & { chainId?: ChainId } {
+    const context = useWeb3ReactCore<Web3Provider>()
+    const contextNetwork = useWeb3ReactCore<Web3Provider>(NetworkContextName)
+    return context.active ? context : contextNetwork
+}
+
+export default useActiveWeb3React
---
+++ src/hooks/useEagerConnect.ts
@@ -0,0 +1,38 @@
+import { useWeb3React as useWeb3ReactCore } from '@web3-react/core'
+import { useEffect, useState } from 'react'
+import { isMobile } from 'react-device-detect'
+import { injected } from '../connectors'
+
+function useEagerConnect() {
+    const { activate, active } = useWeb3ReactCore() // specifically using useWeb3ReactCore because of what this hook does
+    const [tried, setTried] = useState(false)
+
+    useEffect(() => {
+        injected.isAuthorized().then(isAuthorized => {
+            if (isAuthorized) {
+                activate(injected, undefined, true).catch(() => {
+                    setTried(true)
+                })
+            } else {
+                if (isMobile && window.ethereum) {
+                    activate(injected, undefined, true).catch(() => {
+                        setTried(true)
+                    })
+                } else {
+                    setTried(true)
+                }
+            }
+        })
+    }, [activate]) // intentionally only running on mount (make sure it's only mounted once :))
+
+    // if the connection worked, wait until we get confirmation of that to flip the flag
+    useEffect(() => {
+        if (active) {
+            setTried(true)
+        }
+    }, [active])
+
+    return tried
+}
+
+export default useEagerConnect
