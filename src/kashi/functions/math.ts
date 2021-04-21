99edff52556a342cb7ae84ee417f8d6a86e65993
Matthew Lilley <hello@matthewlilley.com>
2021-04-21 05:51:04 +0100

    refactor: prototype extension to entry point bootstrap script

--- src/kashi/functions/math.ts
+++ src/kashi/functions/math.ts
@@ -3,31 +3,6 @@ import { Zero } from '@ethersproject/constants'
 import { parseUnits } from '@ethersproject/units'
 import { Fraction } from '../../entities'
 
-String.prototype.toBigNumber = function(decimals: BigNumberish): BigNumber {
-    try {
-        return parseUnits(this as string, decimals)
-    } catch (error) {
-        console.debug(`Failed to parse input amount: "${this}"`, error)
-    }
-    return BigNumber.from(0)
-}
-
-BigNumber.prototype.muldiv = function(multiplier: BigNumberish, divisor: BigNumberish): BigNumber {
-    return BigNumber.from(divisor).gt(0)
-        ? BigNumber.from(this)
-              .mul(multiplier)
-              .div(divisor)
-        : Zero
-}
-
-BigNumber.prototype.toFixed = function(decimals: BigNumberish = 18, maxFractions: BigNumberish = 8): string {
-    return this.toFraction(decimals, 10).toString(BigNumber.from(maxFractions).toNumber())
-}
-
-BigNumber.prototype.toFraction = function(decimals: BigNumberish = 18): Fraction {
-    return Fraction.from(this, decimals ? BigNumber.from(10).pow(decimals) : Zero)
-}
-
 export const ZERO = BigNumber.from('0')
 
 export function e10(exponent: BigNumber | Number | string): BigNumber {
