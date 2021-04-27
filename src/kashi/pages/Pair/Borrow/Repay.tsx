828bd3b14a60c65aeff922f5e4029691f3a9f6b0
Matthew Lilley <hello@matthewlilley.com>
2021-04-27 02:17:46 +0100

    feat: enable repay & enable max remove where appropriate

--- src/kashi/pages/Pair/Borrow/Repay.tsx
+++ src/kashi/pages/Pair/Borrow/Repay.tsx
@@ -24,186 +24,186 @@ interface RepayProps {
 }
 
 export default function Repay({ pair }: RepayProps) {
-    // const { chainId } = useActiveWeb3React()
-    // const info = useContext(KashiContext).state.info
+    const { chainId } = useActiveWeb3React()
+    const info = useContext(KashiContext).state.info
 
-    // // State
-    // const [useBentoRepay, setUseBentoRepay] = useState<boolean>(pair.asset.bentoBalance.gt(0))
-    // const [useBentoRemove, setUseBentoRemoveCollateral] = useState<boolean>(true)
+    // State
+    const [useBentoRepay, setUseBentoRepay] = useState<boolean>(pair.asset.bentoBalance.gt(0))
+    const [useBentoRemove, setUseBentoRemoveCollateral] = useState<boolean>(true)
 
-    // const [repayValue, setRepayAssetValue] = useState('')
-    // const [removeValue, setRemoveCollateralValue] = useState('')
-    // const [pinRemoveMax, setPinRemoveMax] = useState(false)
-    // const [pinRepayMax, setPinRepayMax] = useState(false)
-    // const [updateOracle, setUpdateOracle] = useState(false)
-    // const [swap, setSwap] = useState(false)
+    const [repayValue, setRepayAssetValue] = useState('')
+    const [removeValue, setRemoveCollateralValue] = useState('')
+    const [pinRemoveMax, setPinRemoveMax] = useState(false)
+    const [pinRepayMax, setPinRepayMax] = useState(false)
+    const [updateOracle, setUpdateOracle] = useState(false)
+    const [swap, setSwap] = useState(false)
 
-    // const assetToken = useCurrency(pair.asset.address) || undefined
-    // const collateralToken = useCurrency(pair.collateral.address) || undefined
+    const assetToken = useCurrency(pair.asset.address) || undefined
+    const collateralToken = useCurrency(pair.collateral.address) || undefined
 
-    // // Calculated
-    // const assetNative = WETH[chainId || 1].address == pair.asset.address
+    // Calculated
+    const assetNative = WETH[chainId || 1].address == pair.asset.address
 
-    // const balance = useBentoRepay
-    //     ? toAmount(pair.asset, pair.asset.bentoBalance)
-    //     : assetNative
-    //     ? info?.ethBalance
-    //     : pair.asset.balance
+    const balance = useBentoRepay
+        ? toAmount(pair.asset, pair.asset.bentoBalance)
+        : assetNative
+        ? info?.ethBalance
+        : pair.asset.balance
 
-    // const displayUpdateOracle = pair.currentExchangeRate.gt(0) ? updateOracle : true
+    const displayUpdateOracle = pair.currentExchangeRate.gt(0) ? updateOracle : true
 
-    // const displayRepayValue = pinRepayMax
-    //     ? minimum(pair.currentUserBorrowAmount.value, balance).toFixed(pair.asset.decimals)
-    //     : repayValue
+    const displayRepayValue = pinRepayMax
+        ? minimum(pair.currentUserBorrowAmount.value, balance).toFixed(pair.asset.decimals)
+        : repayValue
 
-    // const nextUserBorrowAmount = pair.currentUserBorrowAmount.value.sub(
-    //     displayRepayValue.toBigNumber(pair.asset.decimals)
-    // )
+    const nextUserBorrowAmount = pair.currentUserBorrowAmount.value.sub(
+        displayRepayValue.toBigNumber(pair.asset.decimals)
+    )
 
-    // const nextMinCollateralOracle = nextUserBorrowAmount.muldiv(pair.oracleExchangeRate, e10(16).mul('75'))
-    // const nextMinCollateralSpot = nextUserBorrowAmount.muldiv(pair.spotExchangeRate, e10(16).mul('75'))
-    // const nextMinCollateralStored = nextUserBorrowAmount.muldiv(
-    //     displayUpdateOracle ? pair.oracleExchangeRate : pair.currentExchangeRate,
-    //     e10(16).mul('75')
-    // )
-    // const nextMinCollateralMinimum = maximum(nextMinCollateralOracle, nextMinCollateralSpot, nextMinCollateralStored)
-    // const nextMaxRemoveCollateral = maximum(
-    //     pair.userCollateralAmount.value.sub(nextMinCollateralMinimum.mul(100).div(95)),
-    //     ZERO
-    // )
-    // const maxRemoveCollateral = nextMaxRemoveCollateral.toFixed(pair.collateral.decimals)
+    const nextMinCollateralOracle = nextUserBorrowAmount.muldiv(pair.oracleExchangeRate, e10(16).mul('75'))
+    const nextMinCollateralSpot = nextUserBorrowAmount.muldiv(pair.spotExchangeRate, e10(16).mul('75'))
+    const nextMinCollateralStored = nextUserBorrowAmount.muldiv(
+        displayUpdateOracle ? pair.oracleExchangeRate : pair.currentExchangeRate,
+        e10(16).mul('75')
+    )
+    const nextMinCollateralMinimum = maximum(nextMinCollateralOracle, nextMinCollateralSpot, nextMinCollateralStored)
+    const nextMaxRemoveCollateral = maximum(
+        pair.userCollateralAmount.value.sub(nextMinCollateralMinimum.mul(100).div(95)),
+        ZERO
+    )
+    const maxRemoveCollateral = nextMaxRemoveCollateral.toFixed(pair.collateral.decimals)
 
-    // const displayRemoveValue = pinRemoveMax ? maxRemoveCollateral : removeValue
+    const displayRemoveValue = pinRemoveMax ? maxRemoveCollateral : removeValue
 
-    // // Swap
-    // const [allowedSlippage] = useUserSlippageTolerance() // 10 = 0.1%
-    // const parsedAmount = tryParseAmount(displayRemoveValue, collateralToken)
-    // const foundTrade = useTradeExactIn(parsedAmount, assetToken) || undefined
-    // const extraRepay = swap
-    //     ? computeSlippageAdjustedAmounts(foundTrade, allowedSlippage)
-    //           [Field.OUTPUT]?.toFixed(pair.asset.decimals)
-    //           .toBigNumber(pair.asset.decimals) || ZERO
-    //     : ZERO
-    // //const nextUserCollateralValue = pair.userCollateralAmount.value.add(collateralValue.toBigNumber(pair.collateral.decimals)).add(extraCollateral)
+    // Swap
+    const [allowedSlippage] = useUserSlippageTolerance() // 10 = 0.1%
+    const parsedAmount = tryParseAmount(displayRemoveValue, collateralToken)
+    const foundTrade = useTradeExactIn(parsedAmount, assetToken) || undefined
+    const extraRepay = swap
+        ? computeSlippageAdjustedAmounts(foundTrade, allowedSlippage)
+              [Field.OUTPUT]?.toFixed(pair.asset.decimals)
+              .toBigNumber(pair.asset.decimals) || ZERO
+        : ZERO
+    //const nextUserCollateralValue = pair.userCollateralAmount.value.add(collateralValue.toBigNumber(pair.collateral.decimals)).add(extraCollateral)
 
-    // const nextUserCollateralAmount = pair.userCollateralAmount.value.sub(
-    //     displayRemoveValue.toBigNumber(pair.collateral.decimals)
-    // )
+    const nextUserCollateralAmount = pair.userCollateralAmount.value.sub(
+        displayRemoveValue.toBigNumber(pair.collateral.decimals)
+    )
 
-    // const nextMaxBorrowableOracle = nextUserCollateralAmount.muldiv(e10(16).mul('75'), pair.oracleExchangeRate)
-    // const nextMaxBorrowableSpot = nextUserCollateralAmount.muldiv(e10(16).mul('75'), pair.spotExchangeRate)
-    // const nextMaxBorrowableStored = nextUserCollateralAmount.muldiv(
-    //     e10(16).mul('75'),
-    //     displayUpdateOracle ? pair.oracleExchangeRate : pair.currentExchangeRate
-    // )
-    // const nextMaxBorrowMinimum = minimum(nextMaxBorrowableOracle, nextMaxBorrowableSpot, nextMaxBorrowableStored)
-    // const nextMaxBorrowSafe = nextMaxBorrowMinimum.muldiv('95', '100').sub(pair.currentUserBorrowAmount.value)
-    // const nextMaxBorrowPossible = maximum(minimum(nextMaxBorrowSafe, pair.maxAssetAvailable), ZERO)
+    const nextMaxBorrowableOracle = nextUserCollateralAmount.muldiv(e10(16).mul('75'), pair.oracleExchangeRate)
+    const nextMaxBorrowableSpot = nextUserCollateralAmount.muldiv(e10(16).mul('75'), pair.spotExchangeRate)
+    const nextMaxBorrowableStored = nextUserCollateralAmount.muldiv(
+        e10(16).mul('75'),
+        displayUpdateOracle ? pair.oracleExchangeRate : pair.currentExchangeRate
+    )
+    const nextMaxBorrowMinimum = minimum(nextMaxBorrowableOracle, nextMaxBorrowableSpot, nextMaxBorrowableStored)
+    const nextMaxBorrowSafe = nextMaxBorrowMinimum.muldiv('95', '100').sub(pair.currentUserBorrowAmount.value)
+    const nextMaxBorrowPossible = maximum(minimum(nextMaxBorrowSafe, pair.maxAssetAvailable), ZERO)
 
-    // const nextHealth = pair.currentUserBorrowAmount.value
-    //     .sub(displayRepayValue.toBigNumber(pair.asset.decimals))
-    //     .muldiv(BigNumber.from('1000000000000000000'), nextMaxBorrowMinimum)
+    const nextHealth = pair.currentUserBorrowAmount.value
+        .sub(displayRepayValue.toBigNumber(pair.asset.decimals))
+        .muldiv(BigNumber.from('1000000000000000000'), nextMaxBorrowMinimum)
 
-    // const transactionReview = new TransactionReview()
-    // if (displayRepayValue || displayRemoveValue) {
-    //     transactionReview.addTokenAmount(
-    //         'Borrow Limit',
-    //         pair.maxBorrowable.safe.value,
-    //         nextMaxBorrowSafe.add(displayRepayValue.toBigNumber(pair.asset.decimals)),
-    //         pair.asset
-    //     )
-    //     transactionReview.addPercentage('Health', pair.health.value, nextHealth)
-    // }
+    const transactionReview = new TransactionReview()
+    if (displayRepayValue || displayRemoveValue) {
+        transactionReview.addTokenAmount(
+            'Borrow Limit',
+            pair.maxBorrowable.safe.value,
+            nextMaxBorrowSafe.add(displayRepayValue.toBigNumber(pair.asset.decimals)),
+            pair.asset
+        )
+        transactionReview.addPercentage('Health', pair.health.value, nextHealth)
+    }
 
-    // const warnings = new Warnings()
-    //     .addError(
-    //         assetNative && !useBentoRepay && pinRepayMax,
-    //         `You cannot MAX repay ${pair.asset.symbol} directly from your wallet. Please deposit your ${pair.asset.symbol} into the BentoBox first, then repay. Because your debt is slowly accrueing interest we can't predict how much it will be once your transaction gets mined.`
-    //     )
-    //     .addError(
-    //         displayRemoveValue.toBigNumber(pair.collateral.decimals).gt(pair.userCollateralAmount.value),
-    //         'You have insufficient collateral. Please enter a smaller amount or repay more.'
-    //     )
-    //     .addError(
-    //         displayRepayValue.toBigNumber(pair.asset.decimals).gt(pair.currentUserBorrowAmount.value),
-    //         "You can't repay more than you owe. To fully repay, please click the 'max' button.",
-    //         new Warning(
-    //             balance?.lt(displayRepayValue.toBigNumber(pair.asset.decimals)),
-    //             `Please make sure your ${
-    //                 useBentoRepay ? 'BentoBox' : 'wallet'
-    //             } balance is sufficient to repay and then try again.`,
-    //             true
-    //         )
-    //     )
-    //     .addError(
-    //         displayRemoveValue
-    //             .toBigNumber(pair.collateral.decimals)
-    //             .gt(maximum(pair.userCollateralAmount.value.sub(nextMinCollateralMinimum), ZERO)),
-    //         'Removing this much collateral would put you into insolvency.',
-    //         new Warning(
-    //             displayRemoveValue.toBigNumber(pair.collateral.decimals).gt(nextMaxRemoveCollateral),
-    //             'Removing this much collateral would put you very close to insolvency.'
-    //         )
-    //     )
+    const warnings = new Warnings()
+        .addError(
+            assetNative && !useBentoRepay && pinRepayMax,
+            `You cannot MAX repay ${pair.asset.symbol} directly from your wallet. Please deposit your ${pair.asset.symbol} into the BentoBox first, then repay. Because your debt is slowly accrueing interest we can't predict how much it will be once your transaction gets mined.`
+        )
+        .addError(
+            displayRemoveValue.toBigNumber(pair.collateral.decimals).gt(pair.userCollateralAmount.value),
+            'You have insufficient collateral. Please enter a smaller amount or repay more.'
+        )
+        .addError(
+            displayRepayValue.toBigNumber(pair.asset.decimals).gt(pair.currentUserBorrowAmount.value),
+            "You can't repay more than you owe. To fully repay, please click the 'max' button.",
+            new Warning(
+                balance?.lt(displayRepayValue.toBigNumber(pair.asset.decimals)),
+                `Please make sure your ${
+                    useBentoRepay ? 'BentoBox' : 'wallet'
+                } balance is sufficient to repay and then try again.`,
+                true
+            )
+        )
+        .addError(
+            displayRemoveValue
+                .toBigNumber(pair.collateral.decimals)
+                .gt(maximum(pair.userCollateralAmount.value.sub(nextMinCollateralMinimum), ZERO)),
+            'Removing this much collateral would put you into insolvency.',
+            new Warning(
+                displayRemoveValue.toBigNumber(pair.collateral.decimals).gt(nextMaxRemoveCollateral),
+                'Removing this much collateral would put you very close to insolvency.'
+            )
+        )
 
-    // // Handlers
-    // async function onExecute(cooker: KashiCooker) {
-    //     let summary = ''
-    //     if (pinRepayMax && pair.userBorrowPart.gt(0) && balance.gte(pair.currentUserBorrowAmount.value)) {
-    //         cooker.repayPart(pair.userBorrowPart, useBentoRepay)
-    //         summary = 'Repay Max'
-    //     } else if (displayRepayValue.toBigNumber(pair.asset.decimals).gt(0)) {
-    //         cooker.repay(displayRepayValue.toBigNumber(pair.asset.decimals), useBentoRepay)
-    //         summary = 'Repay'
-    //     }
-    //     if (
-    //         displayRemoveValue.toBigNumber(pair.collateral.decimals).gt(0) ||
-    //         (pinRemoveMax && pair.userCollateralShare.gt(0))
-    //     ) {
-    //         const share =
-    //             pinRemoveMax &&
-    //             (nextUserBorrowAmount.isZero() ||
-    //                 (pinRepayMax && pair.userBorrowPart.gt(0) && balance.gte(pair.currentUserBorrowAmount.value)))
-    //                 ? pair.userCollateralShare
-    //                 : toShare(pair.collateral, displayRemoveValue.toBigNumber(pair.collateral.decimals))
+    // Handlers
+    async function onExecute(cooker: KashiCooker) {
+        let summary = ''
+        if (pinRepayMax && pair.userBorrowPart.gt(0) && balance.gte(pair.currentUserBorrowAmount.value)) {
+            cooker.repayPart(pair.userBorrowPart, useBentoRepay)
+            summary = 'Repay Max'
+        } else if (displayRepayValue.toBigNumber(pair.asset.decimals).gt(0)) {
+            cooker.repay(displayRepayValue.toBigNumber(pair.asset.decimals), useBentoRepay)
+            summary = 'Repay'
+        }
+        if (
+            displayRemoveValue.toBigNumber(pair.collateral.decimals).gt(0) ||
+            (pinRemoveMax && pair.userCollateralShare.gt(0))
+        ) {
+            const share =
+                pinRemoveMax &&
+                (nextUserBorrowAmount.isZero() ||
+                    (pinRepayMax && pair.userBorrowPart.gt(0) && balance.gte(pair.currentUserBorrowAmount.value)))
+                    ? pair.userCollateralShare
+                    : toShare(pair.collateral, displayRemoveValue.toBigNumber(pair.collateral.decimals))
 
-    //         cooker.removeCollateral(share, useBentoRemove)
-    //         summary += (summary ? ' and ' : '') + 'Remove Collateral'
-    //     }
-    //     setPinRemoveMax(false)
-    //     setRemoveCollateralValue('')
-    //     setRepayAssetValue('')
-    //     return summary
-    // }
+            cooker.removeCollateral(share, useBentoRemove)
+            summary += (summary ? ' and ' : '') + 'Remove Collateral'
+        }
+        setPinRemoveMax(false)
+        setRemoveCollateralValue('')
+        setRepayAssetValue('')
+        return summary
+    }
 
-    // const removeValueSet =
-    //     !displayRemoveValue.toBigNumber(pair.collateral.decimals).isZero() ||
-    //     (pinRemoveMax && pair.userCollateralShare.gt(ZERO))
-    // const repayValueSet = !displayRepayValue.toBigNumber(pair.asset.decimals).isZero()
+    const removeValueSet =
+        !displayRemoveValue.toBigNumber(pair.collateral.decimals).isZero() ||
+        (pinRemoveMax && pair.userCollateralShare.gt(ZERO))
+    const repayValueSet = !displayRepayValue.toBigNumber(pair.asset.decimals).isZero()
 
-    // const trade = swap && removeValueSet ? foundTrade : undefined
+    const trade = swap && removeValueSet ? foundTrade : undefined
 
-    // let actionName = 'Nothing to do'
+    let actionName = 'Nothing to do'
 
-    // if (removeValueSet) {
-    //     if (repayValueSet) {
-    //         actionName = 'Repay and remove collateral'
-    //     } else {
-    //         actionName = 'Remove collateral'
-    //     }
-    // } else if (repayValueSet) {
-    //     actionName = 'Repay'
-    // }
+    if (removeValueSet) {
+        if (repayValueSet) {
+            actionName = 'Repay and remove collateral'
+        } else {
+            actionName = 'Remove collateral'
+        }
+    } else if (repayValueSet) {
+        actionName = 'Repay'
+    }
 
-    // const actionDisabled =
-    //     (displayRepayValue.toBigNumber(pair.asset.decimals).lte(0) &&
-    //         displayRemoveValue.toBigNumber(pair.collateral.decimals).lte(0) &&
-    //         (!pinRemoveMax || pair.userCollateralShare.isZero())) ||
-    //     warnings.some(warning => warning.breaking)
+    const actionDisabled =
+        (displayRepayValue.toBigNumber(pair.asset.decimals).lte(0) &&
+            displayRemoveValue.toBigNumber(pair.collateral.decimals).lte(0) &&
+            (!pinRemoveMax || pair.userCollateralShare.isZero())) ||
+        warnings.some(warning => warning.breaking)
 
     return (
         <>
-            {/* <div className="text-3xl text-high-emphesis mt-6 mb-4">Repay {pair.asset.symbol}</div>
+            <div className="text-3xl text-high-emphesis mt-6 mb-4">Repay {pair.asset.symbol}</div>
 
             <SmartNumberInput
                 color="pink"
@@ -218,6 +218,7 @@ export default function Repay({ pair }: RepayProps) {
                 max={balance}
                 pinMax={pinRepayMax}
                 setPinMax={setPinRepayMax}
+                showMax
             />
 
             <SmartNumberInput
@@ -232,6 +233,10 @@ export default function Repay({ pair }: RepayProps) {
                 max={nextMaxRemoveCollateral}
                 pinMax={pinRemoveMax}
                 setPinMax={setPinRemoveMax}
+                showMax={
+                    pair.currentUserBorrowAmount.value.eq(displayRepayValue.toBigNumber(pair.asset.decimals)) ||
+                    pair.currentUserBorrowAmount.value.eq(0)
+                }
             />
 
             <WarningsView warnings={warnings}></WarningsView>
@@ -269,7 +274,7 @@ export default function Repay({ pair }: RepayProps) {
                         </Button>
                     </TokenApproveButton>
                 )}
-            /> */}
+            />
         </>
     )
 }
