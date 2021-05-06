204c7f42fa37c6661417d300f645bce2c1f360db
webeli <pe_lias@msn.com>
2021-05-06 20:14:40 +0200

    chore: removed useV1ExchangeAddress

--- src/data/V1.ts
+++ src/data/V1.ts
@@ -22,26 +22,3 @@ export function useV1ExchangeAddress(tokenAddress?: string): string | undefined 
     const inputs = useMemo(() => [tokenAddress], [tokenAddress])
     return useSingleCallResult(contract, 'getExchange', inputs)?.result?.[0]
 }
-
-export class MockV1Pair extends Pair {
-    constructor(etherAmount: BigintIsh, tokenAmount: TokenAmount) {
-        super(tokenAmount, new TokenAmount(WETH[tokenAmount.token.chainId], etherAmount))
-    }
-}
-
-function useMockV1Pair(inputCurrency?: Currency): MockV1Pair | undefined {
-    const token = inputCurrency instanceof Token ? inputCurrency : undefined
-
-    const isWETH = Boolean(token && token.equals(WETH[token.chainId]))
-    const v1PairAddress = useV1ExchangeAddress(isWETH ? undefined : token?.address)
-    const tokenBalance = useTokenBalance(v1PairAddress, token)
-    const ETHBalance = useETHBalances([v1PairAddress])[v1PairAddress ?? '']
-
-    return useMemo(
-        () =>
-            token && tokenBalance && ETHBalance && inputCurrency
-                ? new MockV1Pair(ETHBalance.raw, tokenBalance)
-                : undefined,
-        [ETHBalance, inputCurrency, token, tokenBalance]
-    )
-}
