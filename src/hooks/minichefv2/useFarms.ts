f61c434e7ada42b73e568054b88b2a15f1147a04
omakasebar <omakasebar@gmail.com>
2021-05-07 17:56:34 -0500

    chore: reduce logging

--- src/hooks/minichefv2/useFarms.ts
+++ src/hooks/minichefv2/useFarms.ts
@@ -60,7 +60,7 @@ const useFarms = () => {
         const farms = pools
             .filter((pool: any) => {
                 //console.log(KASHI_PAIRS.includes(Number(pool.id)), pool, Number(pool.id))
-                console.log(pool.id, Number(pool.miniChef.totalAllocPoint) > 0)
+                //console.log(pool.id, Number(pool.miniChef.totalAllocPoint) > 0)
                 return (
                     !POOL_DENY.includes(pool?.id) &&
                     pairs.find((pair: any) => pair?.id === pool?.pair) &&
@@ -82,14 +82,14 @@ const useFarms = () => {
                 const rewardPerSecond = ((pool.allocPoint / totalAllocPoint) * pool.miniChef.sushiPerSecond) / 1e18
                 const rewardPerDay = rewardPerSecond * 86400
 
-                console.log('pool:', pool.allocPoint, totalAllocPoint, pool.miniChef.sushiPerSecond)
+                //console.log('pool:', pool.allocPoint, totalAllocPoint, pool.miniChef.sushiPerSecond)
 
                 const secondaryRewardPerSecond =
                     ((pool.allocPoint / totalAllocPoint) * pool.rewarder.rewardPerSecond) / 1e18
                 const secondaryRewardPerDay = secondaryRewardPerSecond * 86400
 
                 // const secondaryRewardPerSecond = pool.rewarder.rewardPerSecond / 1e18
-                console.log('rewardsPerDay:', rewardPerDay * 10, secondaryRewardPerDay * 10)
+                //console.log('rewardsPerDay:', rewardPerDay * 10, secondaryRewardPerDay * 10)
 
                 const roiPerSecond = (rewardPerSecond * 2 * sushiPrice) / balanceUSD // *2 with matic rewards
                 const roiPerHour = roiPerSecond * 3600
