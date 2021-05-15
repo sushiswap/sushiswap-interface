e94e53582e56f173a49ad0a39299330693063027
omakasebar <omakasebar@gmail.com>
2021-05-14 21:59:34 -0500

    style: nowrap for long token names on swap

--- src/components/CurrencyInputPanel/index.tsx
+++ src/components/CurrencyInputPanel/index.tsx
@@ -213,7 +213,7 @@ export default function CurrencyInputPanel({
                                     margin={true}
                                 />
                             ) : currency ? (
-                                <div className="flex-1">
+                                <div className="flex">
                                     <CurrencyLogo currency={currency} size={'54px'} />
                                 </div>
                             ) : (
@@ -228,7 +228,7 @@ export default function CurrencyInputPanel({
                                     {pair?.token0.symbol}:{pair?.token1.symbol}
                                 </StyledTokenName>
                             ) : (
-                                <div className="flex flex-1 flex-col items-start justify-center mx-3.5">
+                                <div className="flex flex-1 flex-col items-start justify-center w-full text-left mx-3.5 overflow-x-hidden">
                                     {label && (
                                         <div className="text-xs text-secondary font-medium whitespace-nowrap">
                                             {label}
@@ -239,7 +239,7 @@ export default function CurrencyInputPanel({
                                             className="token-symbol-container"
                                             active={Boolean(currency && currency.symbol)}
                                         > */}
-                                        <div className="text-lg md:text-2xl font-bold">
+                                        <div className="text-lg md:text-2xl font-bold whitespace-nowrap">
                                             {(currency && currency.symbol && currency.symbol.length > 20
                                                 ? currency.symbol.slice(0, 4) +
                                                   '...' +
