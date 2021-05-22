24c549ea67f7ac6139284cb1fcaf4841c0e87a49
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-05-22 18:56:19 -0400

    Update BalanceCard.tsx

--- src/pages/Meowshi/BalanceCard.tsx
+++ src/pages/Meowshi/BalanceCard.tsx
@@ -1,12 +1,13 @@
 import { BalanceProps } from '../../hooks/useTokenBalance'
-import React from 'react'
+import React, { useContext } from 'react'
 import MeowshiImage from '../../assets/images/meowshi.png'
-import SushiImage from '../../assets/images/sushi.png'
-import XSushiImage from '../../assets/images/xsushi.png'
 import { formatFromBalance } from '../../utils'
 import { t } from '@lingui/macro'
 import { useActiveWeb3React } from '../../hooks/useActiveWeb3React'
 import { useLingui } from '@lingui/react'
+import { useDarkModeManager } from '../../state/user/hooks'
+import styled, { ThemeContext } from 'styled-components'
+import { ExternalLink, TYPE } from '../../theme'
 
 interface BalanceCardProps {
     sushiEarnings?: number
@@ -17,21 +18,16 @@ interface BalanceCardProps {
 }
 
 export default function BalanceCard({
-    nyanBalance,
-    xSushiBalance,
-    sushiBalance,
-    sushiEarnings = 0,
-    weightedApr = 0
+    nyanBalance
 }: BalanceCardProps) {
     const { i18n } = useLingui()
     const { account } = useActiveWeb3React()
+    const darkMode = useDarkModeManager()
+    const theme = useContext(ThemeContext)
     return (
-      <div className="flex flex-col w-full bg-dark-900 rounded px-4 md:px-8 pt-6 pb-5 md:pt-7 md:pb-9">
+      <div className="flex flex-col w-full bg-dark-400 rounded px-4 md:px-8 pt-6 pb-5 md:pt-7 md:pb-9">
           <div className="flex flex-wrap">
               <div className="flex flex-col flex-grow md:mb-14">
-                  <p className="mb-3 text-lg font-bold md:text-h5 md:font-medium text-high-emphesis">
-                      {i18n._(t`NYAN Balance`)}
-                  </p>
                   <div className="flex items-center">
                       <img className="w-10 md:w-16 -ml-1 mr-1 md:mr-2 -mb-1.5" src={MeowshiImage} alt="meowshi" />
                       <div className="flex flex-col justify-center">
@@ -39,75 +35,19 @@ export default function BalanceCard({
                               {formatFromBalance(nyanBalance.value)}
                           </p>
                           <p className="text-caption2 md:text-caption text-primary">NYAN</p>
+                          <ExternalLink
+                                   style={{ color: `${darkMode ? 'white' : 'black'}`, textDecoration: 'underline' }}
+                                   target="_blank"
+                                   href="https://etherscan.io/address/0xEb8B45EB9084D05b25B045Ff8fE4d18fb1248B38#code"
+                               >
+                                   <TYPE.white fontSize={14} color={theme.text1}>
+                                       {i18n._(t`Read the contract`)}
+                                   </TYPE.white>
+                               </ExternalLink>
                       </div>
                   </div>
               </div>
-
-        <div className="flex flex-col w-full bg-dark-900 rounded px-4 md:px-8 pt-6 pb-5 md:pt-7 md:pb-9">
-            <div className="flex flex-wrap">
-                <div className="flex flex-col flex-grow md:mb-14">
-                    <p className="mb-3 text-lg font-bold md:text-h5 md:font-medium text-high-emphesis">
-                        {i18n._(t`xSUSHI Balance`)}
-                    </p>
-                    <div className="flex items-center">
-                        <img className="w-10 md:w-16 -ml-1 mr-1 md:mr-2 -mb-1.5" src={XSushiImage} alt="sushi" />
-                        <div className="flex flex-col justify-center">
-                            <p className="text-caption2 md:text-lg font-bold text-high-emphesis">
-                                {formatFromBalance(xSushiBalance.value)}
-                            </p>
-                            <p className="text-caption2 md:text-caption text-primary">xSUSHI</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="flex flex-col flex-grow">
-                    <div className="flex flex-nowrap mb-3 ml-8 md:ml-0">
-                        <p className="text-lg font-bold md:text-h5 md:font-medium text-high-emphesis">
-                            {i18n._(t`Unstaked`)}
-                        </p>
-                        {/* <img className="cursor-pointer ml-2 w-4" src={MoreInfoSymbol} alt={'more info'} /> */}
-                    </div>
-                    <div className="flex items-center ml-8 md:ml-0">
-                        <img className="w-10 md:w-16 -ml-1 mr-1 md:mr-2 -mb-1.5" src={SushiImage} alt="sushi" />
-                        <div className="flex flex-col justify-center">
-                            <p className="text-caption2 md:text-lg font-bold text-high-emphesis">
-                                {formatFromBalance(sushiBalance.value)}
-                                {/* {sushiEarnings.toPrecision(7)} */}
-                            </p>
-                            <p className="text-caption2 md:text-caption text-primary">SUSHI</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="flex flex-col w-full mt-7 mb-4 md:mb-0">
-                    {/* <div className="flex justify-between items-center">
-                        <div className="flex flex-nowrap items-center flex-1">
-                            <p className="text-caption md:text-lg font-bold text-high-emphesis">Weighted APR</p>
-                            <img className="cursor-pointer ml-2 w-4" src={MoreInfoSymbol} alt={'more info'} />
-                        </div>
-                        <div className="flex flex-1 md:flex-initial">
-                            <p className="text-caption text-primary ml-5 md:ml-0">{`${weightedApr}%`}</p>
-                        </div>
-                    </div> */}
-                    {account && (
-                        <a
-                            href={`https://analytics.sushi.com/users/${account}`}
-                            target="_blank"
-                            rel="noreferrer noopener"
-                            className={`
-                                flex flex-grow justify-center items-center
-                                h-14 mt-6 rounded
-                                bg-dark-700 text-high-emphesis
-                                focus:outline-none focus:ring hover:bg-opacity-80
-                                text-caption2 font-bold cursor-pointer
-                            `}
-                        >
-                            {i18n._(t`Your SushiBar Stats`)}
-                        </a>
-                    )}
-                </div>
             </div>
         </div>
-      </div></div>
     )
 }
