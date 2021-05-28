69525bed9653f352858e15861d89f34ff9b19b04
Matthew Lilley <hello@matthewlilley.com>
2021-05-28 04:36:48 +0100

    feat: covalent service hooks

--- src/fetchers/covalent/index.ts
+++ src/fetchers/covalent/index.ts
@@ -3,19 +3,31 @@ import { ChainId } from '@sushiswap/sdk'
 // CLASS A
 
 export const getTokenBalances = (chainId = ChainId.MAINNET, address) =>
-    fetch(`https://api.covalenthq.com/v1/${chainId}/address/${address}/balances_v2/`)
+    fetch(
+        `https://api.covalenthq.com/v1/${chainId}/address/${address}/balances_v2/`
+    )
 
 export const getPortfolio = (chainId = ChainId.MAINNET, address) =>
-    fetch(`https://api.covalenthq.com/v1/${chainId}/address/${address}/portfolio_v2/`)
+    fetch(
+        `https://api.covalenthq.com/v1/${chainId}/address/${address}/portfolio_v2/`
+    )
 
 export const getTransfers = (chainId = ChainId.MAINNET, address) =>
-    fetch(`https://api.covalenthq.com/v1/${chainId}/address/${address}/transfers_v2/`)
+    fetch(
+        `https://api.covalenthq.com/v1/${chainId}/address/${address}/transfers_v2/`
+    )
 
 export const getBlock = (chainId = ChainId.MAINNET, blockHeight) =>
     fetch(`https://api.covalenthq.com/v1/${chainId}/block_v2/${blockHeight}/`)
 
-export const getBlockHeights = (chainId = ChainId.MAINNET, startDate, endDate) =>
-    fetch(`https://api.covalenthq.com/v1/${chainId}/block_v2/${startDate}/${endDate}`)
+export const getBlockHeights = (
+    chainId = ChainId.MAINNET,
+    startDate,
+    endDate
+) =>
+    fetch(
+        `https://api.covalenthq.com/v1/${chainId}/block_v2/${startDate}/${endDate}/`
+    )
 
 export const getLogs = (chainId = ChainId.MAINNET, address) =>
     fetch(`https://api.covalenthq.com/v1/${chainId}/events/address/${address}/`)
@@ -24,19 +36,33 @@ export const getLogsForTopic = (chainId = ChainId.MAINNET, topic) =>
     fetch(`https://api.covalenthq.com/v1/${chainId}/events/topics/${topic}/`)
 
 export const getNftMetadata = (chainId = ChainId.MAINNET, address, tokenId) =>
-    fetch(`https://api.covalenthq.com/v1/${chainId}/tokens/${address}/nft_metadata/${tokenId}`)
+    fetch(
+        `https://api.covalenthq.com/v1/${chainId}/tokens/${address}/nft_metadata/${tokenId}/`
+    )
 
 export const getNftTokenIds = (chainId = ChainId.MAINNET, address) =>
-    fetch(`https://api.covalenthq.com/v1/${chainId}/tokens/${address}/nft_token_ids/`)
+    fetch(
+        `https://api.covalenthq.com/v1/${chainId}/tokens/${address}/nft_token_ids/`
+    )
 
-export const getNftTransactions = (chainId = ChainId.MAINNET, address, tokenId) =>
-    fetch(`https://api.covalenthq.com/v1/${chainId}/tokens/${address}/nft_transactions/${tokenId}`)
+export const getNftTransactions = (
+    chainId = ChainId.MAINNET,
+    address,
+    tokenId
+) =>
+    fetch(
+        `https://api.covalenthq.com/v1/${chainId}/tokens/${address}/nft_transactions/${tokenId}/`
+    )
 
 export const getHoldersChanges = (chainId = ChainId.MAINNET, address) =>
-    fetch(`https://api.covalenthq.com/v1/${chainId}/tokens/${address}/token_holders_changes/`)
+    fetch(
+        `https://api.covalenthq.com/v1/${chainId}/tokens/${address}/token_holders_changes/`
+    )
 
 export const getTokenHolders = (chainId = ChainId.MAINNET, address) =>
-    fetch(`https://api.covalenthq.com/v1/${chainId}/tokens/${address}/token_holders/`)
+    fetch(
+        `https://api.covalenthq.com/v1/${chainId}/tokens/${address}/token_holders/`
+    )
 
 export const getTokenMetadata = (chainId = ChainId.MAINNET, id) =>
     fetch(`https://api.covalenthq.com/v1/${chainId}/tokens/tokenlists/${id}/`)
@@ -44,8 +70,9 @@ export const getTokenMetadata = (chainId = ChainId.MAINNET, id) =>
 export const getTransaction = (chainId = ChainId.MAINNET, txHash) =>
     fetch(`https://api.covalenthq.com/v1/${chainId}/trasaction_v2/${txHash}/`)
 
-export const getChains = () => fetch(`https://api.covalenthq.com/v1/chains`)
+export const getChains = () => fetch(`https://api.covalenthq.com/v1/chains/`)
 
-export const getChainStatus = () => fetch(`https://api.covalenthq.com/v1/chains/status`)
+export const getChainsStatus = () =>
+    fetch(`https://api.covalenthq.com/v1/chains/status/`)
 
 // CLASS B
