a9554f1ccf911b7a1dd97c7e45245c8407ec2ad8
Matthew Lilley <hello@matthewlilley.com>
2021-05-28 07:52:40 +0100

    chore: remove custom distDir

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
 
 module.exports = withPWA({
-  distDir: 'build',
   pwa: {
     dest: 'public',
     runtimeCaching,
