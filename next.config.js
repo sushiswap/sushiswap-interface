447ab550c97978c448c2de4e2dfd158da9dfc463
Matthew Lilley <hello@matthewlilley.com>
2021-05-28 06:33:23 +0100

    chore: set dist dir to emulate CRA for time being

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
 
 module.exports = withPWA({
+  distDir: 'build',
   pwa: {
     dest: 'public',
     runtimeCaching,
