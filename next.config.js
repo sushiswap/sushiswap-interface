4fcb9669ab075b324f1d9abf3ca856b38234a717
Matthew Lilley <hello@matthewlilley.com>
2021-05-28 10:23:43 +0100

    feat: add bundle analysis

--- next.config.js
+++ next.config.js
@@ -1,29 +1,39 @@
 const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
 
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    runtimeCaching,
-    disable: process.env.NODE_ENV === 'development',
-  },
-  future: {
-    webpack5: true
-  },
-  images: {
-    domains: ['raw.githubusercontent.com', 'ftmscan.com', 'cloudflare-ipfs.com'],
-  },
-  reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/swap',
-        permanent: true,
-      },
-    ]
-  },
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
 })
+
+module.exports = withBundleAnalyzer(
+    withPWA({
+        pwa: {
+            dest: 'public',
+            runtimeCaching,
+            disable: process.env.NODE_ENV === 'development',
+        },
+        future: {
+            webpack5: true,
+        },
+        images: {
+            domains: [
+                'raw.githubusercontent.com',
+                'ftmscan.com',
+                'cloudflare-ipfs.com',
+            ],
+        },
+        reactStrictMode: true,
+        async redirects() {
+            return [
+                {
+                    source: '/',
+                    destination: '/swap',
+                    permanent: true,
+                },
+            ]
+        },
+    })
+)
 
 // Don't delete this console log, useful to see the config in Vercel deployments
 console.log('next.config.js', JSON.stringify(module.exports, null, 2))
