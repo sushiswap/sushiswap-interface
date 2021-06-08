823540ea94de5d476d31a04bfd9d0a5e35e7787b
Matthew Lilley <hello@matthewlilley.com>
2021-06-08 15:41:10 +0100

    fix: public paths

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -51,7 +51,7 @@ export default class MyDocument extends Document {
                     <meta name="mobile-web-app-capable" content="yes" />
                     <meta
                         name="msapplication-config"
-                        content="/static/icons/browserconfig.xml"
+                        content="/icons/browserconfig.xml"
                     />
                     <meta name="msapplication-TileColor" content="#0D0415" />
                     <meta name="msapplication-tap-highlight" content="no" />
@@ -59,40 +59,40 @@ export default class MyDocument extends Document {
 
                     <link
                         rel="apple-touch-icon"
-                        href="/static/icons/touch-icon-iphone.png"
+                        href="/icons/touch-icon-iphone.png"
                     />
                     <link
                         rel="apple-touch-icon"
                         sizes="152x152"
-                        href="/static/icons/touch-icon-ipad.png"
+                        href="/icons/touch-icon-ipad.png"
                     />
                     <link
                         rel="apple-touch-icon"
                         sizes="180x180"
-                        href="/static/icons/touch-icon-iphone-retina.png"
+                        href="/icons/touch-icon-iphone-retina.png"
                     />
                     <link
                         rel="apple-touch-icon"
                         sizes="167x167"
-                        href="/static/icons/touch-icon-ipad-retina.png"
+                        href="/icons/touch-icon-ipad-retina.png"
                     />
 
                     <link
                         rel="icon"
                         type="image/png"
                         sizes="32x32"
-                        href="/static/icons/favicon-32x32.png"
+                        href="/icons/favicon-32x32.png"
                     />
                     <link
                         rel="icon"
                         type="image/png"
                         sizes="16x16"
-                        href="/static/icons/favicon-16x16.png"
+                        href="/icons/favicon-16x16.png"
                     />
-                    <link rel="manifest" href="/static/manifest.json" />
+                    <link rel="manifest" href="/manifest.json" />
                     <link
                         rel="mask-icon"
-                        href="/static/icons/safari-pinned-tab.svg"
+                        href="/icons/safari-pinned-tab.svg"
                         color="#5bbad5"
                     />
                     <link rel="shortcut icon" href="/favicon.ico" />
@@ -107,7 +107,7 @@ export default class MyDocument extends Document {
                     />
                     <meta
                         name="twitter:image"
-                        content="https://sushi.com/static/icons/android-chrome-192x192.png"
+                        content="https://sushi.com/icons/android-chrome-192x192.png"
                     />
                     <meta name="twitter:creator" content="@SushiSwap" />
                     <meta property="og:type" content="website" />
@@ -120,43 +120,43 @@ export default class MyDocument extends Document {
                     <meta property="og:url" content="https://sushi.com" />
                     <meta
                         property="og:image"
-                        content="https://sushi.com/static/icons/apple-touch-icon.png"
+                        content="https://sushi.com/icons/apple-touch-icon.png"
                     />
 
                     {/* apple splash screen images */}
                     <link
                         rel="apple-touch-startup-image"
-                        href="/static/images/apple_splash_2048.png"
+                        href="/images/apple_splash_2048.png"
                         sizes="2048x2732"
                     />
                     <link
                         rel="apple-touch-startup-image"
-                        href="/static/images/apple_splash_1668.png"
+                        href="/images/apple_splash_1668.png"
                         sizes="1668x2224"
                     />
                     <link
                         rel="apple-touch-startup-image"
-                        href="/static/images/apple_splash_1536.png"
+                        href="/images/apple_splash_1536.png"
                         sizes="1536x2048"
                     />
                     <link
                         rel="apple-touch-startup-image"
-                        href="/static/images/apple_splash_1125.png"
+                        href="/images/apple_splash_1125.png"
                         sizes="1125x2436"
                     />
                     <link
                         rel="apple-touch-startup-image"
-                        href="/static/images/apple_splash_1242.png"
+                        href="/images/apple_splash_1242.png"
                         sizes="1242x2208"
                     />
                     <link
                         rel="apple-touch-startup-image"
-                        href="/static/images/apple_splash_750.png"
+                        href="/images/apple_splash_750.png"
                         sizes="750x1334"
                     />
                     <link
                         rel="apple-touch-startup-image"
-                        href="/static/images/apple_splash_640.png"
+                        href="/images/apple_splash_640.png"
                         sizes="640x1136"
                     />
                 </Head>
