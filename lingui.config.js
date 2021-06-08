de7f535cb4627713437e1ca2ad74b90d61b063e3
Matthew Lilley <hello@matthewlilley.com>
2021-06-08 14:59:59 +0100

    move locale back into src temporarily

--- lingui.config.js
+++ lingui.config.js
@@ -2,18 +2,31 @@ module.exports = {
     catalogs: [
         {
             path: '<rootDir>/locale/{locale}/catalog',
-            include: ['<rootDir>/src'],
-            exclude: ['**/node_modules/**']
-        }
+            include: ['<rootDir>'],
+        },
     ],
     compileNamespace: 'cjs',
     extractBabelOptions: {},
     fallbackLocales: {},
     format: 'minimal',
     sourceLocale: 'en',
-    locales: ['de', 'en', 'es-AR', 'es', 'it', 'ro', 'ru', 'vi', 'zh-CN', 'zh-TW', 'ko', 'ja', 'fr'],
+    locales: [
+        'de',
+        'en',
+        'es-AR',
+        'es',
+        'it',
+        'ro',
+        'ru',
+        'vi',
+        'zh-CN',
+        'zh-TW',
+        'ko',
+        'ja',
+        'fr',
+    ],
     orderBy: 'messageId',
     pseudoLocale: '',
-    rootDir: '.',
-    runtimeConfigModule: ['@lingui/core', 'i18n']
+    rootDir: './src',
+    runtimeConfigModule: ['@lingui/core', 'i18n'],
 }
