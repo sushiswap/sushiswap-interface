e8ce1ac5fc97585dbacc244db64601dda308ae75
Matthew Lilley <hello@matthewlilley.com>
2021-06-08 14:19:52 +0100

    feat: impersonate

--- src/hooks/useActiveWeb3React.ts
+++ src/hooks/useActiveWeb3React.ts
@@ -7,9 +7,13 @@ import { useWeb3React as useWeb3ReactCore } from '@web3-react/core'
 export function useActiveWeb3React(): Web3ReactContextInterface<Web3Provider> & {
     chainId?: ChainId
 } {
+    // replace with address to impersonate
+    const impersonate = false
     const context = useWeb3ReactCore<Web3Provider>()
     const contextNetwork = useWeb3ReactCore<Web3Provider>(NetworkContextName)
-    return context.active ? context : contextNetwork
+    return context.active
+        ? { ...context, account: impersonate || context.account }
+        : { ...contextNetwork, account: impersonate || contextNetwork.account }
 }
 
 export default useActiveWeb3React
