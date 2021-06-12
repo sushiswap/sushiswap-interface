c104cdbcd5ea31a8f6298d07381e5c79447810a3
Matthew Lilley <hello@matthewlilley.com>
2021-06-12 10:20:59 +0100

    chore: add weth base to binancne config

--- src/constants/tokens/index.ts
+++ src/constants/tokens/index.ts
@@ -6,6 +6,7 @@ export const BSC: { [key: string]: Token } = {
     USDC: new Token(ChainId.BSC, '0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d', 18, 'USDC', 'USD Coin'),
     USDT: new Token(ChainId.BSC, '0x55d398326f99059fF775485246999027B3197955', 18, 'USDT', 'Tether USD'),
     BTCB: new Token(ChainId.BSC, '0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c', 18, 'BTCB', 'Bitcoin'),
+    WETH: new Token(ChainId.BSC, '0x2170Ed0880ac9A755fd29B2688956BD959F933F8', 18, 'WETH', 'Wrapped Ether'),
 }
 
 export const FANTOM: { [key: string]: Token } = {
