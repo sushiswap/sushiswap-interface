2f341df5b3265a370dc06b838e1752d0a6e4ff81
Matthew Lilley <hello@matthewlilley.com>
2021-06-14 13:51:01 +0100

    feat: improve container

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,6 +1,12 @@
 import { classNames } from "../functions";
 
 const MAX_WIDTH = {
+  full: "max-w-full",
+  "7xl": "max-w-7xl",
+  "6xl": "max-w-6xl",
+  "5xl": "max-w-5xl",
+  "4xl": "max-w-4xl",
+  "3xl": "max-w-3xl",
   "2xl": "max-w-2xl",
   xl: "max-w-xl",
   lg: "max-w-lg",
