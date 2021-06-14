a55d39550b921015abefccf1a4da8e12166442ad
Matthew Lilley <hello@matthewlilley.com>
2021-06-14 13:52:25 +0100

    refactor: cleanup of pools page

--- src/pages/pool.tsx
+++ src/pages/pool.tsx
@@ -7,13 +7,17 @@ import { BIG_INT_ZERO } from "../constants";
 import Back from "../components/Back";
 import Button from "../components/Button";
 import Card from "../components/Card";
+import Container from "../components/Container";
 import { Currency } from "@sushiswap/sdk";
 import Dots from "../components/Dots";
 import Empty from "../components/Empty";
+import ExternalLink from "../components/ExternalLink";
 import FullPositionCard from "../components/PositionCard";
 import Head from "next/head";
 import Layout from "../layouts/DefaultLayout";
 import Link from "next/link";
+import Typography from "../components/Typography";
+import Web3Connect from "../components/Web3Connect";
 import { t } from "@lingui/macro";
 import { useActiveWeb3React } from "../hooks/useActiveWeb3React";
 import { useETHBalances } from "../state/wallet/hooks";
@@ -85,20 +89,86 @@ export default function Pool() {
           content="SushiSwap liquidity pools are markets for trades between the two tokens, you can provide these tokens and become a liquidity provider to earn 0.25% of fees from trades."
         />
       </Head>
-      <Back />
-      <div className="w-full max-w-2xl p-4 rounded bg-dark-900">
-        <Alert
-          title={i18n._(t`Liquidity Provider Rewards`)}
-          message={t`Liquidity providers earn a 0.25% fee on all trades proportional to their share of
+
+      <Container maxWidth="3xl">
+        <div className="p-4 mb-3 space-y-3">
+          <Back />
+
+          <Typography component="h1" variant="h5">
+            {i18n._(t`Your Liquidity Positions`)}
+          </Typography>
+        </div>
+
+        <div className="p-4 space-y-4 rounded bg-dark-900">
+          <Alert
+            title={i18n._(t`Liquidity Provider Rewards`)}
+            message={t`Liquidity providers earn a 0.25% fee on all trades proportional to their share of
                         the pool. Fees are added to the pool, accrue in real time and can be claimed by
                         withdrawing your liquidity`}
-          type="information"
-        />
-        <div className="flex items-center justify-between mt-6 mb-6">
-          <div className="font-medium text-h5 text-high-emphesis">
-            {i18n._(t`Your Liquidity Positions`)}
+            type="information"
+          />
+
+          <div className="grid grid-flow-row gap-3">
+            {!account ? (
+              <Web3Connect size="large" />
+            ) : v2IsLoading ? (
+              <Empty>
+                <Dots>{i18n._(t`Loading`)}</Dots>
+              </Empty>
+            ) : allV2PairsWithLiquidity?.length > 0 ? (
+              <>
+                <div className="flex items-center justify-center">
+                  <ExternalLink
+                    href={"https://analytics.sushi.com/user/" + account}
+                  >
+                    Account analytics and accrued fees <span> ↗</span>
+                  </ExternalLink>
+                </div>
+                {allV2PairsWithLiquidity.map((v2Pair) => (
+                  <FullPositionCard
+                    key={v2Pair.liquidityToken.address}
+                    pair={v2Pair}
+                  />
+                ))}
+              </>
+            ) : (
+              <Empty className="text-center">
+                {i18n._(t`No liquidity found`)}
+              </Empty>
+            )}
           </div>
-          <div className="flex space-x-2 text-sm font-bold">
+          {account && (
+            <div className="grid grid-cols-2 gap-4 mt-4">
+              <Button
+                id="add-pool-button"
+                color="gradient"
+                size="large"
+                onClick={() =>
+                  router.push(
+                    `/add/${Currency.getNativeCurrencySymbol(chainId)}`
+                  )
+                }
+              >
+                {i18n._(t`Add Liquidity`)}
+              </Button>
+              <Button
+                id="create-pool-button"
+                color="gray"
+                size="large"
+                onClick={() =>
+                  router.push(
+                    `/create/${Currency.getNativeCurrencySymbol(chainId)}`
+                  )
+                }
+              >
+                {i18n._(t`Create a pair`)}
+              </Button>
+            </div>
+          )}
+        </div>
+
+        <div className="flex items-center justify-between p-4 mt-6 mb-6">
+          <div className="flex flex-col space-y-1 text-sm font-bold">
             <div>{i18n._(t`Don't see a pool you joined?`)}</div>
             <Link href="/find">
               <a
@@ -109,76 +179,23 @@ export default function Pool() {
               </a>
             </Link>
           </div>
+          {chainId &&
+            migrateFrom[chainId] &&
+            [ChainId.MAINNET, ChainId.BSC, ChainId.MATIC].includes(chainId) && (
+              <div className="flex flex-col space-y-1 text-sm font-bold text-right">
+                <div>{i18n._(t`Have ${migrateFrom[chainId]} Liquidity?`)}</div>
+                <Link href="/migrate">
+                  <a
+                    id="migrate-pool-link"
+                    className="text-blue text-opacity-80 hover:text-opacity-100"
+                  >
+                    {i18n._(t`Migrate!`)}
+                  </a>
+                </Link>
+              </div>
+            )}
         </div>
-        <div className="grid grid-flow-row gap-3">
-          {!account ? (
-            <Card>
-              <div>{i18n._(t`Connect to a wallet to view your liquidity`)}</div>
-            </Card>
-          ) : v2IsLoading ? (
-            <Empty>
-              <Dots>{i18n._(t`Loading`)}</Dots>
-            </Empty>
-          ) : allV2PairsWithLiquidity?.length > 0 ? (
-            <>
-              {/* <div className="flex items-center justify-center">
-                                <ExternalLink href={'https://analytics.sushi.com/user/' + account}>
-                                    Account analytics and accrued fees <span> ↗</span>
-                                </ExternalLink>
-                            </div> */}
-              {allV2PairsWithLiquidity.map((v2Pair) => (
-                <FullPositionCard
-                  key={v2Pair.liquidityToken.address}
-                  pair={v2Pair}
-                />
-              ))}
-            </>
-          ) : (
-            <Empty className="text-center">
-              {i18n._(t`No liquidity found`)}
-            </Empty>
-          )}
-        </div>
-        {chainId &&
-          migrateFrom[chainId] &&
-          [ChainId.MAINNET, ChainId.BSC, ChainId.MATIC].includes(chainId) && (
-            <div className="flex justify-center mt-4 space-x-2 text-sm font-bold">
-              <div>{i18n._(t`Have ${migrateFrom[chainId]} Liquidity?`)}</div>
-              <Link href="/migrate">
-                <a
-                  id="migrate-pool-link"
-                  className="text-blue text-opacity-80 hover:text-opacity-100"
-                >
-                  {i18n._(t`Migrate!`)}
-                </a>
-              </Link>
-            </div>
-          )}
-        <div className="grid grid-cols-2 gap-4 mt-4">
-          <Button
-            id="add-pool-button"
-            color="gradient"
-            size="large"
-            onClick={() =>
-              router.push(`/add/${Currency.getNativeCurrencySymbol(chainId)}`)
-            }
-          >
-            {i18n._(t`Add Liquidity`)}
-          </Button>
-          <Button
-            id="create-pool-button"
-            color="gray"
-            size="large"
-            onClick={() =>
-              router.push(
-                `/create/${Currency.getNativeCurrencySymbol(chainId)}`
-              )
-            }
-          >
-            {i18n._(t`Create a pair`)}
-          </Button>
-        </div>
-      </div>
+      </Container>
     </Layout>
   );
 }
