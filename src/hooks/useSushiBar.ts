91ba2dc9e2996f86f7fac5eff576b3a5ca2dcff3
Matthew Lilley <hello@matthewlilley.com>
2021-06-14 06:49:47 +0100

    feat: simplifiy useSushiBar

--- src/hooks/useSushiBar.ts
+++ src/hooks/useSushiBar.ts
@@ -1,51 +1,11 @@
-import { useCallback, useEffect, useState } from 'react'
-import { useSushiBarContract, useSushiContract } from '../hooks/useContract'
-
 import { CurrencyAmount } from '@sushiswap/sdk'
-import Fraction from '../entities/Fraction'
-import { ethers } from 'ethers'
-import { useActiveWeb3React } from './useActiveWeb3React'
+import { useCallback } from 'react'
+import { useSushiBarContract } from '../hooks/useContract'
 import { useTransactionAdder } from '../state/transactions/hooks'
 
-const { BigNumber } = ethers
-
 const useSushiBar = () => {
-    const { account } = useActiveWeb3React()
     const addTransaction = useTransactionAdder()
-    const sushiContract = useSushiContract()
     const barContract = useSushiBarContract()
-
-    const [allowance, setAllowance] = useState('0')
-
-    const fetchAllowance = useCallback(async () => {
-        if (account) {
-            try {
-                const allowance = await sushiContract?.allowance(account, barContract?.address)
-                const formatted = Fraction.from(BigNumber.from(allowance), BigNumber.from(10).pow(18)).toString()
-                setAllowance(formatted)
-            } catch (error) {
-                setAllowance('0')
-                throw error
-            }
-        }
-    }, [account, barContract, sushiContract])
-
-    useEffect(() => {
-        if (account && barContract && sushiContract) {
-            fetchAllowance()
-        }
-        const refreshInterval = setInterval(fetchAllowance, 10000)
-        return () => clearInterval(refreshInterval)
-    }, [account, barContract, fetchAllowance, sushiContract])
-
-    const approve = useCallback(async () => {
-        try {
-            const tx = await sushiContract?.approve(barContract?.address, ethers.constants.MaxUint256.toString())
-            return addTransaction(tx, { summary: 'Approve' })
-        } catch (e) {
-            return e
-        }
-    }, [addTransaction, barContract, sushiContract])
 
     const enter = useCallback(
         async (amount: CurrencyAmount | undefined) => {
@@ -75,7 +35,7 @@ const useSushiBar = () => {
         [addTransaction, barContract]
     )
 
-    return { allowance, approve, enter, leave }
+    return { enter, leave }
 }
 
 export default useSushiBar
