f6cb35ea3ae57077ddcdfe06cd35ae8be1e561b0
Matthew Lilley <hello@matthewlilley.com>
2021-06-16 14:29:48 +0100

    chore: celo config & fix compilation

--- src/functions/parse.ts
+++ src/functions/parse.ts
@@ -18,6 +18,10 @@ export function tryParseAmount(
   try {
     const typedValueParsed = parseUnits(value, currency.decimals).toString();
     if (typedValueParsed !== "0") {
+      // return CurrencyAmount.fromRawAmount(
+      //   currency,
+      //   JSBI.BigInt(typedValueParsed)
+      // );
       return currency instanceof Token
         ? new TokenAmount(currency, JSBI.BigInt(typedValueParsed))
         : CurrencyAmount.fromRawAmount(currency, JSBI.BigInt(typedValueParsed));
