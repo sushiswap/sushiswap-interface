758c19f16840ab5be0409b488fa0a490c4c6fa7d
Matthew Lilley <hello@matthewlilley.com>
2021-06-22 15:49:51 +0100

    chore: connector updates

--- src/connectors/Fortmatic.ts
+++ src/connectors/Fortmatic.ts
@@ -1,59 +1,53 @@
-import { ChainId } from "@sushiswap/sdk";
-import { FortmaticConnector as FortmaticConnectorCore } from "@web3-react/fortmatic-connector";
+import { ChainId } from '@sushiswap/sdk'
+import { FortmaticConnector as FortmaticConnectorCore } from '@web3-react/fortmatic-connector'
 
-export const OVERLAY_READY = "OVERLAY_READY";
+export const OVERLAY_READY = 'OVERLAY_READY'
 
-type FormaticSupportedChains = Extract<
-  ChainId,
-  ChainId.MAINNET | ChainId.ROPSTEN | ChainId.RINKEBY | ChainId.KOVAN
->;
+type FormaticSupportedChains = Extract<ChainId, ChainId.MAINNET | ChainId.ROPSTEN | ChainId.RINKEBY | ChainId.KOVAN>
 
 const CHAIN_ID_NETWORK_ARGUMENT: {
-  readonly [chainId in FormaticSupportedChains]: string | undefined;
+  readonly [chainId in FormaticSupportedChains]: string | undefined
 } = {
   [ChainId.MAINNET]: undefined,
-  [ChainId.ROPSTEN]: "ropsten",
-  [ChainId.RINKEBY]: "rinkeby",
-  [ChainId.KOVAN]: "kovan",
-};
+  [ChainId.ROPSTEN]: 'ropsten',
+  [ChainId.RINKEBY]: 'rinkeby',
+  [ChainId.KOVAN]: 'kovan',
+}
 
 export class FortmaticConnector extends FortmaticConnectorCore {
   async activate() {
     if (!this.fortmatic) {
-      const { default: Fortmatic } = await import("fortmatic");
+      const { default: Fortmatic } = await import('fortmatic')
 
-      const { apiKey, chainId } = this as any;
+      const { apiKey, chainId } = this as any
       if (chainId in CHAIN_ID_NETWORK_ARGUMENT) {
-        this.fortmatic = new Fortmatic(
-          apiKey,
-          CHAIN_ID_NETWORK_ARGUMENT[chainId as FormaticSupportedChains]
-        );
+        this.fortmatic = new Fortmatic(apiKey, CHAIN_ID_NETWORK_ARGUMENT[chainId as FormaticSupportedChains])
       } else {
-        throw new Error(`Unsupported network ID: ${chainId}`);
+        throw new Error(`Unsupported network ID: ${chainId}`)
       }
     }
 
-    const provider = this.fortmatic.getProvider();
+    const provider = this.fortmatic.getProvider()
 
     const pollForOverlayReady = new Promise<void>((resolve) => {
       const interval = setInterval(() => {
         if (provider.overlayReady) {
-          clearInterval(interval);
-          this.emit(OVERLAY_READY);
-          resolve();
+          clearInterval(interval)
+          this.emit(OVERLAY_READY)
+          resolve()
         }
-      }, 200);
-    });
+      }, 200)
+    })
 
     const [account] = await Promise.all([
       provider.enable().then((accounts: string[]) => accounts[0]),
       pollForOverlayReady,
-    ]);
+    ])
 
     return {
       provider: this.fortmatic.getProvider(),
       chainId: (this as any).chainId,
       account,
-    };
+    }
   }
 }
--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -1,63 +1,52 @@
-import {
-  ExternalProvider,
-  JsonRpcFetchFunc,
-  Web3Provider,
-} from "@ethersproject/providers";
+import { ExternalProvider, JsonRpcFetchFunc, Web3Provider } from '@ethersproject/providers'
 
-import { BscConnector } from "@binance-chain/bsc-connector";
-import { ChainId } from "@sushiswap/sdk";
-import { FortmaticConnector } from "./Fortmatic";
-import { InjectedConnector } from "@web3-react/injected-connector";
-import { LatticeConnector } from "@web3-react/lattice-connector";
-import { NetworkConnector } from "./NetworkConnector";
-import { PortisConnector } from "@web3-react/portis-connector";
-import { TorusConnector } from "@web3-react/torus-connector";
-import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
-import { WalletLinkConnector } from "@web3-react/walletlink-connector";
+import { BscConnector } from '@binance-chain/bsc-connector'
+import { ChainId } from '@sushiswap/sdk'
+import { FortmaticConnector } from './Fortmatic'
+import { InjectedConnector } from '@web3-react/injected-connector'
+import { LatticeConnector } from '@web3-react/lattice-connector'
+import { NetworkConnector } from './NetworkConnector'
+import { PortisConnector } from '@web3-react/portis-connector'
+import { TorusConnector } from '@web3-react/torus-connector'
+import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
+import { WalletLinkConnector } from '@web3-react/walletlink-connector'
 
 const RPC = {
-  [ChainId.MAINNET]:
-    "https://eth-mainnet.alchemyapi.io/v2/q1gSNoSMEzJms47Qn93f9-9Xg5clkmEC",
-  [ChainId.ROPSTEN]:
-    "https://eth-ropsten.alchemyapi.io/v2/cidKix2Xr-snU3f6f6Zjq_rYdalKKHmW",
-  [ChainId.RINKEBY]:
-    "https://eth-rinkeby.alchemyapi.io/v2/XVLwDlhGP6ApBXFz_lfv0aZ6VmurWhYD",
-  [ChainId.GÖRLI]:
-    "https://eth-goerli.alchemyapi.io/v2/Dkk5d02QjttYEoGmhZnJG37rKt8Yl3Im",
-  [ChainId.KOVAN]:
-    "https://eth-kovan.alchemyapi.io/v2/6OVAa_B_rypWWl9HqtiYK26IRxXiYqER",
-  [ChainId.FANTOM]: "https://rpcapi.fantom.network",
-  [ChainId.FANTOM_TESTNET]: "https://rpc.testnet.fantom.network",
-  [ChainId.MATIC]: "https://rpc-mainnet.maticvigil.com",
+  [ChainId.MAINNET]: 'https://eth-mainnet.alchemyapi.io/v2/q1gSNoSMEzJms47Qn93f9-9Xg5clkmEC',
+  [ChainId.ROPSTEN]: 'https://eth-ropsten.alchemyapi.io/v2/cidKix2Xr-snU3f6f6Zjq_rYdalKKHmW',
+  [ChainId.RINKEBY]: 'https://eth-rinkeby.alchemyapi.io/v2/XVLwDlhGP6ApBXFz_lfv0aZ6VmurWhYD',
+  [ChainId.GÖRLI]: 'https://eth-goerli.alchemyapi.io/v2/Dkk5d02QjttYEoGmhZnJG37rKt8Yl3Im',
+  [ChainId.KOVAN]: 'https://eth-kovan.alchemyapi.io/v2/6OVAa_B_rypWWl9HqtiYK26IRxXiYqER',
+  [ChainId.FANTOM]: 'https://rpcapi.fantom.network',
+  [ChainId.FANTOM_TESTNET]: 'https://rpc.testnet.fantom.network',
+  [ChainId.MATIC]: 'https://rpc-mainnet.maticvigil.com',
   // [ChainId.MATIC]:
   //     'https://apis.ankr.com/e22bfa5f5a124b9aa1f911b742f6adfe/c06bb163c3c2a10a4028959f4d82836d/polygon/full/main',
-  [ChainId.MATIC_TESTNET]: "https://rpc-mumbai.matic.today",
-  [ChainId.XDAI]: "https://rpc.xdaichain.com",
-  [ChainId.BSC]: "https://bsc-dataseed.binance.org/",
-  [ChainId.BSC_TESTNET]: "https://data-seed-prebsc-2-s3.binance.org:8545",
-  [ChainId.MOONBEAM_TESTNET]: "https://rpc.testnet.moonbeam.network",
-  [ChainId.AVALANCHE]: "https://api.avax.network/ext/bc/C/rpc",
-  [ChainId.AVALANCHE_TESTNET]: "https://api.avax-test.network/ext/bc/C/rpc",
-  [ChainId.HECO]: "https://http-mainnet.hecochain.com",
-  [ChainId.HECO_TESTNET]: "https://http-testnet.hecochain.com",
-  [ChainId.HARMONY]: "https://explorer.harmony.one",
-  [ChainId.HARMONY_TESTNET]: "https://explorer.pops.one",
-  [ChainId.OKEX]: "https://exchainrpc.okex.org",
-  [ChainId.OKEX_TESTNET]: "https://exchaintestrpc.okex.org",
-  [ChainId.ARBITRUM]: "https://arb1.arbitrum.io/rpc",
-};
+  [ChainId.MATIC_TESTNET]: 'https://rpc-mumbai.matic.today',
+  [ChainId.XDAI]: 'https://rpc.xdaichain.com',
+  [ChainId.BSC]: 'https://bsc-dataseed.binance.org/',
+  [ChainId.BSC_TESTNET]: 'https://data-seed-prebsc-2-s3.binance.org:8545',
+  [ChainId.MOONBEAM_TESTNET]: 'https://rpc.testnet.moonbeam.network',
+  [ChainId.AVALANCHE]: 'https://api.avax.network/ext/bc/C/rpc',
+  [ChainId.AVALANCHE_TESTNET]: 'https://api.avax-test.network/ext/bc/C/rpc',
+  [ChainId.HECO]: 'https://http-mainnet.hecochain.com',
+  [ChainId.HECO_TESTNET]: 'https://http-testnet.hecochain.com',
+  [ChainId.HARMONY]: 'https://explorer.harmony.one',
+  [ChainId.HARMONY_TESTNET]: 'https://explorer.pops.one',
+  [ChainId.OKEX]: 'https://exchainrpc.okex.org',
+  [ChainId.OKEX_TESTNET]: 'https://exchaintestrpc.okex.org',
+  [ChainId.ARBITRUM]: 'https://arb1.arbitrum.io/rpc',
+}
 
 export const network = new NetworkConnector({
   defaultChainId: 1,
   urls: RPC,
-});
+})
 
-let networkLibrary: Web3Provider | undefined;
+let networkLibrary: Web3Provider | undefined
 
 export function getNetworkLibrary(): Web3Provider {
-  return (networkLibrary =
-    networkLibrary ??
-    new Web3Provider(network.provider as ExternalProvider | JsonRpcFetchFunc));
+  return (networkLibrary = networkLibrary ?? new Web3Provider(network.provider as ExternalProvider | JsonRpcFetchFunc))
 }
 
 export const injected = new InjectedConnector({
@@ -86,46 +75,45 @@ export const injected = new InjectedConnector({
     42161, // arbitrum
     42220, // celo
   ],
-});
+})
 
 // mainnet only
 export const walletconnect = new WalletConnectConnector({
   rpc: RPC,
-  bridge: "https://bridge.walletconnect.org",
+  bridge: 'https://bridge.walletconnect.org',
   qrcode: true,
   pollingInterval: 15000,
-});
+})
 
 // mainnet only
 export const lattice = new LatticeConnector({
   chainId: 1,
   url: RPC[ChainId.MAINNET],
-  appName: "SushiSwap",
-});
+  appName: 'SushiSwap',
+})
 
 // mainnet only
 export const fortmatic = new FortmaticConnector({
-  apiKey: process.env.REACT_APP_FORTMATIC_API_KEY ?? "",
+  apiKey: process.env.REACT_APP_FORTMATIC_API_KEY ?? '',
   chainId: 1,
-});
+})
 
 // mainnet only
 export const portis = new PortisConnector({
-  dAppId: process.env.REACT_APP_PORTIS_ID ?? "",
+  dAppId: process.env.REACT_APP_PORTIS_ID ?? '',
   networks: [1],
-});
+})
 
 // mainnet only
 export const walletlink = new WalletLinkConnector({
   url: RPC[ChainId.MAINNET],
-  appName: "SushiSwap",
-  appLogoUrl:
-    "https://raw.githubusercontent.com/sushiswap/art/master/sushi/logo-256x256.png",
-});
+  appName: 'SushiSwap',
+  appLogoUrl: 'https://raw.githubusercontent.com/sushiswap/art/master/sushi/logo-256x256.png',
+})
 
 // mainnet only
 export const torus = new TorusConnector({
   chainId: 1,
-});
+})
 
-export const bsc = new BscConnector({ supportedChainIds: [56] });
+export const bsc = new BscConnector({ supportedChainIds: [56] })
