ea5304d10e0767a59cde49b92b511948189308c0
Matthew Lilley <hello@matthewlilley.com>
2021-06-22 22:40:35 +0100

    chore: cleanup and use both pair types as before

--- src/hooks/useZapperFarms.ts
+++ src/hooks/useZapperFarms.ts
@@ -16,61 +16,106 @@ const useZapperFarms = () => {
   const boringHelperContract = useBoringHelperContract()
 
   const fetchAllFarms = useCallback(async () => {
-    const [pools, liquidityPositions, averageBlockTime, sushiPrice, pairs, masterChef] = await Promise.all([
-      sushiData.masterchef.pools(),
-      sushiData.exchange_v1.userPositions({
-        user_address: '0xc2edad668740f1aa35e4d8f227fb8e17dca888cd',
-      }),
-      sushiData.utils.getAverageBlockTime(),
-      sushiData.sushi.priceUSD(),
-      sushiData.exchange.pairs(),
-      sushiData.masterchef.info(),
-    ])
+    let [pools, liquidityPositions, averageBlockTime, sushiPrice, kashiPairs, sushiPairs, masterChef] =
+      await Promise.all([
+        sushiData.masterchef.pools(),
+        sushiData.exchange_v1.userPositions({
+          user_address: '0xc2edad668740f1aa35e4d8f227fb8e17dca888cd',
+        }),
+        sushiData.utils.getAverageBlockTime(), // results[2]
+        sushiData.sushi.priceUSD(), // results[3]
+        sushiData.bentobox.kashiStakedInfo(), // results[4]
+        sushiData.exchange.pairs(), // results[5]
+        sushiData.masterchef.info(), // results[6]
+      ])
+
+    const pairAddresses = pools
+      .map((pool) => {
+        return pool.pair
+      })
+      .sort()
+
+    kashiPairs = kashiPairs.filter((result) => result !== undefined) // filter out undefined (not in onsen) from all kashiPairs
+    sushiPairs = sushiPairs.filter((pair) => pairAddresses.includes(pair.id))
+
+    const KASHI_PAIRS = range(190, 230, 1) // kashiPair pids 189-229
+
     const farms = pools
       .filter((pool: any) => {
-        return !POOL_DENY.includes(pool?.id) && pairs.find((pair) => pair?.id === pool?.pair)
+        // console.log(KASHI_PAIRS.includes(Number(pool.id)), pool, Number(pool.id))
+        return (
+          !POOL_DENY.includes(pool?.id) &&
+          (sushiPairs.find((pair: any) => pair?.id === pool?.pair) || KASHI_PAIRS.includes(Number(pool?.id)))
+        )
       })
       .map((pool) => {
-        const pair = pairs.find((pair) => pair.id === pool.pair)
-        const liquidityPosition = liquidityPositions.find(
-          (liquidityPosition: any) => liquidityPosition.pair.id === pair.id
-        )
-        const blocksPerHour = 3600 / Number(averageBlockTime)
-        const balance = Number(pool.slpBalance / 1e18) > 0 ? Number(pool.slpBalance / 1e18) : 0.1
-        const totalSupply = pair.totalSupply > 0 ? pair.totalSupply : 0.1
-        const reserveUSD = pair.reserveUSD > 0 ? pair.reserveUSD : 0.1
-        const balanceUSD = (balance / Number(totalSupply)) * Number(reserveUSD)
-        const rewardPerBlock = ((pool.allocPoint / masterChef.totalAllocPoint) * masterChef.sushiPerBlock) / 1e18
-        const roiPerBlock = (rewardPerBlock * sushiPrice) / balanceUSD
-        const roiPerHour = roiPerBlock * blocksPerHour
-        const roiPerDay = roiPerHour * 24
-        const roiPerMonth = roiPerDay * 30
-        const roiPerYear = roiPerMonth * 12
-        return {
-          ...pool,
-          type: 'SLP',
-          symbol: pair.token0.symbol + '-' + pair.token1.symbol,
-          name: pair.token0.name + ' ' + pair.token1.name,
-          pid: Number(pool.id),
-          pairAddress: pair.id,
-          slpBalance: pool.slpBalance,
-          liquidityPair: pair,
-          roiPerBlock,
-          roiPerHour,
-          roiPerDay,
-          roiPerMonth,
-          roiPerYear,
-          rewardPerThousand: 1 * roiPerDay * (1000 / sushiPrice),
-          tvl: liquidityPosition?.liquidityTokenBalance
-            ? (pair.reserveUSD / pair.totalSupply) * liquidityPosition.liquidityTokenBalance
-            : 0.1,
+        if (KASHI_PAIRS.includes(Number(pool?.id))) {
+          const pair = kashiPairs.find((pair) => pair?.id === pool?.pair)
+          // console.log('kpair:', pair, pool)
+          return {
+            ...pool,
+            ...pair,
+            type: 'KMP',
+            pid: Number(pool.id),
+            pairAddress: pair?.id,
+            pairSymbol: pair?.symbol,
+            liquidityPair: {
+              token0: {
+                id: pair?.collateral,
+                symbol: pair?.collateralSymbol,
+                decimals: pair?.collateralDecimals,
+              },
+              token1: {
+                id: pair?.asset,
+                symbol: pair?.assetSymbol,
+                decimals: pair?.assetDecimals,
+              },
+            },
+            roiPerYear: pair?.roiPerYear,
+            totalAssetStaked: pair?.totalAssetStaked ? pair?.totalAssetStaked / Math.pow(10, pair?.assetDecimals) : 0,
+            tvl: pair?.balanceUSD ? pair?.balanceUSD : 0,
+          }
+        } else {
+          const pair = sushiPairs.find((pair) => pair.id === pool.pair)
+          const liquidityPosition = liquidityPositions.find(
+            (liquidityPosition: any) => liquidityPosition.pair.id === pair.id
+          )
+          const blocksPerHour = 3600 / Number(averageBlockTime)
+          const balance = Number(pool.slpBalance / 1e18) > 0 ? Number(pool.slpBalance / 1e18) : 0.1
+          const totalSupply = pair.totalSupply > 0 ? pair.totalSupply : 0.1
+          const reserveUSD = pair.reserveUSD > 0 ? pair.reserveUSD : 0.1
+          const balanceUSD = (balance / Number(totalSupply)) * Number(reserveUSD)
+          const rewardPerBlock = ((pool.allocPoint / masterChef.totalAllocPoint) * masterChef.sushiPerBlock) / 1e18
+          const roiPerBlock = (rewardPerBlock * sushiPrice) / balanceUSD
+          const roiPerHour = roiPerBlock * blocksPerHour
+          const roiPerDay = roiPerHour * 24
+          const roiPerMonth = roiPerDay * 30
+          const roiPerYear = roiPerMonth * 12
+
+          return {
+            ...pool,
+            type: 'SLP',
+            symbol: pair.token0.symbol + '-' + pair.token1.symbol,
+            name: pair.token0.name + ' ' + pair.token1.name,
+            pid: Number(pool.id),
+            pairAddress: pair.id,
+            slpBalance: pool.slpBalance,
+            liquidityPair: pair,
+            roiPerBlock,
+            roiPerHour,
+            roiPerDay,
+            roiPerMonth,
+            roiPerYear,
+            rewardPerThousand: 1 * roiPerDay * (1000 / sushiPrice),
+            tvl: liquidityPosition?.liquidityTokenBalance
+              ? (pair.reserveUSD / pair.totalSupply) * liquidityPosition.liquidityTokenBalance
+              : 0.1,
+          }
         }
       })
 
     // console.log('farms:', farms)
     const sorted = orderBy(farms, ['pid'], ['desc'])
-
-    console.log({ sorted })
 
     const pids = sorted.map((pool) => {
       return pool.pid
