a30f6de498a4ffe38644b82f6e05d643354b4c3c
Matthew Lilley <hello@matthewlilley.com>
2021-06-30 01:54:59 +0100

    refactor: copy component

--- src/components/AccountDetails/Copy.tsx
+++ src/components/AccountDetails/Copy.tsx
@@ -1,47 +1,27 @@
 import { CheckCircle, Copy } from 'react-feather'
-
-import LinkStyledButton from '../LinkStyledButton'
 import React from 'react'
-import styled from 'styled-components'
 import useCopyClipboard from '../../hooks/useCopyClipboard'
-
-const CopyIcon = styled(LinkStyledButton)`
-  // color: ${({ theme }) => theme.text3};
-  flex-shrink: 0;
-  display: flex;
-  text-decoration: none;
-  font-size: 0.825rem;
-  :hover,
-  :active,
-  :focus {
-    text-decoration: none;
-    // color: ${({ theme }) => theme.text2};
-  }
-`
-const TransactionStatusText = styled.span`
-  margin-left: 0.25rem;
-  font-size: 0.825rem;
-  display: flex;
-  flex-wrap: nowrap;
-  align-items: center;
-`
 
 export default function CopyHelper(props: { toCopy: string; children?: React.ReactNode }): any {
   const [isCopied, setCopied] = useCopyClipboard()
 
   return (
-    <CopyIcon onClick={() => setCopied(props.toCopy)}>
-      {isCopied ? (
-        <TransactionStatusText>
+    <div
+      className="flex items-center flex-shrink-0 p-2 space-x-1 no-underline cursor-pointer text-baseline md:p-3 whitespace-nowrap hover:no-underline focus:no-underline active:no-underline text-blue opacity-80 hover:opacity-100 focus:opacity-100"
+      onClick={() => setCopied(props.toCopy)}
+    >
+      {isCopied && (
+        <div className="flex items-center space-x-1 whitespace-nowrap">
           <CheckCircle size={'16'} />
-          <TransactionStatusText>Copied</TransactionStatusText>
-        </TransactionStatusText>
-      ) : (
-        <TransactionStatusText>
-          <Copy size={'16'} />
-        </TransactionStatusText>
+          <div>Copied</div>
+        </div>
       )}
-      {isCopied ? '' : props.children}
-    </CopyIcon>
+      {!isCopied && (
+        <>
+          <Copy size={'16'} />
+          {props.children}
+        </>
+      )}
+    </div>
   )
 }
