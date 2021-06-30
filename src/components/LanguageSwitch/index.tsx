489d84711a13a6b050dbc05a415617be6ec38711
Matthew Lilley <hello@matthewlilley.com>
2021-06-30 04:52:50 +0100

    style: lang switch bg-dark-900 on hover

--- src/components/LanguageSwitch/index.tsx
+++ src/components/LanguageSwitch/index.tsx
@@ -75,7 +75,7 @@ export default function LangSwitcher() {
       {({ open }) => (
         <>
           <div>
-            <Menu.Button className="inline-flex justify-center w-full px-4 py-2 text-sm font-bold bg-transparent border rounded shadow-sm text-primary border-dark-800 hover:bg-dark-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-dark-700 focus:ring-dark-800">
+            <Menu.Button className="inline-flex justify-center w-full px-4 py-2 text-sm font-bold bg-transparent border rounded shadow-sm text-primary border-dark-800 hover:bg-dark-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-dark-700 focus:ring-dark-800">
               <Image src={LANGUAGES[locale].flag} alt={LANGUAGES[locale].language} width={20} height={20} />
               <ChevronDownIcon className="w-5 h-5 ml-2 -mr-1" aria-hidden="true" />
             </Menu.Button>
