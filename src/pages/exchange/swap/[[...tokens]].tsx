fc0bb3beb5231f5bd6ab293e9d462aae05eb3565
Matthew Lilley <hello@matthewlilley.com>
2021-07-04 04:19:53 +0100

    chore: swap page cleanup

--- src/pages/exchange/swap/[[...tokens]].tsx
+++ src/pages/exchange/swap/[[...tokens]].tsx
@@ -205,33 +205,34 @@ export default function Swap() {
   )
 
   const routeNotFound = !trade?.route
-  const route = trade?.route
-
-  const noRoute = !route
 
   // check whether the user has approved the router on the input token
   const [approvalState, approveCallback] = useApproveCallbackFromTrade(trade, allowedSlippage, doArcher)
 
-  const {
-    state: signatureState,
-    signatureData,
-    gatherPermitSignature,
-  } = useERC20PermitFromTrade(trade, allowedSlippage)
+  const signatureData = undefined
+
+  // const {
+  //   state: signatureState,
+  //   signatureData,
+  //   gatherPermitSignature,
+  // } = useERC20PermitFromTrade(trade, allowedSlippage)
 
   const handleApprove = useCallback(async () => {
-    if (signatureState === UseERC20PermitState.NOT_SIGNED && gatherPermitSignature) {
-      try {
-        await gatherPermitSignature()
-      } catch (error) {
-        // try to approve if gatherPermitSignature failed for any reason other than the user rejecting it
-        if (error?.code !== 4001) {
-          await approveCallback()
-        }
-      }
-    } else {
-      await approveCallback()
-    }
-  }, [approveCallback, gatherPermitSignature, signatureState])
+    await approveCallback()
+    // if (signatureState === UseERC20PermitState.NOT_SIGNED && gatherPermitSignature) {
+    //   try {
+    //     await gatherPermitSignature()
+    //   } catch (error) {
+    //     // try to approve if gatherPermitSignature failed for any reason other than the user rejecting it
+    //     if (error?.code !== 4001) {
+    //       await approveCallback()
+    //     }
+    //   }
+    // } else {
+    //   await approveCallback()
+    // }
+  }, [approveCallback])
+  // }, [approveCallback, gatherPermitSignature, signatureState])
 
   // check if user has gone through approval process, used to show two step buttons, reset on token change
   const [approvalSubmitted, setApprovalSubmitted] = useState<boolean>(false)
@@ -242,15 +243,6 @@ export default function Swap() {
       setApprovalSubmitted(true)
     }
   }, [approvalState, approvalSubmitted])
-
-  // const maxAmountInput: CurrencyAmount | undefined = maxAmountSpend(
-  //   currencyBalances[Field.INPUT],
-  //   doArcher ? archerETHTip : undefined,
-  //   chainId
-  // );
-  // const atMaxAmountInput = Boolean(
-  //   maxAmountInput && parsedAmounts[Field.INPUT]?.equalTo(maxAmountInput)
-  // );
 
   const maxInputAmount: CurrencyAmount<Currency> | undefined = maxAmountSpend(currencyBalances[Field.INPUT])
   const showMaxButton = Boolean(maxInputAmount?.greaterThan(0) && !parsedAmounts[Field.INPUT]?.equalTo(maxInputAmount))
@@ -264,17 +256,7 @@ export default function Swap() {
     doArcher ? ttl : undefined
   )
 
-  // // the callback to execute the swap
-  // const { callback: swapCallback, error: swapCallbackError } = useSwapCallback(
-  //   trade,
-  //   allowedSlippage,
-  //   recipient,
-  //   doArcher ? ttl : undefined
-  // );
-
   const [singleHopOnly] = useUserSingleHopOnly()
-
-  // const { priceImpactWithoutFee } = computeTradePriceBreakdown(trade);
 
   const handleSwap = useCallback(() => {
     if (!swapCallback) {
@@ -603,7 +585,7 @@ export default function Swap() {
                   ? i18n._(t`Unwrap`)
                   : null)}
             </Button>
-          ) : noRoute && userHasSpecifiedInputOutput ? (
+          ) : routeNotFound && userHasSpecifiedInputOutput ? (
             <div style={{ textAlign: 'center' }}>
               <div className="mb-1">{i18n._(t`Insufficient liquidity for this trade`)}</div>
               {singleHopOnly && <div className="mb-1">{i18n._(t`Try enabling multi-hop trades`)}</div>}
