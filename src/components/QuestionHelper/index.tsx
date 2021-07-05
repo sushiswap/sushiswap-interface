a2bc79278fc231bb92b107734c338ad5474b3885
chillichelli <ramin@erfani.dev>
2021-07-05 14:52:45 +0200

    refactor: styled components to tailwind for settings

--- src/components/QuestionHelper/index.tsx
+++ src/components/QuestionHelper/index.tsx
@@ -1,30 +1,6 @@
 import React, { FC, useCallback, useState } from 'react'
-
-import { QuestionMarkCircleIcon as OutlineQuestionMarkCircleIcon } from '@heroicons/react/outline'
 import { QuestionMarkCircleIcon as SolidQuestionMarkCircleIcon } from '@heroicons/react/solid'
 import Tooltip from '../Tooltip'
-import styled from 'styled-components'
-
-const LightQuestionWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 0.2rem;
-  border: none;
-  background: none;
-  outline: none;
-  cursor: default;
-  border-radius: 36px;
-  width: 24px;
-  height: 24px;
-  background-color: rgba(255, 255, 255, 0.1);
-  color: ${({ theme }) => theme.white};
-
-  :hover,
-  :focus {
-    opacity: 0.7;
-  }
-`
 
 const QuestionHelper: FC<{ text: any }> = ({ children, text }) => {
   const [show, setShow] = useState<boolean>(false)
@@ -58,23 +34,6 @@ const QuestionHelper: FC<{ text: any }> = ({ children, text }) => {
         >
           <SolidQuestionMarkCircleIcon width={16} height={16} />
         </div>
-      </Tooltip>
-    </span>
-  )
-}
-
-export const LightQuestionHelper: FC<{ text: string }> = ({ text }) => {
-  const [show, setShow] = useState<boolean>(false)
-
-  const open = useCallback(() => setShow(true), [setShow])
-  const close = useCallback(() => setShow(false), [setShow])
-
-  return (
-    <span className="ml-1">
-      <Tooltip text={text} show={show}>
-        <LightQuestionWrapper onClick={open} onMouseEnter={open} onMouseLeave={close}>
-          <OutlineQuestionMarkCircleIcon>?</OutlineQuestionMarkCircleIcon>
-        </LightQuestionWrapper>
       </Tooltip>
     </span>
   )
