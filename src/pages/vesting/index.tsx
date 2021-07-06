2e09103949baee16a2f5fb6297c839fcad252e38
Matthew Lilley <hello@matthewlilley.com>
2021-07-06 08:24:01 +0100

    fix: vesting script

--- src/pages/vesting/index.tsx
+++ src/pages/vesting/index.tsx
@@ -69,7 +69,9 @@ export default function Vesting() {
   useEffect(() => {
     const fetchLockup = async () => {
       if (account) {
-        fetch('https://raw.githubusercontent.com/sushiswap/sushi-vesting/master/amounts-10959148-11550728.json')
+        fetch(
+          'https://raw.githubusercontent.com/sushiswap/sushi-vesting/master/merkle/week-13/merkle-10959148-11550728.json'
+        )
           .then((response) => response.json())
           .then((data) => {
             // console.log('vesting:', data)
