9be14a03782f43a48a44d74523d675999f1f3627
Matthew Lilley <hello@matthewlilley.com>
2021-07-07 07:00:57 +0100

    chore: update root meta keys

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -146,21 +146,22 @@ export default class MyDocument extends Document {
 
           {/* <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500' /> */}
 
-          <meta name="twitter:card" content="app" />
-          <meta name="twitter:url" content="https://sushi.com" />
-          <meta name="twitter:title" content="SUSHI App" />
+          <meta key="twitter:card" name="twitter:card" content="app" />
+          <meta key="twitter:url" name="twitter:url" content="https://sushi.com" />
+          <meta key="twitter:title" name="twitter:title" content="SUSHI App" />
           <meta
+            key="twitter:description"
             name="twitter:description"
             content="Be a DeFi Chef with Sushi. Swap, earn, stack yields, lend, borrow, leverage all on one decentralized, community driven platform. Welcome home to DeFi"
           />
-          <meta name="twitter:image" content="https://app.sushi.com/icons/icon-192x192.png" />
-          <meta name="twitter:creator" content="@SushiSwap" />
-          <meta property="og:type" content="website" />
-          <meta property="og:title" content="SUSHI App" />
+          <meta key="twitter:image" name="twitter:image" content="https://app.sushi.com/icons/icon-192x192.png" />
+          <meta key="twitter:creator" name="twitter:creator" content="@SushiSwap" />
+          <meta key="og:type" property="og:type" content="website" />
+          <meta key="og:title" property="og:title" content="SUSHI App" />
 
-          <meta property="og:site_name" content="SUSHI App" />
-          <meta property="og:url" content="https://sushi.com" />
-          <meta property="og:image" content="https://app.sushi.com/apple-touch-icon.png" />
+          <meta key="og:site_name" property="og:site_name" content="SUSHI App" />
+          <meta key="og:url" property="og:url" content="https://sushi.com" />
+          <meta key="og:image" property="og:image" content="https://app.sushi.com/apple-touch-icon.png" />
           <meta
             key="og:description"
             property="og:description"
