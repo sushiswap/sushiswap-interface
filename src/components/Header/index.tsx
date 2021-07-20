ec85f9b4340d75b5b26e2c39409f69a7a9c6c758
TopETH <supernathanliu@gmail.com>
2021-07-20 19:11:24 +0900

    feat: add trident nft landing page on miso

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -114,6 +114,16 @@ function AppBar(): JSX.Element {
                           </a>
                         </NavLink>
                       )}
+                      {chainId === ChainId.MAINNET && (
+                        <Link href={'/miso'}>
+                          <a
+                            id={`stake-nav-link`}
+                            className="p-2 text-baseline text-primary hover:text-high-emphesis focus:text-high-emphesis md:p-3 whitespace-nowrap"
+                          >
+                            {i18n._(t`Miso`)}
+                          </a>
+                        </Link>
+                      )}
                     </div>
                   </div>
                 </div>
@@ -353,6 +363,17 @@ function AppBar(): JSX.Element {
                       {i18n._(t`Analytics`)}
                     </ExternalLink>
                   )}
+                  
+                {chainId === ChainId.MAINNET && (
+                  <Link href={'/miso'}>
+                    <a
+                      id={`stake-nav-link`}
+                      className="p-2 text-baseline text-primary hover:text-high-emphesis focus:text-high-emphesis md:p-3 whitespace-nowrap"
+                    >
+                      {i18n._(t`Miso`)}
+                    </a>
+                  </Link>
+                )}
               </div>
             </Popover.Panel>
           </>
