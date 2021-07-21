d77c30adf34ceee9ced73dfd20620fd53fd213a4
Matthew Lilley <hello@matthewlilley.com>
2021-07-21 21:01:57 +0100

    fix: show farms only if alloc > 0

--- src/services/graph/queries/minichef.ts
+++ src/services/graph/queries/minichef.ts
@@ -6,7 +6,7 @@ export const miniChefPoolsQuery = gql`
     $skip: Int! = 0
     $orderBy: String! = "id"
     $orderDirection: String! = "desc"
-    $where: Pool_filter! = { allocPoint_gt: 0, accSushiPerShare_gt: 0 }
+    $where: Pool_filter! = { allocPoint_gt: 0 }
   ) {
     pools(first: $first, skip: $skip, orderBy: $orderBy, orderDirection: $orderDirection, where: $where) {
       id
@@ -36,7 +36,7 @@ export const miniChefPairAddressesQuery = gql`
     $skip: Int! = 0
     $orderBy: String! = "id"
     $orderDirection: String! = "desc"
-    $where: Pool_filter! = { allocPoint_gt: 0, accSushiPerShare_gt: 0 }
+    $where: Pool_filter! = { allocPoint_gt: 0 }
   ) {
     pools(first: $first, skip: $skip, orderBy: $orderBy, orderDirection: $orderDirection, where: $where) {
       id
