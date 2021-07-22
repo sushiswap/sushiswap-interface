c94b681ecc15cd3669c43af4277c8fe71e92c265
Matthew Lilley <hello@matthewlilley.com>
2021-07-22 09:46:39 +0100

    refactor: bentobox page

--- src/pages/bentobox/index.tsx
+++ src/pages/bentobox/index.tsx
@@ -1,7 +1,9 @@
 import Card from '../../components/Card'
+import Container from '../../components/Container'
 import Head from 'next/head'
 import Image from '../../components/Image'
 import Link from 'next/link'
+import NextImage from 'next/image'
 import React from 'react'
 import Web3Status from '../../components/Web3Status'
 import bentoBoxHero from '../../../public/bentobox-hero.jpg'
@@ -13,7 +15,11 @@ export default function BenotBox() {
   const { i18n } = useLingui()
   const { account } = useActiveWeb3React()
   return (
-    <>
+    <Container
+      id="bentobox-page"
+      className="z-0 flex flex-col justify-center text-center y-4 md:py-8 lg:py-12"
+      maxWidth="full"
+    >
       <Head>
         <title>BentoBox | Sushi</title>
         <meta
@@ -22,91 +28,86 @@ export default function BenotBox() {
           content="BentoBox is a token vault that generates yield for liquidity providers. BentoBox creates a source of liquidity that any user can access with minimal approvals, minimal gas usage, and maximal capital efficiency."
         />
       </Head>
-      <div className="absolute top-0 left-0 right-0" style={{ maxHeight: 700 }}>
+      <div className="absolute top-0 left-0 right-0 z-0 w-full h-full" style={{ maxHeight: 700 }}>
         <Image
-          className="opacity-50"
-          // src="/bentobox-hero.jpg"
           src={bentoBoxHero}
           alt="BentoBox Hero"
+          layout="responsive"
           objectFit="contain"
           objectPosition="top"
-          layout="responsive"
-          height="100%"
-          width="auto"
+          className="opacity-50"
+          priority
         />
       </div>
-      <div className="z-10 text-center">
+
+      <div className="flex justify-center">
         <Image
           src="/bentobox-logo.png"
           alt="BentoBox Logo"
-          className="object-scale-down w-40 h-auto md:w-60"
-          width="100%"
-          height="auto"
+          objectFit="scale-down"
+          layout="responsive"
+          height={150}
+          width={240}
         />
-
-        <div className="container max-w-5xl mx-auto">
-          <div className="text-3xl font-bold md:text-5xl text-high-emphesis">{i18n._(t`BentoBox Apps`)}</div>
-          <div className="p-4 mt-0 mb-8 text-base font-medium md:text-lg lg:text-xltext-high-emphesis md:mt-4">
-            {i18n._(t`BentoBox is an innovative way to use dapps gas-efficiently and gain extra yield.`)}
-          </div>
-        </div>
-
-        <div className="container max-w-5xl mx-auto">
-          <div className="grid grid-cols-4 gap-4 sm:gap-12 grid-flow-auto">
-            <Card className="w-full col-span-2 rounded cursor-pointer md:col-span-1 bg-dark-800 hover:bg-dark-900 shadow-pink-glow hover:shadow-pink-glow-hovered">
-              <div className="relative w-full">
-                <Image
-                  src="/kashi-neon.png"
-                  alt="Kashi Logo"
-                  className="mb-4"
-                  objectFit="scale-down"
-                  width="100%"
-                  height="auto"
-                />
-                {account ? (
-                  <Link href="/borrow">
-                    <a className="w-full px-4 py-2 text-center bg-transparent border border-transparent rounded text-high-emphesis border-gradient-r-blue-pink-dark-900">
-                      {i18n._(t`Enter`)}
-                    </a>
-                  </Link>
-                ) : (
-                  <Web3Status />
-                )}
-              </div>
-            </Card>
-            <Card className="flex items-center justify-center col-span-2 transition-colors cursor-pointer md:col-span-1 bg-dark-800 hover:bg-dark-900 shadow-blue-glow hover:shadow-blue-glow-hovered">
-              <Image
-                src="/coming-soon.png"
-                alt="Coming Soon"
-                className="mb-4"
-                objectFit="scale-down"
-                width="100%"
-                height="auto"
-              />
-            </Card>
-            <Card className="flex items-center justify-center col-span-2 transition-colors cursor-pointer md:col-span-1 bg-dark-800 hover:bg-dark-900 shadow-pink-glow hover:shadow-pink-glow-hovered">
-              <Image
-                src="/coming-soon.png"
-                alt="Coming Soon"
-                className="mb-4"
-                objectFit="scale-down"
-                width="100%"
-                height="auto"
-              />
-            </Card>
-            <Card className="flex items-center justify-center col-span-2 transition-colors cursor-pointer md:col-span-1 bg-dark-800 hover:bg-dark-900 shadow-blue-glow hover:shadow-blue-glow-hovered">
-              <Image
-                src="/coming-soon.png"
-                alt="Coming Soon"
-                className="mb-4"
-                objectFit="scale-down"
-                width="100%"
-                height="auto"
-              />
-            </Card>
-          </div>
-        </div>
       </div>
-    </>
+
+      <Container className="z-50 mx-auto" maxWidth="5xl">
+        <div className="text-3xl font-bold md:text-5xl text-high-emphesis">{i18n._(t`BentoBox Apps`)}</div>
+        <div className="p-4 mt-0 mb-8 text-base font-medium md:text-lg lg:text-xltext-high-emphesis md:mt-4">
+          {i18n._(t`BentoBox is an innovative way to use dapps gas-efficiently and gain extra yield.`)}
+        </div>
+        <div className="grid grid-cols-4 gap-4 sm:gap-12 grid-flow-auto">
+          <Card className="flex items-center justify-center w-full col-span-2 space-y-1 rounded cursor-pointer md:col-span-1 bg-dark-800 hover:bg-dark-900 shadow-pink-glow hover:shadow-pink-glow-hovered">
+            <Image
+              src="/kashi-neon.png"
+              alt="Kashi Logo"
+              objectFit="scale-down"
+              layout="responsive"
+              height={150}
+              width={100}
+            />
+            {account ? (
+              <Link href="/borrow">
+                <a className="w-full px-4 py-2 text-center bg-transparent border border-transparent rounded text-high-emphesis border-gradient-r-blue-pink-dark-900">
+                  {i18n._(t`Enter`)}
+                </a>
+              </Link>
+            ) : (
+              <Web3Status />
+            )}
+          </Card>
+          <Card className="flex items-center justify-center col-span-2 transition-colors cursor-pointer md:col-span-1 bg-dark-800 hover:bg-dark-900 shadow-blue-glow hover:shadow-blue-glow-hovered">
+            <Image
+              src="/coming-soon.png"
+              alt="Coming Soon"
+              objectFit="scale-down"
+              layout="responsive"
+              height={150}
+              width={100}
+            />
+          </Card>
+          <Card className="flex items-center justify-center col-span-2 transition-colors cursor-pointer md:col-span-1 bg-dark-800 hover:bg-dark-900 shadow-pink-glow hover:shadow-pink-glow-hovered">
+            <Image
+              src="/coming-soon.png"
+              alt="Coming Soon"
+              objectFit="scale-down"
+              layout="responsive"
+              height={150}
+              width={100}
+            />
+          </Card>
+          <Card className="flex items-center justify-center col-span-2 transition-colors cursor-pointer md:col-span-1 bg-dark-800 hover:bg-dark-900 shadow-blue-glow hover:shadow-blue-glow-hovered">
+            <Image
+              src="/coming-soon.png"
+              alt="Coming Soon"
+              objectFit="scale-down"
+              layout="responsive"
+              height={150}
+              width={100}
+            />
+          </Card>
+        </div>
+      </Container>
+    </Container>
   )
 }
