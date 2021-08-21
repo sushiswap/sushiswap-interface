84b3e181a0a437ea04dbf6ff3844143bcd43627e
Matthew Lilley <hello@matthewlilley.com>
2021-08-21 08:57:35 +0100

    feat: local storage service

---
+++ src/services/local-storage/fetcher.ts
@@ -0,0 +1,7 @@
+export default async function fetcher(key) {
+  const value = localStorage.getItem(key)
+  if (!value) return undefined
+  return JSON.parse(value)
+}
+
+// https://github.com/vercel/swr/blob/master/examples/storage-tab-sync/pages/index.js
