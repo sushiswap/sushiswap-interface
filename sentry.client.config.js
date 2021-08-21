fcee9923278c4b014bda5fc2d0950ddeac3776b2
Matthew Lilley <hello@matthewlilley.com>
2021-08-21 04:55:10 +0100

    feat: show report dialog before sending error

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -14,4 +14,11 @@ Sentry.init({
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
   // that it will also get attached to your source maps
+  beforeSend(event, hint) {
+    // Check if it is an exception, and if so, show the report dialog
+    if (event.exception) {
+      Sentry.showReportDialog({ eventId: event.event_id })
+    }
+    return event
+  },
 })
