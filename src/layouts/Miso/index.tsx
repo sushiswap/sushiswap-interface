e3aa444da47db7b2cb6bfcead37bb9f638d9bb5f
TopETH <supernathanliu@gmail.com>
2021-08-23 17:40:05 -0400

    feat: extract stepper component

--- src/layouts/Miso/index.tsx
+++ src/layouts/Miso/index.tsx
@@ -1,15 +1,14 @@
-import { CheckIcon, ChevronRightIcon } from '@heroicons/react/solid'
+import { ChevronRightIcon } from '@heroicons/react/solid'
 import { t } from '@lingui/macro'
 import { useLingui } from '@lingui/react'
 import styled from 'styled-components'
 
+import ExternalLink from '../../components/ExternalLink'
 import Footer from '../../components/Footer'
 import Header from '../../components/Header'
 import Main from '../../components/Main'
 import NavLink from '../../components/NavLink'
-import Popups from '../../components/Popups'
-import { classNames } from '../../functions/styling'
-import ExternalLink from '../../components/ExternalLink'
+import Stepper from '../../components/Stepper'
 
 const Navbar = styled.div`
   background: linear-gradient(90.12deg, rgba(9, 147, 236, 0.05) 12.77%, rgba(243, 56, 195, 0.05) 87.29%), #161522;
@@ -75,107 +74,7 @@ function Title({ data, isFactoryPage }) {
           </div>
         </div>
       </div>
-      {steps.length > 0 && (
-        <div className="lg:border-t lg:border-b lg:border-gray-500">
-          <nav className="mx-auto" aria-label="Progress">
-            <ol role="list" className="rounded-md overflow-hidden lg:flex lg:border-gray-500 lg:rounded-none">
-              {steps.map((step, stepIdx) => (
-                <li key={stepIdx} className="relative overflow-hidden lg:flex-1">
-                  <div
-                    className={classNames(
-                      stepIdx === 0 ? 'border-b-0 rounded-t-md' : '',
-                      stepIdx === steps.length - 1 ? 'border-t-0 rounded-b-md' : '',
-                      'border border-gray-200 overflow-hidden lg:border-0'
-                    )}
-                  >
-                    {stepIdx < data.active ? (
-                      <a href={step.href} className="group">
-                        <span
-                          className="absolute top-0 left-0 w-1 h-full bg-transparent lg:w-full lg:h-1 lg:bottom-0 lg:top-auto"
-                          aria-hidden="true"
-                        />
-                        <span className={classNames('pl-9 py-5 flex items-start text-sm font-medium')}>
-                          <span className="flex-shrink-0">
-                            <span className="w-10 h-10 flex items-center justify-center bg-gradient-to-r from-blue to-pink rounded-full">
-                              <CheckIcon className="w-6 h-6 text-white" aria-hidden="true" />
-                            </span>
-                          </span>
-                          <span className="mt-0.5 ml-4 min-w-0 flex flex-col">
-                            <span className="text-xs font-semibold tracking-wide uppercase text-white">
-                              {step.heading}
-                            </span>
-                            <span className="text-sm font-medium">{step.content}</span>
-                          </span>
-                        </span>
-                      </a>
-                    ) : stepIdx === data.active ? (
-                      <div>
-                        <span
-                          className="absolute top-0 left-0 w-1 h-full bg-blue lg:w-full lg:h-1 lg:bottom-0 lg:top-auto"
-                          aria-hidden="true"
-                        />
-                        <span className={classNames('pl-9 py-5 flex items-start text-sm font-medium')}>
-                          <span className="flex-shrink-0">
-                            <span className="w-10 h-10 flex items-center justify-center border-2 border-blue rounded-full">
-                              <span className="text-blue">{stepIdx + 1}</span>
-                            </span>
-                          </span>
-                          <span className="mt-0.5 ml-4 min-w-0 flex flex-col">
-                            <span className="text-xs font-semibold text-white tracking-wide uppercase">
-                              {step.heading}
-                            </span>
-                            <span className="text-sm font-medium">{step.content}</span>
-                          </span>
-                        </span>
-                      </div>
-                    ) : (
-                      <div>
-                        <span
-                          className="absolute top-0 left-0 w-1 h-full bg-transparent lg:w-full lg:h-1 lg:bottom-0 lg:top-auto"
-                          aria-hidden="true"
-                        />
-                        <span className={classNames('pl-9 py-5 flex items-start text-sm font-medium')}>
-                          <span className="flex-shrink-0">
-                            <span className="w-10 h-10 flex items-center justify-center border-2 border-gray-500 rounded-full">
-                              <span className="text-gray-500">{stepIdx + 1}</span>
-                            </span>
-                          </span>
-                          <span className="mt-0.5 ml-4 min-w-0 flex flex-col">
-                            <span className="text-xs font-semibold text-gray-500 tracking-wide uppercase">
-                              {step.heading}
-                            </span>
-                            <span className="text-sm font-medium text-gray-500">{step.content}</span>
-                          </span>
-                        </span>
-                      </div>
-                    )}
-
-                    {stepIdx !== 0 ? (
-                      <>
-                        {/* Separator */}
-                        <div className="hidden absolute top-0 left-0 w-3 inset-0 lg:block" aria-hidden="true">
-                          <svg
-                            className="h-full w-full text-gray-500"
-                            viewBox="0 0 12 82"
-                            fill="none"
-                            preserveAspectRatio="none"
-                          >
-                            <path
-                              d="M0.5 0V31L10.5 41L0.5 51V82"
-                              stroke="currentcolor"
-                              vectorEffect="non-scaling-stroke"
-                            />
-                          </svg>
-                        </div>
-                      </>
-                    ) : null}
-                  </div>
-                </li>
-              ))}
-            </ol>
-          </nav>
-        </div>
-      )}
+      {steps.length > 0 && <Stepper steps={steps} active={data.active} />}
     </div>
   )
 }
@@ -208,7 +107,6 @@ const Layout = ({
           <Footer />
         </div>
       </div>
-      <Popups />
     </div>
   )
 }
