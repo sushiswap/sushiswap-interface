22e8b6ccda49fbb4bc401b85ae7769896f898bc7
Jack Clancy <jack.clancy93@gmail.com>
2021-09-09 10:20:00 -0400

    feat: adding range pill buttons

--- src/features/trident/pool/PoolStatsChart.tsx
+++ src/features/trident/pool/PoolStatsChart.tsx
@@ -3,10 +3,12 @@ import ToggleButtonGroup from '../../../components/ToggleButton'
 import { t } from '@lingui/macro'
 import { useLingui } from '@lingui/react'
 import { BarGraph } from '../../../components/BarGraph'
+import Button from '../../../components/Button'
 
 const PoolStatsChart = () => {
   const { i18n } = useLingui()
   const [chartType, setChartType] = useState('Volume')
+  const [chartRange, setChartRange] = useState('ALL')
 
   return (
     <>
@@ -19,8 +21,22 @@ const PoolStatsChart = () => {
           </ToggleButtonGroup.Button>
         </ToggleButtonGroup>
       </div>
-      <div className="w-full h-32 px-5 mt-5">
+      <div className="w-full h-40 px-5 mt-5">
         <BarGraph />
+      </div>
+      <div className="flex justify-between mx-5">
+        {['24H', '1W', '1M', '1Y', 'ALL'].map((e) => (
+          <Button
+            key={e}
+            onClick={() => setChartRange(e)}
+            variant={e === chartRange ? 'outlined' : 'empty'}
+            size="xs"
+            color={e === chartRange ? 'blue' : 'gray'}
+            className={e === chartRange ? 'w-10 py-1 text-sm bg-blue-400' : 'w-10 py-1 text-sm'}
+          >
+            {e}
+          </Button>
+        ))}
       </div>
     </>
   )
