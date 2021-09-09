9870f320835d24d0a820642c15a6c4952c56012c
Matthew Lilley <hello@matthewlilley.com>
2021-09-09 21:16:39 +0100

    fix: compilation

--- src/config/wallets.ts
+++ src/config/wallets.ts
@@ -52,7 +52,34 @@ export const SUPPORTED_WALLETS: { [key: string]: WalletInfo } = {
         rpc: RPC,
         bridge: 'https://bridge.walletconnect.org',
         qrcode: true,
-        pollingInterval: 15000,
+        supportedChainIds: [
+          1, // mainnet
+          3, // ropsten
+          4, // rinkeby
+          5, // goreli
+          42, // kovan
+          250, // fantom
+          4002, // fantom testnet
+          137, // matic
+          80001, // matic testnet
+          100, // xdaiW
+          56, // binance smart chain
+          97, // binance smart chain testnet
+          1287, // moonbase
+          43114, // avalanche
+          43113, // fuji
+          128, // heco
+          256, // heco testnet
+          1666600000, // harmony
+          1666700000, // harmony testnet
+          66, // okex testnet
+          65, // okex testnet
+          42161, // arbitrum
+          42220, // celo
+          11297108109, // palm
+          1285, // moonriver
+        ],
+        // pollingInterval: 15000,
       })
     },
     name: 'WalletConnect',
