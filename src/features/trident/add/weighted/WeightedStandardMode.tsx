d1eae3cf11552fe1d876fa08fcda3de11ed45894
chillichelli <ramin@erfani.dev>
2021-09-11 14:56:24 +0200

    feat: first things on adding web3 to weighted pools

--- src/features/trident/add/weighted/WeightedStandardMode.tsx
+++ src/features/trident/add/weighted/WeightedStandardMode.tsx
@@ -1,57 +1,166 @@
 import AssetInput from '../../../../components/AssetInput'
-import React from 'react'
-import DepositButtons from './../DepositButtons'
-import useSufficientBalances from '../../../../hooks/useSufficientBalances'
+import React, { useMemo } from 'react'
 import { ZERO } from '@sushiswap/sdk'
 import TransactionDetails from './../TransactionDetails'
-import { useTridentAddWeightedContext, useTridentAddWeightedState } from './context'
+import { useRecoilCallback, useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil'
+import { attemptingTxnAtom, noLiquiditySelector, showReviewAtom, spendFromWalletAtom } from '../../context/atoms'
+import { useLingui } from '@lingui/react'
+import { useActiveWeb3React, useBentoBoxContract } from '../../../../hooks'
+
+import { useCurrencyBalances } from '../../../../state/wallet/hooks'
+import { useUSDCValue } from '../../../../hooks/useUSDCPrice'
+import TridentApproveGate from '../../ApproveButton'
+import Dots from '../../../../components/Dots'
+import { t } from '@lingui/macro'
+import { classNames, maxAmountSpend } from '../../../../functions'
+import Button from '../../../../components/Button'
+import Typography from '../../../../components/Typography'
+import Lottie from 'lottie-react'
+import loadingCircle from '../../../../animation/loading-circle.json'
+import { ConstantProductPoolState } from '../../../../hooks/useTridentClassicPools'
+import { poolAtom } from './context/atoms'
 
 const WeightedStandardMode = () => {
-  const { inputAmounts, spendFromWallet } = useTridentAddWeightedState()
-  const { currencies, handleInput, parsedInputAmounts, showReview, setSpendFromWallet } = useTridentAddWeightedContext()
-  const sufficientBalances = useSufficientBalances(parsedInputAmounts, spendFromWallet)
-  const validInputs = sufficientBalances && Object.values(parsedInputAmounts).every((el) => el?.greaterThan(ZERO))
+  const { i18n } = useLingui()
+  const { account } = useActiveWeb3React()
+  const [poolState, pool] = useRecoilValue(poolAtom)
+  const [parsedAmountA, parsedAmountB] = useRecoilValue(parsedAmountsSelector)
+  const bentoBox = useBentoBoxContract()
 
-  // TODO
-  const onMax = () => {}
-  const isMaxInput = false
+  const setShowReview = useSetRecoilState(showReviewAtom)
+  const setMainInput = useSetRecoilState(mainInputAtom)
+  const setSecondaryInput = useSetRecoilState(secondaryInputSelector)
+  const formattedAmounts = useRecoilValue(formattedAmountsSelector)
+  const setTypedField = useSetRecoilState(typedFieldAtom)
+  const [spendFromWallet, setSpendFromWallet] = useRecoilState(spendFromWalletAtom)
+  const balances = useCurrencyBalances(account ?? undefined, [pool?.token0, pool?.token1])
+  const noLiquidity = useRecoilValue(noLiquiditySelector)
+  const attemptingTxn = useRecoilValue(attemptingTxnAtom)
 
-  // TODO Fixture
-  const [addressA, addressB] = Object.keys(currencies)
-  const weights = {
-    [addressA]: '70%',
-    [addressB]: '30%',
-  }
+  const usdcA = useUSDCValue(balances?.[0])
+  const usdcB = useUSDCValue(balances?.[1])
+
+  const onMax = useRecoilCallback(
+    ({ set }) =>
+      async () => {
+        if (!balances || !usdcA || !usdcB) return
+
+        if (!noLiquidity) {
+          usdcA?.lessThan(usdcB)
+            ? set(mainInputAtom, maxAmountSpend(balances[0])?.toExact())
+            : set(secondaryInputSelector, maxAmountSpend(balances[1])?.toExact())
+        } else {
+          set(mainInputAtom, maxAmountSpend(balances[0])?.toExact())
+          set(secondaryInputSelector, maxAmountSpend(balances[1])?.toExact())
+        }
+      },
+    [balances, noLiquidity, usdcA, usdcB]
+  )
+
+  const isMax = useMemo(() => {
+    if (!balances || !usdcA || !usdcB) return false
+
+    if (!noLiquidity) {
+      return usdcA?.lessThan(usdcB)
+        ? parsedAmountA?.equalTo(maxAmountSpend(balances[0]))
+        : parsedAmountB?.equalTo(maxAmountSpend(balances[1]))
+    } else {
+      return parsedAmountA?.equalTo(maxAmountSpend(balances[0])) && parsedAmountB?.equalTo(maxAmountSpend(balances[1]))
+    }
+  }, [balances, noLiquidity, parsedAmountA, parsedAmountB, usdcA, usdcB])
+
+  // TODO ramin: balance check for bento
+  let error = !account
+    ? i18n._(t`Connect Wallet`)
+    : poolState === ConstantProductPoolState.INVALID
+    ? i18n._(t`Invalid pair`)
+    : !parsedAmountA?.greaterThan(ZERO) || !parsedAmountB?.greaterThan(ZERO)
+    ? i18n._(t`Enter an amount`)
+    : parsedAmountA && balances[0]?.lessThan(parsedAmountA)
+    ? i18n._(t`Insufficient ${pool?.token0?.symbol} balance`)
+    : parsedAmountB && balances?.length && balances[1]?.lessThan(parsedAmountB)
+    ? i18n._(t`Insufficient ${pool?.token1?.symbol} balance`)
+    : ''
 
   return (
     <div className="flex flex-col gap-6">
       <div className="flex flex-col gap-4 px-5">
         <AssetInput
-          value={inputAmounts[addressA]}
-          currency={currencies[addressA]}
-          onChange={(val) => handleInput(val, addressA)}
-          headerRight={<AssetInput.WalletSwitch onChange={setSpendFromWallet} checked={spendFromWallet} />}
+          value={formattedAmounts[0]}
+          currency={pool?.token0}
+          onChange={(val) => {
+            setTypedField(TypedField.A)
+            setMainInput(val)
+          }}
+          headerRight={
+            <AssetInput.WalletSwitch onChange={() => setSpendFromWallet(!spendFromWallet)} checked={spendFromWallet} />
+          }
           spendFromWallet={spendFromWallet}
-          chip={weights[addressA]}
         />
         <AssetInput
-          value={inputAmounts[addressB]}
-          currency={currencies[addressB]}
-          onChange={(val) => handleInput(val, addressB)}
+          value={formattedAmounts[1]}
+          currency={pool?.token1}
+          onChange={(val) => {
+            setTypedField(TypedField.B)
+            setSecondaryInput(val)
+          }}
           spendFromWallet={spendFromWallet}
-          chip={weights[addressB]}
         />
 
-        <div className="flex flex-col">
-          <DepositButtons
-            inputValid={validInputs}
-            onMax={onMax}
-            isMaxInput={isMaxInput}
-            onClick={() => showReview(true)}
-          />
+        <div className="flex flex-col gap-3">
+          <TridentApproveGate inputAmounts={[parsedAmountA, parsedAmountB]} tokenApproveOn={bentoBox?.address}>
+            {({ approved, loading }) => {
+              const disabled = !!error || !approved || loading || attemptingTxn
+              const buttonText = attemptingTxn ? (
+                <Dots>{i18n._(t`Depositing`)}</Dots>
+              ) : loading ? (
+                ''
+              ) : error ? (
+                error
+              ) : (
+                i18n._(t`Confirm Deposit`)
+              )
+
+              return (
+                <div className={classNames(onMax && !isMax ? 'grid grid-cols-2 gap-3' : 'flex')}>
+                  {!isMax && (
+                    <Button color="gradient" variant={isMax ? 'filled' : 'outlined'} disabled={isMax} onClick={onMax}>
+                      <Typography
+                        variant="sm"
+                        weight={700}
+                        className={!isMax ? 'text-high-emphesis' : 'text-low-emphasis'}
+                      >
+                        {i18n._(t`Max Deposit`)}
+                      </Typography>
+                    </Button>
+                  )}
+                  <Button
+                    {...(loading && {
+                      startIcon: (
+                        <div className="w-4 h-4 mr-1">
+                          <Lottie animationData={loadingCircle} autoplay loop />
+                        </div>
+                      ),
+                    })}
+                    color="gradient"
+                    disabled={disabled}
+                    onClick={() => setShowReview(true)}
+                  >
+                    <Typography
+                      variant="sm"
+                      weight={700}
+                      className={!error ? 'text-high-emphesis' : 'text-low-emphasis'}
+                    >
+                      {buttonText}
+                    </Typography>
+                  </Button>
+                </div>
+              )
+            }}
+          </TridentApproveGate>
         </div>
       </div>
-      {validInputs && (
+      {!error && (
         <div className="flex flex-col px-5">
           <TransactionDetails />
         </div>
--- src/features/trident/add/weighted/WeightedZapMode.tsx
+++ src/features/trident/add/weighted/WeightedZapMode.tsx
@@ -5,69 +5,119 @@ import { useLingui } from '@lingui/react'
 import Typography from '../../../../components/Typography'
 import ListPanel from '../../../../components/ListPanel'
 import AssetInput from '../../../../components/AssetInput'
-import { Token } from '@sushiswap/sdk'
+import { NATIVE } from '@sushiswap/sdk'
 import TransactionDetails from './../TransactionDetails'
 import React from 'react'
-import { useTridentAddWeightedContext, useTridentAddWeightedState } from './context'
+import { useActiveWeb3React, useBentoBoxContract } from '../../../../hooks'
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil'
+
+import { attemptingTxnAtom, noLiquiditySelector, showReviewAtom } from '../../context/atoms'
+import { useCurrencyBalance } from '../../../../state/wallet/hooks'
+import { ConstantProductPoolState } from '../../../../hooks/useTridentClassicPools'
+import TridentApproveGate from '../../ApproveButton'
+import Lottie from 'lottie-react'
+import loadingCircle from '../../../../animation/loading-circle.json'
+import Dots from '../../../../components/Dots'
+import {
+  parsedZapAmountSelector,
+  parsedZapSplitAmountsSelector,
+  poolAtom,
+  selectedZapCurrencyAtom,
+  zapInputAtom,
+} from './context/atoms'
 
 const WeightedZapMode = () => {
+  const { account, chainId } = useActiveWeb3React()
   const { i18n } = useLingui()
-  const { inputAmounts, inputTokenAddress } = useTridentAddWeightedState()
-  const { currencies, handleInput, showReview, parsedOutputAmounts, selectInputToken } = useTridentAddWeightedContext()
+  const bentoBox = useBentoBoxContract()
 
-  const validInput = inputTokenAddress && inputAmounts[inputTokenAddress]
+  const [poolState, pool] = useRecoilValue(poolAtom)
+  const [zapInput, setZapInput] = useRecoilState(zapInputAtom)
+  const parsedZapAmount = useRecoilValue(parsedZapAmountSelector)
+  const parsedZapSplitAmounts = useRecoilValue(parsedZapSplitAmountsSelector)
+  const [selectedZapCurrency, setSelectedZapCurrency] = useRecoilState(selectedZapCurrencyAtom)
+  const setShowReview = useSetRecoilState(showReviewAtom)
+  const balance = useCurrencyBalance(account ?? undefined, selectedZapCurrency)
+  const noLiquidity = useRecoilValue(noLiquiditySelector)
+  const attemptingTxn = useRecoilValue(attemptingTxnAtom)
 
-  // TODO Fixture
-  const [addressA, addressB] = Object.keys(currencies)
-  const weights = {
-    [addressA]: '70%',
-    [addressB]: '30%',
-  }
+  let error = !account
+    ? i18n._(t`Connect Wallet`)
+    : poolState === ConstantProductPoolState.INVALID
+    ? i18n._(t`Invalid pair`)
+    : !zapInput
+    ? i18n._(t`Enter an amount`)
+    : parsedZapAmount && balance?.lessThan(parsedZapAmount)
+    ? i18n._(t`Insufficient ${selectedZapCurrency?.symbol} balance`)
+    : ''
 
   return (
     <>
-      <div className="px-5">
-        <Alert
-          dismissable={false}
-          type="information"
-          showIcon
-          message={i18n._(t`In Zap mode, your selected asset will be split and rebalanced into the corresponding tokens and their weights
+      {noLiquidity ? (
+        <div className="px-5 pt-5">
+          <Alert
+            dismissable={false}
+            type="error"
+            showIcon
+            message={i18n._(t`Zap mode is unavailable when there is no liquidity in the pool`)}
+          />
+        </div>
+      ) : (
+        <div className="px-5 pt-5">
+          <Alert
+            dismissable={false}
+            type="information"
+            showIcon
+            message={i18n._(t`In Zap mode, your selected asset will be split and rebalanced into the corresponding tokens and their weights
           automatically.`)}
-        />
-      </div>
-      <div className="flex flex-col gap-4 px-5">
+          />
+        </div>
+      )}
+
+      <div className="flex flex-col gap-3 px-5">
         <AssetInput
-          value={inputAmounts[inputTokenAddress]}
-          currency={currencies[inputTokenAddress]}
-          onChange={(value) => handleInput(value, inputTokenAddress, { clear: true })}
-          onSelect={(token: Token) => selectInputToken(token.address)}
+          value={zapInput}
+          currency={selectedZapCurrency}
+          onChange={setZapInput}
+          onSelect={setSelectedZapCurrency}
+          disabled={noLiquidity}
+          currencies={[NATIVE[chainId], pool?.token0, pool?.token1]}
         />
-        <Button
-          color={inputAmounts[inputTokenAddress] ? 'gradient' : 'gray'}
-          disabled={!validInput}
-          className="font-bold text-sm"
-          onClick={() => showReview(true)}
-        >
-          {inputAmounts[inputTokenAddress] ? i18n._(t`Confirm Deposit`) : i18n._(t`Select token & enter amount`)}
-        </Button>
+        <div className="flex flex-col gap-3">
+          <TridentApproveGate inputAmounts={[parsedZapAmount]} tokenApproveOn={bentoBox?.address}>
+            {({ loading, approved }) => (
+              <Button
+                {...(loading && {
+                  startIcon: (
+                    <div className="w-5 h-5 mr-1">
+                      <Lottie animationData={loadingCircle} autoplay loop />
+                    </div>
+                  ),
+                })}
+                color={zapInput ? 'gradient' : 'gray'}
+                disabled={!!error || !approved || attemptingTxn}
+                className="font-bold text-sm"
+                onClick={() => setShowReview(true)}
+              >
+                {attemptingTxn ? <Dots>Depositing</Dots> : loading ? '' : !error ? i18n._(t`Confirm Deposit`) : error}
+              </Button>
+            )}
+          </TridentApproveGate>
+        </div>
       </div>
       <div className="flex flex-col gap-4 px-5 mt-8">
         <Typography weight={700} className="text-high-emphesis">
-          {inputTokenAddress
-            ? i18n._(t`Your ${currencies[inputTokenAddress].symbol} will be split into:`)
+          {selectedZapCurrency
+            ? i18n._(t`Your ${selectedZapCurrency.symbol} will be split into:`)
             : i18n._(t`Your selected token will be split into:`)}
         </Typography>
         <ListPanel
-          items={Object.values(parsedOutputAmounts).map((amount, index) => (
-            <ListPanel.CurrencyAmountItem
-              amount={amount}
-              key={index}
-              weight={weights[amount?.currency?.wrapped.address]}
-            />
+          items={parsedZapSplitAmounts.map((amount, index) => (
+            <ListPanel.CurrencyAmountItem amount={amount} key={index} />
           ))}
         />
       </div>
-      {validInput && (
+      {!error && (
         <div className="mt-6 px-5">
           <TransactionDetails />
         </div>
