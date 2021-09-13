cbd1332defa21ee81fd9f7b4e8726fc1563c0879
omakasebar <omakasebar@gmail.com>
2021-09-14 01:23:35 +0800

    chore: fix &apos;

--- src/layouts/Default/index.tsx
+++ src/layouts/Default/index.tsx
@@ -4,10 +4,14 @@ import Header from '../../components/Header'
 import Main from '../../components/Main'
 import Popups from '../../components/Popups'
 
+import { useActiveWeb3React } from '../../hooks'
+import { ChainId } from '@sushiswap/sdk'
+
 const Layout = ({ children }) => {
+  const { chainId } = useActiveWeb3React()
   return (
     <div className="z-0 flex flex-col items-center w-full h-screen pb-16 lg:pb-0">
-      {/* <Banner /> */}
+      {chainId === ChainId.MAINNET ? <Banner /> : null}
       <Header />
       <Main>{children}</Main>
       <Popups />
