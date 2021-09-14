6674ad2bea2079833ed28ded1d37f68f5a614f8c
LufyCZ <lufy@sushi.com>
2021-09-14 13:33:42 +0000

    fix: rewardPerDay possibly undefined

--- src/features/analytics/Farms/FarmList.tsx
+++ src/features/analytics/Farms/FarmList.tsx
@@ -78,7 +78,7 @@ function Rewards({ rewards }: { rewards: Reward[] }): JSX.Element {
         </div>
         <div className="flex flex-col space-y-1">
           {rewards?.map((reward, i) => {
-            const decimals = 6 - String(reward.rewardPerDay.toFixed(0)).length
+            const decimals = 6 - String(reward?.rewardPerDay?.toFixed(0)).length
             return (
               <div key={i} className="text-base whitespace-nowrap">
                 {reward.rewardPerDay.toFixed(decimals > 0 ? decimals : 0)} {reward.token}
