2a8489f141636d1da59c20cda3f89b2dfc3d50a2
0xIvan <0xivanivan@gmail.com>
2021-09-22 20:58:48 +0200

    test: fix hooks tests

--- src/state/swap/hooks.test.ts
+++ src/state/swap/hooks.test.ts
@@ -1,8 +1,11 @@
 import { Field } from './actions'
 import { parse } from 'qs'
 import { queryParametersToSwapState } from './hooks'
+import { expect } from '@jest/globals'
+import { SUSHI_ADDRESS, ChainId } from '@sushiswap/sdk'
 
 describe('hooks', () => {
+  const sushiAddress = SUSHI_ADDRESS[ChainId.MAINNET]
   describe('#queryParametersToSwapState', () => {
     test('ETH to DAI', () => {
       expect(
@@ -32,8 +35,8 @@ describe('hooks', () => {
           })
         )
       ).toEqual({
-        [Field.INPUT]: { currencyId: '' },
-        [Field.OUTPUT]: { currencyId: 'ETH' },
+        [Field.INPUT]: { currencyId: 'ETH' },
+        [Field.OUTPUT]: { currencyId: sushiAddress },
         typedValue: '',
         independentField: Field.INPUT,
         recipient: null,
@@ -50,7 +53,7 @@ describe('hooks', () => {
         )
       ).toEqual({
         [Field.OUTPUT]: { currencyId: 'ETH' },
-        [Field.INPUT]: { currencyId: '' },
+        [Field.INPUT]: { currencyId: sushiAddress },
         typedValue: '20.5',
         independentField: Field.INPUT,
         recipient: null,
@@ -66,8 +69,8 @@ describe('hooks', () => {
           })
         )
       ).toEqual({
+        [Field.INPUT]: { currencyId: sushiAddress },
         [Field.OUTPUT]: { currencyId: 'ETH' },
-        [Field.INPUT]: { currencyId: '' },
         typedValue: '20.5',
         independentField: Field.INPUT,
         recipient: null,
@@ -84,7 +87,7 @@ describe('hooks', () => {
         )
       ).toEqual({
         [Field.OUTPUT]: { currencyId: 'ETH' },
-        [Field.INPUT]: { currencyId: '' },
+        [Field.INPUT]: { currencyId: sushiAddress },
         typedValue: '20.5',
         independentField: Field.INPUT,
         recipient: '0x0fF2D1eFd7A57B7562b2bf27F3f37899dB27F4a5',
@@ -100,7 +103,7 @@ describe('hooks', () => {
         )
       ).toEqual({
         [Field.OUTPUT]: { currencyId: 'ETH' },
-        [Field.INPUT]: { currencyId: '' },
+        [Field.INPUT]: { currencyId: sushiAddress },
         typedValue: '20.5',
         independentField: Field.INPUT,
         recipient: 'bob.argent.xyz',
