92262a6baff440a3003bb09e0a1752e9c63c0b78
siros-ena <sirosena@protonmail.com>
2021-09-23 18:18:42 -0400

    updated seo info

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -31,7 +31,7 @@ import { i18n } from '@lingui/core';
 import { persistStore } from 'redux-persist';
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
-import { APP_NAME_URL } from '../constants';
+import { APP_NAME_URL, APP_SHORT_BLURB } from '../constants';
 
 import { RecoilRoot } from 'recoil';
 
@@ -107,10 +107,10 @@ function MyApp({
 
         <meta key="description" name="description" content="Enabling lending and borrowing of any asset" />
 
-        <meta name="application-name" content="SUSHI App" />
+        <meta name="application-name" content="Silo Finance App" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-        <meta name="apple-mobile-web-app-title" content="SUSHI App" />
+        <meta name="apple-mobile-web-app-title" content="Silo Finance" />
 
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
@@ -119,24 +119,16 @@ function MyApp({
         <meta name="theme-color" content="#F338C3" />
 
         <meta key="twitter:card" name="twitter:card" content="app" />
-        <meta key="twitter:title" name="twitter:title" content="SUSHI App" />
-        <meta key="twitter:url" name="twitter:url" content="https://app.sushi.com" />
-        <meta
-          key="twitter:description"
-          name="twitter:description"
-          content="Be a DeFi Chef with Sushi. Swap, earn, stack yields, lend, borrow, leverage all on one decentralized, community driven platform. Welcome home to DeFi"
-        />
-        <meta key="twitter:image" name="twitter:image" content="https://app.sushi.com/icons/icon-192x192.png" />
-        <meta key="twitter:creator" name="twitter:creator" content="@SushiSwap" />
+        <meta key="twitter:title" name="twitter:title" content="Silo Finance  App" />
+        <meta key="twitter:url" name="twitter:url" content="https://app.silo.finance" />
+        <meta key="twitter:description" name="twitter:description" content={APP_SHORT_BLURB} />
+        <meta key="twitter:image" name="twitter:image" content="https://app.silo.finance/icons/icon-192x192.png" />
+        <meta key="twitter:creator" name="twitter:creator" content="@SiloFinance" />
         <meta key="og:type" property="og:type" content="website" />
-        <meta key="og:site_name" property="og:site_name" content="SUSHI App" />
-        <meta key="og:url" property="og:url" content="https://app.sushi.com" />
-        <meta key="og:image" property="og:image" content="https://app.sushi.com/apple-touch-icon.png" />
-        <meta
-          key="og:description"
-          property="og:description"
-          content="Be a DeFi Chef with Sushi. Swap, earn, stack yields, lend, borrow, leverage all on one decentralized, community driven platform. Welcome home to DeFi"
-        />
+        <meta key="og:site_name" property="og:site_name" content="Silo Finance App" />
+        <meta key="og:url" property="og:url" content="https://app.silo.finance" />
+        <meta key="og:image" property="og:image" content="https://app.silo.finance/apple-touch-icon.png" />
+        <meta key="og:description" property="og:description" content="Enabling lending and borrowing of any asset" />
       </Head>
       <RecoilRoot>
         <I18nProvider i18n={i18n} forceRenderOnLocaleChange={false}>
