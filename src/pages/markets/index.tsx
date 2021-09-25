09dc63e3e8a31c7f54cd23b170b91ddac114820a
siros-ena <sirosena@protonmail.com>
2021-09-25 17:29:39 -0400

    added urql, react query and silofactory abi

--- src/pages/markets/index.tsx
+++ src/pages/markets/index.tsx
@@ -13,6 +13,19 @@ import Web3Status from '../../components/Web3Status';
 import { Currency } from '@sushiswap/sdk';
 import { CurrencySearch } from '../../modals/SearchModal/CurrencySearch';
 
+/**
+ * !!! Contract on Chain Id (Rinkeby | Matic Mainnet)
+ */
+// rinkeby markets factory contract
+// 0xAAc3Fb8732F415f8DE86Ae5819E16d6233e78423
+
+/**
+ *
+ * !!! Graph on Chain Id (Rinkeby | Matic Mainnet)
+ */
+// graph url
+// https://api.studio.thegraph.com/query/9379/silo/0.3
+
 export default function Markets() {
   return (
     <Container id="supply-page" className="py-12 md:py-14 lg:py-16">
@@ -20,8 +33,19 @@ export default function Markets() {
         <title>{APP_NAME_URL}</title>
         <meta key="description" name="description" content={APP_SHORT_BLURB} />
       </Head>
-      <div className="p-4 rounded-lg shadow-lg bg-dark-900 text-secondary">
-        <h1 className="text-lg font-semibold">Markets</h1>
+      <div className="p-4 rounded-lg shadow-lg bg-dark-900 text-secondary flex justify-between">
+        <h1 className="text-lg font-semibold p-2.5">Markets</h1>
+        <div>
+          <Button
+            color="gradient"
+            className="text-gray-900 font-semibold"
+            onClick={() => {
+              console.log('createMarket.click()');
+            }}
+          >
+            Create Market
+          </Button>
+        </div>
       </div>
     </Container>
   );
