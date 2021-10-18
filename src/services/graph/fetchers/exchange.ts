50edc49aff9eb2be95d086a2426e648be8ad6c82
0xJiro <jirosushi@protonmail.com>
2021-10-17 19:17:14 -0700

    chore: switch celo exchange endpoint to prod

--- src/services/graph/fetchers/exchange.ts
+++ src/services/graph/fetchers/exchange.ts
@@ -25,7 +25,7 @@ export const EXCHANGE = {
   [ChainId.HARMONY]: 'sushiswap/harmony-exchange',
   [ChainId.OKEX]: 'sushiswap/okex-exchange',
   [ChainId.AVALANCHE]: 'sushiswap/avalanche-exchange',
-  [ChainId.CELO]: 'jiro-ono/sushitestsubgraph',
+  [ChainId.CELO]: 'sushiswap/celo-exchange',
   [ChainId.ARBITRUM]: 'sushiswap/arbitrum-exchange',
 }
 
