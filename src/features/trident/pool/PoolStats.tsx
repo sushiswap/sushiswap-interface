802d9f24d1feaf6a2c1052edda5d16dd04bf942e
LufyCZ <lufy@sushi.com>
2021-11-05 15:21:12 +0000

    refactor: extract usePoolStats

--- src/features/trident/pool/PoolStats.tsx
+++ src/features/trident/pool/PoolStats.tsx
@@ -4,63 +4,14 @@ import { t } from '@lingui/macro'
 import { useLingui } from '@lingui/react'
 import { formatNumber, formatPercent } from '../../../functions'
 import useDesktopMediaQuery from '../../../hooks/useDesktopMediaQuery'
-import { useBlock } from '../../../services/graph'
-import { useActiveWeb3React } from '../../../hooks'
-import { useTridentPools } from '../../../services/graph/hooks/pools'
-import { useRecoilValue } from 'recoil'
-import { poolAtom } from '../context/atoms'
+import { usePoolStats } from './usePoolStats'
 
 interface PoolStatsProps {}
 
 const PoolStats: FC<PoolStatsProps> = () => {
   const isDesktop = useDesktopMediaQuery()
-  const { i18n } = useLingui()
 
-  const { chainId } = useActiveWeb3React()
-
-  const poolAddress = useRecoilValue(poolAtom)?.[0]?.[2]
-
-  const block1d = useBlock({ chainId, daysAgo: 1 })
-  const block2d = useBlock({ chainId, daysAgo: 2 })
-
-  const pool = useTridentPools({ chainId, subset: [poolAddress] })?.[0]
-  const pool1d = useTridentPools({ chainId, subset: [poolAddress], block: block1d })?.[0]
-  const pool2d = useTridentPools({ chainId, subset: [poolAddress], block: block2d })?.[0]
-
-  const stats = useMemo(
-    () => [
-      {
-        label: i18n._(t`Volume (24H)`),
-        value: formatNumber(pool?.volumeUSD - pool1d?.volumeUSD, true, false),
-        change: ((pool?.volumeUSD - pool1d?.volumeUSD) / (pool1d?.volumeUSD - pool2d?.volumeUSD)) * 100 - 100,
-      },
-      {
-        label: i18n._(t`Fees (24H)`),
-        value: formatNumber(((pool?.volumeUSD - pool1d?.volumeUSD) / 100) * pool?.swapFeePercent, true, false),
-        change: ((pool?.volumeUSD - pool1d?.volumeUSD) / (pool1d?.volumeUSD - pool2d?.volumeUSD)) * 100 - 100,
-      },
-      {
-        label: i18n._(t`Utilization (24H)`),
-        value: formatPercent(((pool?.volumeUSD - pool1d?.volumeUSD) / pool?.totalValueLockedUSD) * 100),
-        change:
-          ((pool?.volumeUSD - pool1d?.volumeUSD) /
-            pool?.totalValueLockedUSD /
-            ((pool1d?.volumeUSD - pool2d?.volumeUSD) / pool1d?.totalValueLockedUSD)) *
-            100 -
-          100,
-      },
-      {
-        label: i18n._(t`Transactions (24H)`),
-        value: pool?.transactionCount - pool1d?.transactionCount,
-        change:
-          ((pool?.transactionCount - pool1d?.transactionCount) /
-            (pool1d?.transactionCount - pool2d?.transactionCount)) *
-            100 -
-          100,
-      },
-    ],
-    [i18n, pool, pool1d, pool2d]
-  )
+  const stats = usePoolStats()
 
   return (
     <div className="flex flex-col gap-3 lg:grid lg:grid-cols-4">
---
+++ src/features/trident/pool/usePoolStats.ts
@@ -0,0 +1,58 @@
+import { t } from '@lingui/macro'
+import { useLingui } from '@lingui/react'
+import { useMemo } from 'react'
+import { useRecoilValue } from 'recoil'
+import { formatNumber, formatPercent } from '../../../functions'
+import { useActiveWeb3React } from '../../../hooks'
+import { useBlock } from '../../../services/graph'
+import { useTridentPools } from '../../../services/graph/hooks/pools'
+import { poolAtom } from '../context/atoms'
+
+export function usePoolStats() {
+  const { i18n } = useLingui()
+  const { chainId } = useActiveWeb3React()
+
+  const { address: poolAddress } = useRecoilValue(poolAtom)
+
+  const block1d = useBlock({ chainId, daysAgo: 1 })
+  const block2d = useBlock({ chainId, daysAgo: 2 })
+
+  const pool = useTridentPools({ chainId, subset: [poolAddress] })?.[0]
+  const pool1d = useTridentPools({ chainId, subset: [poolAddress], block: block1d })?.[0]
+  const pool2d = useTridentPools({ chainId, subset: [poolAddress], block: block2d })?.[0]
+
+  return useMemo(
+    () => [
+      {
+        label: i18n._(t`Volume (24H)`),
+        value: formatNumber(pool?.volumeUSD - pool1d?.volumeUSD, true, false),
+        change: ((pool?.volumeUSD - pool1d?.volumeUSD) / (pool1d?.volumeUSD - pool2d?.volumeUSD)) * 100 - 100,
+      },
+      {
+        label: i18n._(t`Fees (24H)`),
+        value: formatNumber(((pool?.volumeUSD - pool1d?.volumeUSD) / 100) * pool?.swapFeePercent, true, false),
+        change: ((pool?.volumeUSD - pool1d?.volumeUSD) / (pool1d?.volumeUSD - pool2d?.volumeUSD)) * 100 - 100,
+      },
+      {
+        label: i18n._(t`Utilization (24H)`),
+        value: formatPercent(((pool?.volumeUSD - pool1d?.volumeUSD) / pool?.totalValueLockedUSD) * 100),
+        change:
+          ((pool?.volumeUSD - pool1d?.volumeUSD) /
+            pool?.totalValueLockedUSD /
+            ((pool1d?.volumeUSD - pool2d?.volumeUSD) / pool1d?.totalValueLockedUSD)) *
+            100 -
+          100,
+      },
+      {
+        label: i18n._(t`Transactions (24H)`),
+        value: pool?.transactionCount - pool1d?.transactionCount,
+        change:
+          ((pool?.transactionCount - pool1d?.transactionCount) /
+            (pool1d?.transactionCount - pool2d?.transactionCount)) *
+            100 -
+          100,
+      },
+    ],
+    [i18n, pool, pool1d, pool2d]
+  )
+}
