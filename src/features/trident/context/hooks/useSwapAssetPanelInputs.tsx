62a47a6513514ef5ce1a008d489537a4fd314aa3
Chilli Chelli <ramin@sushi.com>
2021-11-16 16:39:29 +0100

    fix: undefined error when deleting value from mainInputCurrencyAmount input

--- src/features/trident/context/hooks/useSwapAssetPanelInputs.tsx
+++ src/features/trident/context/hooks/useSwapAssetPanelInputs.tsx
@@ -154,7 +154,7 @@ const useSwapAssetPanelInputs = () => {
     ? i18n._(t`Enter an amount`)
     : trade === undefined && !isWrap
     ? i18n._(t`No route found`)
-    : balance && trade && maxAmountSpend(balance)?.lessThan(mainInputCurrencyAmount)
+    : balance && trade && mainInputCurrencyAmount && maxAmountSpend(balance).lessThan(mainInputCurrencyAmount)
     ? i18n._(t`Insufficient ${mainInputCurrencyAmount?.currency.symbol} balance`)
     : ''
 
