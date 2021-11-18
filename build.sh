cf91321a426d1e6e7fc3164c759664809063d164
sam bacha <sam@manifoldfinance.com>
2021-11-18 00:39:37 -0800

    build(prod): deploy 2021.11.18

--- build.sh
+++ build.sh
@@ -5,9 +5,6 @@ echo $BASH_VERSION
 export NEXT_TELEMETRY_DISABLED=1
 export NEXT_PUBLIC_OPENMEV_URI=https://api.sushirelay.com/v1
 
-printf -v beg '%(%s)T\n'
-sleep 1
-
 echo "Building..."
 yarn install --pure-lockfile
 
@@ -20,12 +17,11 @@ S_HASH=$(node -e 'process.stdout.write("sha256-");process.stdin.pipe(crypto.crea
 echo "${S_HASH}" >> public/.well-known/SRI_HASH
 git show -s --format=%ct HEAD > public/.well-known/commit-ts.txt
 
-printf 'https://app.sushi.com/%s\n' */* | sort > INDEX
+#printf 'https://app.sushi.com/%s\n' */* | sort > INDEX
 
-sleep 1
 
-printf -v now '%(%s)T\n'
-echo beg=$beg now=$now elapsed=$((now - beg))
+#printf -v now '%(%s)T\n'
+#echo beg=$beg now=$now elapsed=$((now - beg))
 
 
 exit 0
