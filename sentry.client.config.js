68064cb13ee04d7e503e073e30298cdc8770042b
sam bacha <sam@manifoldfinance.com>
2021-11-19 03:07:32 -0800

    build(release): 2021.11.19

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -7,7 +7,7 @@ import * as Sentry from '@sentry/nextjs'
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
 
 Sentry.init({
-  dsn: SENTRY_DSN || 'https://e852f945bc774d718d25aa807d8417dd@o960777.ingest.sentry.io/5909166',
+  dsn: SENTRY_DSN || "https://b3d1f140b36f441da22791ca98210c95@o1029417.ingest.sentry.io/6052500",
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   // ...
--- src/components/Settings/index.tsx
+++ src/components/Settings/index.tsx
@@ -140,7 +140,6 @@ export default function SettingsTab({ placeholderSlippage }: { placeholderSlippa
                   </Typography>
                   <QuestionHelper
                     text={i18n._(t`Sushi Relay can provide refunds of up to 90% of trading fees in xSushi`)}
-                    href={'https://docs.openmev.org'}
                   />
                 </div>
                 <Toggle
