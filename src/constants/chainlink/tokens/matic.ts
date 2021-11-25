2de5fe4c523e579d171980e03f92be4cfe8521ab
0xJiro <jiro@sushi.com>
2021-11-24 23:08:40 -0800

    chore: add tokens to matic kashi list

--- src/constants/chainlink/tokens/matic.ts
+++ src/constants/chainlink/tokens/matic.ts
@@ -36,12 +36,29 @@ const MATIC_CHAINLINK_TOKENS = [
     address: '0xc2132D05D31c914a87C6611C10748AEb04B58e8F',
     decimals: 6,
   },
-
   {
     symbol: 'WBTC',
     name: 'Wrapped Bitcoin',
     address: '0x1BFD67037B42Cf73acF2047067bd4F2C47D9BfD6',
     decimals: 8,
+  },
+  {
+    symbol: 'COMP',
+    name: 'Compound',
+    address: '0x8505b9d2254A7Ae468c0E9dd10Ccea3A837aef5c',
+    decimals: 18,
+  },
+  {
+    symbol: 'UMA',
+    name: 'UMA Voting Token',
+    address: '0x3066818837c5e6eD6601bd5a91B0762877A6B731',
+    decimals: 18,
+  },
+  {
+    symbol: 'BAL',
+    name: 'Balancer',
+    address: '0x9a71012B13CA4d3D0Cdc72A177DF3ef03b0E76A3',
+    decimals: 18,
   },
 ]
 
