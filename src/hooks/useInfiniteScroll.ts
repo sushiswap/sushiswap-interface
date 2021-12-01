ba4a97d8412fdce28fd128fca923830f046200ef
Matthew Lilley <hello@matthewlilley.com>
2021-12-01 04:43:01 +0000

    chore: extract infinite scroll hooks

---
+++ src/hooks/useInfiniteScroll.ts
@@ -0,0 +1,12 @@
+import { Dispatch, useEffect, useState } from 'react'
+
+/*
+  Currently expensive to render farm list item. The infinite scroll is used to
+  to minimize this impact. This hook pairs with it, keeping track of visible
+  items and passes this to <InfiniteScroll> component.
+*/
+export function useInfiniteScroll(items): [number, Dispatch<number>] {
+  const [itemsDisplayed, setItemsDisplayed] = useState(10)
+  useEffect(() => setItemsDisplayed(10), [items.length])
+  return [itemsDisplayed, setItemsDisplayed]
+}
