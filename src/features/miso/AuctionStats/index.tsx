78e071f522b53af2bd21bd14ea13e589fe845e1e
Jack Clancy <jack.clancy93@gmail.com>
2021-12-22 10:33:11 -0500

    chore: remove log

--- src/features/miso/AuctionStats/index.tsx
+++ src/features/miso/AuctionStats/index.tsx
@@ -41,8 +41,6 @@ const AuctionStats: FC<AuctionStatsProps> = ({ auction }) => {
 
   if (!auction) return <AuctionStatsSkeleton />
 
-  console.log({ auction })
-
   return (
     <div className="flex flex-col gap-6 w-full">
       <div className="grid grid-cols-2 lg:grid-cols-4 gap-6 lg:gap-2">
