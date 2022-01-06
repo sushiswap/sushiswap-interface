a1e2ea1096de9758e75a03a70b404379635835e4
Matthew Lilley <hello@matthewlilley.com>
2022-01-06 09:45:14 +0000

    fix: type error

--- src/functions/cloudinary.ts
+++ src/functions/cloudinary.ts
@@ -4,7 +4,7 @@ export const normalizeUrl = (src) => {
   return src[0] === '/' ? src.slice(1) : src
 }
 
-export const cloudinaryLoader: ImageLoader = ({ src, width = undefined, height = undefined }) => {
+export const cloudinaryLoader: ImageLoader & { height?: number } = ({ src, width = undefined, height = undefined }) => {
   return `https://res.cloudinary.com/sushi-cdn/image/fetch/${width ? `w_${width},` : ''}${
     height ? `h_${height},` : ''
   }f_auto,q_auto/${normalizeUrl(src)}`
