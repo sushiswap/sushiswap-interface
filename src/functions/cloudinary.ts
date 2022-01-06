d60a9537d65b5bfd9863cf266bfbdd88b2017aec
Matthew Lilley <hello@matthewlilley.com>
2022-01-06 09:45:58 +0000

    fix: type error and import

--- src/functions/cloudinary.ts
+++ src/functions/cloudinary.ts
@@ -1,10 +1,14 @@
-import { ImageLoader } from 'next/dist/client/image'
+import { ImageLoader, ImageLoaderProps } from 'next/image'
 
 export const normalizeUrl = (src) => {
   return src[0] === '/' ? src.slice(1) : src
 }
 
-export const cloudinaryLoader: ImageLoader & { height?: number } = ({ src, width = undefined, height = undefined }) => {
+export const cloudinaryLoader: ImageLoader = ({
+  src,
+  width = undefined,
+  height = undefined,
+}: ImageLoaderProps & { height?: number }) => {
   return `https://res.cloudinary.com/sushi-cdn/image/fetch/${width ? `w_${width},` : ''}${
     height ? `h_${height},` : ''
   }f_auto,q_auto/${normalizeUrl(src)}`
