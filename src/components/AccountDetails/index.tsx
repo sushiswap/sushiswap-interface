0b9c06bd84dd7c55a2c270ade818c8c5fd5b987f
Chilli Chelli <ramin@sushi.com>
2022-01-11 16:31:34 +0100

    style: redesign account details modal

--- src/components/AccountDetails/index.tsx
+++ src/components/AccountDetails/index.tsx
@@ -1,5 +1,6 @@
 import { t } from '@lingui/macro'
 import { useLingui } from '@lingui/react'
+import { HeadlessUiModal } from 'app/components/Modal'
 import { injected, SUPPORTED_WALLETS } from 'app/config/wallets'
 import { getExplorerLink } from 'app/functions/explorer'
 import { shortenAddress } from 'app/functions/format'
@@ -7,13 +8,13 @@ import { useActiveWeb3React } from 'app/services/web3'
 import { AppDispatch } from 'app/state'
 import { clearAllTransactions } from 'app/state/transactions/actions'
 import Image from 'next/image'
-import React, { FC, useCallback } from 'react'
+import React, { FC, useCallback, useMemo } from 'react'
+import Identicon from 'react-blockies'
 import { ExternalLink as LinkIcon } from 'react-feather'
 import { useDispatch } from 'react-redux'
 
 import Button from '../Button'
 import ExternalLink from '../ExternalLink'
-import ModalHeader from '../ModalHeader'
 import Typography from '../Typography'
 import Copy from './Copy'
 import Transaction from './Transaction'
@@ -28,13 +29,9 @@ const WalletIcon: FC<{ size?: number; src: string; alt: string }> = ({ size, src
 }
 
 function renderTransactions(transactions: string[]) {
-  return (
-    <div className="flex flex-col gap-2 flex-nowrap">
-      {transactions.map((hash, i) => {
-        return <Transaction key={i} hash={hash} />
-      })}
-    </div>
-  )
+  return transactions.map((hash, i) => {
+    return <Transaction key={i} hash={hash} />
+  })
 }
 
 interface AccountDetailsProps {
@@ -53,10 +50,10 @@ const AccountDetails: FC<AccountDetailsProps> = ({
   openOptions,
 }) => {
   const { i18n } = useLingui()
-  const { chainId, account, connector } = useActiveWeb3React()
+  const { chainId, account, connector, deactivate } = useActiveWeb3React()
   const dispatch = useDispatch<AppDispatch>()
 
-  function formatConnectorName() {
+  const connectorName = useMemo(() => {
     const { ethereum } = window
     const isMetaMask = !!(ethereum && ethereum.isMetaMask)
     const name = Object.keys(SUPPORTED_WALLETS)
@@ -65,13 +62,16 @@ const AccountDetails: FC<AccountDetailsProps> = ({
           SUPPORTED_WALLETS[k].connector === connector && (connector !== injected || isMetaMask === (k === 'METAMASK'))
       )
       .map((k) => SUPPORTED_WALLETS[k].name)[0]
-    return <div className="font-medium text-baseline text-secondary">Connected with {name}</div>
-  }
+    return (
+      <Typography variant="xs" weight={700} className="text-secondary">
+        Connected with {name}
+      </Typography>
+    )
+  }, [connector])
 
-  function getStatusIcon() {
+  const statusIcon = useMemo(() => {
     if (connector === injected) {
-      return null
-      // return <IconWrapper size={16}>{/* <Identicon /> */}</IconWrapper>
+      return <Identicon seed={account} />
     } else if (connector.constructor.name === 'WalletConnectConnector') {
       return <WalletIcon src="/wallet-connect.png" alt="Wallet Connect" size={16} />
     } else if (connector.constructor.name === 'WalletLinkConnector') {
@@ -95,59 +95,30 @@ const AccountDetails: FC<AccountDetailsProps> = ({
       return <WalletIcon src="/torus.png" alt="Torus" size={16} />
     }
     return null
-  }
+  }, [account, connector])
 
   const clearAllTransactionsCallback = useCallback(() => {
     if (chainId) dispatch(clearAllTransactions({ chainId }))
   }, [dispatch, chainId])
 
   return (
-    <div className="space-y-3 p-6">
+    <div className="space-y-3">
       <div className="space-y-3">
-        <ModalHeader title="Account" onClose={toggleWalletModal} />
-        <div className="space-y-3">
+        <HeadlessUiModal.Header header={i18n._(t`Account`)} onClose={toggleWalletModal} />
+        <HeadlessUiModal.BorderedContent className="flex flex-col gap-3">
           <div className="flex items-center justify-between">
-            {formatConnectorName()}
-            <div className="flex space-x-3">
-              {connector === injected &&
-                connector.constructor.name !== 'WalletLinkConnector' &&
-                connector.constructor.name !== 'BscConnector' &&
-                connector.constructor.name !== 'KeystoneConnector' && (
-                  <Button
-                    variant="outlined"
-                    color="gray"
-                    size="xs"
-                    onClick={() => {
-                      ;(connector as any).close()
-                    }}
-                  >
-                    {i18n._(t`Disconnect`)}
-                  </Button>
-                )}
-              <Button
-                variant="outlined"
-                color="gray"
-                size="xs"
-                onClick={() => {
-                  openOptions()
-                }}
-              >
-                {i18n._(t`Change`)}
-              </Button>
-            </div>
+            {connectorName}
+            <Button variant="outlined" color="blue" size="xs" onClick={deactivate}>
+              {i18n._(t`Disconnect`)}
+            </Button>
           </div>
-          <div id="web3-account-identifier-row" className="flex flex-col justify-center space-y-3">
-            {ENSName ? (
-              <div className="bg-dark-800">
-                {getStatusIcon()}
-                <Typography>{ENSName}</Typography>
-              </div>
-            ) : (
-              <div className="px-3 py-2 rounded bg-dark-800">
-                {getStatusIcon()}
-                <Typography>{account && shortenAddress(account)}</Typography>
-              </div>
-            )}
+          <div id="web3-account-identifier-row" className="flex flex-col justify-center gap-4">
+            <div className="flex gap-4 items-center">
+              <div className="rounded-full overflow-hidden">{statusIcon}</div>
+              <Typography weight={700} variant="lg" className="text-white">
+                {ENSName ? ENSName : account && shortenAddress(account)}
+              </Typography>
+            </div>
             <div className="flex items-center gap-2 space-x-3">
               {chainId && account && (
                 <ExternalLink
@@ -155,37 +126,47 @@ const AccountDetails: FC<AccountDetailsProps> = ({
                   startIcon={<LinkIcon size={16} />}
                   href={chainId && getExplorerLink(chainId, ENSName || account, 'address')}
                 >
-                  <Typography variant="sm">{i18n._(t`View on explorer`)}</Typography>
+                  <Typography variant="xs" weight={700}>
+                    {i18n._(t`View on explorer`)}
+                  </Typography>
                 </ExternalLink>
               )}
               {account && (
                 <Copy toCopy={account}>
-                  <Typography variant="sm">{i18n._(t`Copy Address`)}</Typography>
+                  <Typography variant="xs" weight={700}>
+                    {i18n._(t`Copy Address`)}
+                  </Typography>
                 </Copy>
               )}
             </div>
           </div>
-        </div>
-      </div>
-      <div className="space-y-2">
-        <div className="flex items-center justify-between">
-          <Typography weight={700}>{i18n._(t`Recent Transactions`)}</Typography>
-          <div>
-            <Button variant="outlined" color="gray" size="xs" onClick={clearAllTransactionsCallback}>
+        </HeadlessUiModal.BorderedContent>
+        <HeadlessUiModal.BorderedContent className="flex flex-col gap-3">
+          <div className="flex items-center justify-between">
+            <Typography variant="xs" weight={700} className="text-secondary">
+              {i18n._(t`Recent Transactions`)}
+            </Typography>
+            <Button variant="outlined" color="blue" size="xs" onClick={clearAllTransactionsCallback}>
               {i18n._(t`Clear all`)}
             </Button>
           </div>
-        </div>
-        {!!pendingTransactions.length || !!confirmedTransactions.length ? (
-          <>
-            {renderTransactions(pendingTransactions)}
-            {renderTransactions(confirmedTransactions)}
-          </>
-        ) : (
-          <Typography variant="sm" className="text-secondary">
-            {i18n._(t`Your transactions will appear here...`)}
-          </Typography>
-        )}
+          <div className="flex flex-col divide-y divide-dark-800">
+            {!!pendingTransactions.length || !!confirmedTransactions.length ? (
+              <>
+                {pendingTransactions.map((el, index) => (
+                  <Transaction key={index} hash={el} />
+                ))}
+                {confirmedTransactions.map((el, index) => (
+                  <Transaction key={index} hash={el} />
+                ))}
+              </>
+            ) : (
+              <Typography variant="xs" weight={700} className="text-secondary">
+                {i18n._(t`Your transactions will appear here...`)}
+              </Typography>
+            )}
+          </div>
+        </HeadlessUiModal.BorderedContent>
       </div>
     </div>
   )
