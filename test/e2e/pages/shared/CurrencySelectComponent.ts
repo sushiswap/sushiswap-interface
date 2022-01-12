4c1aec434e14153c07d19c05febe234ab6b8b566
bayological <6872903+bayological@users.noreply.github.com>
2022-01-12 12:02:32 -0500

    refactor: remove unused code

--- test/e2e/pages/shared/CurrencySelectComponent.ts
+++ test/e2e/pages/shared/CurrencySelectComponent.ts
@@ -10,17 +10,8 @@ export class CurrencySelectComponent extends AppPageComponent {
     await this.Page.waitForSelector(this.AllCurrenciesListSelector)
     await this.blockingWait(3)
 
-    const nativeTokenButton = await this.Page.$(this.SelectTokenResultsSelector + tokenSymbol)
-    if (nativeTokenButton) {
-      await this.blockingWait(2)
-      await nativeTokenButton.click()
-    } else {
-      const selectTokenInput = await this.Page.waitForSelector(this.SelectTokenInputSelector)
-      selectTokenInput.type(tokenSymbol)
-      await this.blockingWait(2)
-
-      const tokenButton = await this.Page.$(this.SelectTokenResultsSelector + tokenSymbol)
-      await tokenButton.click()
-    }
+    const tokenSelector = this.SelectTokenResultsSelector + tokenSymbol
+    const tokenButton = await this.Page.waitForSelector(tokenSelector)
+    await tokenButton.click()
   }
 }
