c08169324b9ab8f787b08634b14b46df32cc6e84
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2022-01-13 16:15:25 +0100

    feat: added support to OpenMEV tx protection

--- src/state/user/hooks.tsx
+++ src/state/user/hooks.tsx
@@ -37,6 +37,7 @@ import {
   updateUserExpertMode,
   updateUserSingleHopOnly,
   updateUserSlippageTolerance,
+  updateUserUseOpenMev,
 } from './actions'
 
 function serializeToken(token: Token): SerializedToken {
@@ -415,4 +416,20 @@ export function useUserSlippageToleranceWithDefault(defaultSlippageTolerance: Pe
     () => (allowedSlippage === 'auto' ? defaultSlippageTolerance : allowedSlippage),
     [allowedSlippage, defaultSlippageTolerance]
   )
+}
+
+/**
+ * Returns a boolean indicating if the user has enabled OpenMEV protection.
+ */
+export function useUserOpenMev(): [boolean, (newUseOpenMev: boolean) => void] {
+  const dispatch = useAppDispatch()
+
+  const useOpenMev = useSelector<AppState, AppState['user']['useOpenMev']>((state) => state.user.userUseOpenMev)
+
+  const setUseOpenMev = useCallback(
+    (newUseOpenMev: boolean) => dispatch(updateUserUseOpenMev({ userUseOpenMev: newUseOpenMev })),
+    [dispatch]
+  )
+
+  return [useOpenMev, setUseOpenMev]
 }
