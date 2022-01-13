a58dea3daaf5c3718e663eccd2f2d0c49933d6dc
Chilli Chelli <ramin@sushi.com>
2022-01-13 18:53:50 +0100

    style: restyle trident modals with new headless modal design

--- src/features/trident/TransactionDetailsExplanationModal.tsx
+++ src/features/trident/TransactionDetailsExplanationModal.tsx
@@ -1,8 +1,5 @@
-import { XIcon } from '@heroicons/react/solid'
 import { t } from '@lingui/macro'
 import { useLingui } from '@lingui/react'
-import Alert from 'app/components/Alert'
-import Button from 'app/components/Button'
 import HeadlessUiModal from 'app/components/Modal/HeadlessUIModal'
 import Typography from 'app/components/Typography'
 import React, { FC } from 'react'
@@ -13,18 +10,11 @@ const TransactionDetailsExplanationModal: FC = ({ children }) => {
   return (
     <HeadlessUiModal trigger={children}>
       {({ setOpen }) => (
-        <div className="flex flex-col gap-9 px-7 py-9 lg:max-w-2xl">
-          <div className="flex justify-between gap-3">
-            <Typography variant="h3" weight={700} className="text-high-emphesis">
-              {i18n._(t`Transaction Details`)}
-            </Typography>
-            <div role="button" className="h-8 w-8 cursor-pointer" onClick={() => setOpen(false)}>
-              <XIcon className="text-high-emphesis" />
-            </div>
-          </div>
-          <div className="flex flex-col gap-6">
+        <div className="flex flex-col gap-4 lg:max-w-2xl">
+          <HeadlessUiModal.Header header={i18n._(t`Transaction Details`)} onClose={() => setOpen(false)} />
+          <HeadlessUiModal.BorderedContent className="flex flex-col gap-4 border-dark-800">
             <div className="flex flex-col gap-2">
-              <Typography weight={700} className="text-high-emphesis">
+              <Typography weight={700} variant="sm" className="text-high-emphesis">
                 {i18n._(t`Minimum Received`)}
               </Typography>
               <Typography variant="sm">
@@ -34,34 +24,31 @@ const TransactionDetailsExplanationModal: FC = ({ children }) => {
               </Typography>
             </div>
             <div className="flex flex-col gap-2">
-              <Typography weight={700} className="text-high-emphesis">
+              <Typography weight={700} variant="sm" className="text-high-emphesis">
                 {i18n._(t`Price Impact`)}
               </Typography>
               <Typography variant="sm">
                 {i18n._(
                   t`The difference between market price and estimated price due to the proportional makeup of the assets deposited.`
-                )}
+                )}{' '}
               </Typography>
             </div>
-          </div>
-          <Typography variant="h3" weight={700} className="text-high-emphesis">
-            {i18n._(t`Fee Breakdown`)}
-          </Typography>
-          <div className="flex flex-col gap-6">
+          </HeadlessUiModal.BorderedContent>
+          <HeadlessUiModal.BorderedContent className="flex flex-col gap-4">
             <div className="flex flex-col gap-2">
-              <Typography weight={700} className="text-high-emphesis">
+              <Typography weight={700} variant="sm" className="text-high-emphesis">
                 {i18n._(t`Liquidity Provider Fee`)}
               </Typography>
               <Typography variant="sm">{i18n._(t`0.25% of each swap goes to liquidity providers.`)}</Typography>
             </div>
             <div className="flex flex-col gap-2">
-              <Typography weight={700} className="text-high-emphesis">
+              <Typography weight={700} variant="sm" className="text-high-emphesis">
                 {i18n._(t`xSUSHI Fee`)}
               </Typography>
-              <Typography variant="sm">{i18n._(t`0.055% of each swap goes to xSUSHI holders.`)}</Typography>
+              <Typography variant="sm">{i18n._(t`0.055% of each swap goes to xSUSHI holders.`)} </Typography>
             </div>
             <div className="flex flex-col gap-2">
-              <Typography weight={700} className="text-high-emphesis">
+              <Typography weight={700} variant="sm" className="text-high-emphesis">
                 {i18n._(t`Estimated network Fee`)}
               </Typography>
               <Typography variant="sm">
@@ -70,18 +57,14 @@ const TransactionDetailsExplanationModal: FC = ({ children }) => {
                 )}
               </Typography>
             </div>
-            <Alert
-              showIcon
-              dismissable={false}
-              type="information"
-              message={i18n._(
+          </HeadlessUiModal.BorderedContent>
+          <HeadlessUiModal.BorderedContent className="border-purple/60">
+            <Typography variant="sm">
+              {i18n._(
                 t`Depositing with Zap Mode involves swapping your asset for the assets in the pool - this makes your transaction subject to swap-related fees.`
               )}
-            />
-          </div>
-
-          {/*HeadlessUIModals need a focusable element so this is a hack..*/}
-          <Button />
+            </Typography>
+          </HeadlessUiModal.BorderedContent>
         </div>
       )}
     </HeadlessUiModal>
