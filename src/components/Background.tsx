aef70663fd61d4d2b2c96e31528bd352a760cc27
Chilli Chelli <ramin@sushi.com>
2022-01-13 13:17:56 +0100

    fix: build errors

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+import Image from 'app/components/Image'
 import { FC } from 'react'
 
 interface Background {
--- src/functions/cloudinary.ts
+++ src/functions/cloudinary.ts
@@ -1,21 +1,15 @@
-import { ImageLoaderProps } from 'next/image'
+import { ImageLoader, ImageLoaderProps } from 'next/image'
 
 export const normalizeUrl = (src) => {
   return src[0] === '/' ? src.slice(1) : src
 }
 
-interface CloudinaryLoader extends Omit<ImageLoaderProps, 'width' | 'height'> {
-  src: string
-  width?: string
-  height?: string
-}
-
-type CloudinaryLoaderType = (T: CloudinaryLoader) => string
-
-export const cloudinaryLoader: CloudinaryLoaderType = ({ src, width, height }) => {
+export const cloudinaryLoader: ImageLoader = ({
+  src,
+  width = undefined,
+  height = undefined,
+}: ImageLoaderProps & { height?: number }) => {
   return `https://res.cloudinary.com/sushi-cdn/image/fetch/${width ? `w_${width},` : ''}${
     height ? `h_${height},` : ''
   }f_auto,q_auto/${normalizeUrl(src)}`
 }
-
-export default cloudinaryLoader
