f9fb850ab5c265a4be13bc3673678fdf47734302
Chilli Chelli <ramin@sushi.com>
2022-01-17 16:51:38 +0100

    fix(LiquidityLauncher): errors with undefined liquidityLauncher

--- src/features/miso/AuctionTabs/AuctionDetailsTab.tsx
+++ src/features/miso/AuctionTabs/AuctionDetailsTab.tsx
@@ -37,27 +37,35 @@ const AuctionDetailsTab: FC<AuctionDetailsTabProps> = ({ auction, active }) => {
     <div className={classNames(active ? 'block' : 'hidden', 'grid grid-cols-2 gap-6')}>
       <AuctionDetailsTabStat
         label={i18n._(t`Liquidity Locked Until`)}
-        value={new Date(Number(auction.launcherInfo?.unlock.mul(1000))).toLocaleString('en-uS', {
-          year: 'numeric',
-          month: 'long',
-          day: 'numeric',
-          hour: 'numeric',
-          minute: 'numeric',
-          timeZone: 'UTC',
-        })}
+        value={
+          auction.launcherInfo
+            ? new Date(Number(auction.launcherInfo.unlock.mul(1000))).toLocaleString('en-uS', {
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric',
+                hour: 'numeric',
+                minute: 'numeric',
+                timeZone: 'UTC',
+              })
+            : i18n._(t`No liquidity locked`)
+        }
       />
       <AuctionDetailsTabStat
         label={i18n._(t`Liquidity`)}
         value={
-          <>
-            <div className="flex gap-1">
-              {auction.totalTokens
-                ?.multiply(new Percent(Number(auction.launcherInfo?.liquidityPercent), 10000))
-                ?.toSignificant(6)}{' '}
-              {auction.totalTokens?.currency.symbol} + {Number(auction.launcherInfo?.liquidityPercent) / 100}% of{' '}
-              {auction.paymentToken.symbol} proceeds
-            </div>
-          </>
+          auction.launcherInfo ? (
+            <>
+              <div className="flex gap-1">
+                {auction.totalTokens
+                  ?.multiply(new Percent(Number(auction.launcherInfo.liquidityPercent), 10000))
+                  ?.toSignificant(6)}{' '}
+                {auction.totalTokens?.currency.symbol} + {Number(auction.launcherInfo.liquidityPercent) / 100}% of{' '}
+                {auction.paymentToken.symbol} proceeds
+              </div>
+            </>
+          ) : (
+            i18n._(t`No liquidity locked`)
+          )
         }
       />
       <AuctionDetailsTabStat
