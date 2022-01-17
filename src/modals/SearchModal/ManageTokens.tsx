23ff4e0508c236f4a707a6eecc450756107d34d8
Matthew Lilley <hello@matthewlilley.com>
2022-01-17 14:33:27 +0000

    fix: alignment of custom tokens text & clear

--- src/modals/SearchModal/ManageTokens.tsx
+++ src/modals/SearchModal/ManageTokens.tsx
@@ -50,22 +50,22 @@ const ManageTokens: FC = () => {
     return (
       chainId &&
       userAddedTokens.map((token) => (
-        <div className="flex justify-between py-3 px-4 hover:bg-dark-800/40" key={token.address}>
-          <div className="flex gap-3 items-center">
+        <div className="flex justify-between px-4 py-3 hover:bg-dark-800/40" key={token.address}>
+          <div className="flex items-center gap-3">
             <CurrencyLogo currency={token} size={32} className="!rounded-full overflow-hidden" />
             <ExternalLink href={getExplorerLink(chainId, token.address, 'address')}>
               <Typography weight={700}>{token.symbol}</Typography>
             </ExternalLink>
           </div>
-          <div className="flex gap-2 items-center">
+          <div className="flex items-center gap-2">
             <div
-              className="w-4 h-4 cursor-pointer hover:text-white items-center flex"
+              className="flex items-center w-4 h-4 cursor-pointer hover:text-white"
               onClick={() => removeToken(chainId, token.address)}
             >
               <Trash width={20} />
             </div>
             <ExternalLinkIcon
-              className="min-w-4 min-h-4 cursor-pointer hover:text-white items-center flex"
+              className="flex items-center cursor-pointer min-w-4 min-h-4 hover:text-white"
               href={getExplorerLink(chainId, token.address, 'address')}
             />
           </div>
@@ -99,7 +99,7 @@ const ManageTokens: FC = () => {
       />
       {searchToken && <ImportRow token={searchToken} onClick={handleImport} />}
       <HeadlessUiModal.BorderedContent className="flex flex-col bg-[rgba(0,0,0,0.2)] !p-0">
-        <div className="flex justify-between p-4 pb-2">
+        <div className="flex items-center justify-between p-4">
           <Typography variant="xs" weight={700} className="text-secondary">
             {userAddedTokens?.length} Custom {userAddedTokens.length === 1 ? 'Token' : 'Tokens'}
           </Typography>
@@ -110,7 +110,7 @@ const ManageTokens: FC = () => {
         <div className="divide-y divide-dark-800">{tokenList}</div>
       </HeadlessUiModal.BorderedContent>
       <div className="flex flex-grow h-full" />
-      <Typography variant="xs" weight={700} className="text-secondary text-center">
+      <Typography variant="xs" weight={700} className="text-center text-secondary">
         {i18n._(t`Custom tokens are stored locally in your browser`)}
       </Typography>
     </>
