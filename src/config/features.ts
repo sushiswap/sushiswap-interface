67551189e273decafd76e96396e567edae02b864
Matthew Lilley <hello@matthewlilley.com>
2022-01-19 02:23:17 +0000

    chore: remove feature flag

--- src/config/features.ts
+++ src/config/features.ts
@@ -33,8 +33,7 @@ const features: FeatureMap = {
   [ChainId.FANTOM_TESTNET]: [Feature.AMM],
   [ChainId.MATIC]: [
     Feature.AMM,
-    Feature.LIQUIDITY_MINING,
-    Feature.LIMIT_ORDERS,
+    Feature.LIQUIDITY_MINING
     Feature.BENTOBOX,
     Feature.KASHI,
     Feature.MIGRATE,
@@ -43,7 +42,7 @@ const features: FeatureMap = {
   [ChainId.MATIC_TESTNET]: [Feature.AMM],
   [ChainId.HARMONY]: [Feature.AMM, Feature.LIQUIDITY_MINING, Feature.ANALYTICS],
   [ChainId.HARMONY_TESTNET]: [Feature.AMM],
-  [ChainId.AVALANCHE]: [Feature.AMM, Feature.BENTOBOX, Feature.KASHI, Feature.LIMIT_ORDERS],
+  [ChainId.AVALANCHE]: [Feature.AMM, Feature.BENTOBOX, Feature.KASHI],
   [ChainId.AVALANCHE_TESTNET]: [Feature.AMM],
   [ChainId.OKEX]: [Feature.AMM],
   [ChainId.OKEX_TESTNET]: [Feature.AMM],
