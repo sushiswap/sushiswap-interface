f269de83c8b18c394b347a8769f716b3cff74de5
Matthew Lilley <hello@matthewlilley.com>
2022-01-19 23:08:40 +0000

    config: wdyr

---
+++ babel.config.js
@@ -0,0 +1,30 @@
+// babel.config.js
+module.exports = function (api) {
+  const isServer = api.caller((caller) => caller?.isServer)
+  const isCallerDevelopment = api.caller((caller) => caller?.isDev)
+
+  const presets = [
+    [
+      'next/babel',
+      {
+        'preset-react': {
+          importSource: !isServer && isCallerDevelopment ? '@welldone-software/why-did-you-render' : 'react',
+        },
+      },
+    ],
+  ]
+
+  return { presets, plugins: ['macros'] }
+}
+
+// module.exports = {
+//   presets: [
+//     'next/babel',
+//     {
+//       'preset-react': {
+//         importSource: !isServer && isCallerDevelopment ? '@welldone-software/why-did-you-render' : 'react',
+//       },
+//     },
+//   ],
+//   plugins: ['macros'],
+// }
