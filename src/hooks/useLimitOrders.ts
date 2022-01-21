b8e068dfb5903bab17b36af1619124eb5d6b8c6c
Matthew Lilley <hello@matthewlilley.com>
2022-01-21 03:53:18 +0000

    feat: show filled percentage

--- src/hooks/useLimitOrders.ts
+++ src/hooks/useLimitOrders.ts
@@ -1,4 +1,3 @@
-import { BigNumber } from '@ethersproject/bignumber'
 import { JSBI, Percent, Token } from '@sushiswap/core-sdk'
 import { LAMBDA_URL, LimitOrder, OrderStatus } from '@sushiswap/limit-order-sdk'
 import { useActiveWeb3React } from 'app/services/web3'
@@ -41,7 +40,7 @@ const viewUrl = `${LAMBDA_URL}/orders/view`
 const viewFetcher = (url, account, chainId, pendingPage, page) => {
   return fetch(url, {
     method: 'POST',
-    body: JSON.stringify({ address: account, chainId, page, pendingPage }),
+    body: JSON.stringify({ address: '0xad8F72A7612Bb91B2dfaB09E54464aaA5150914E', chainId, page, pendingPage }),
   })
     .then((r) => r.json())
     .then((j) => j.data)
@@ -130,9 +129,7 @@ const useLimitOrders = () => {
           tokens[tokenOut.address] ||
           new Token(chainId, tokenOut.address.toLowerCase(), tokenOut.decimals, tokenOut.symbol),
         limitOrder,
-        filledPercent: order.filledAmount
-          ? order.filledAmount.mul(BigNumber.from('100')).div(BigNumber.from(order.amountIn)).toString()
-          : '0',
+        filledPercent: order.filledAmount ? Number((order.filledAmount / order.amountIn) * 100).toFixed(2) : '0.00',
         status: order.status,
         rate: new Percent(limitOrder.amountOut.quotient, denominator(tokenOut.decimals))
           .divide(new Percent(limitOrder.amountIn.quotient, denominator(tokenIn.decimals)))
@@ -145,7 +142,11 @@ const useLimitOrders = () => {
 
     ;(async () => {
       // @ts-ignore TYPE NEEDS FIXING
-      const openOrders = await Promise.all<OpenOrder>(ordersData.pendingOrders.orders.map((el) => transform(el)))
+      const openOrders = await Promise.all<OpenOrder>(
+        ordersData.pendingOrders.orders.map((el, i) =>
+          transform({ ...el, filledAmount: ordersData.pendingOrders.filledAmounts[i] })
+        )
+      )
       // @ts-ignore TYPE NEEDS FIXING
       const completedOrders = await Promise.all<OpenOrder>(ordersData.otherOrders.orders.map((el) => transform(el)))
 
