6ec7c62339cb13912fa629daf0ea81a41bb06db9
Chilli Chelli <ramin@sushi.com>
2022-01-22 19:53:25 +0100

    feat(LimitOrders): add subheader

--- src/features/legacy/limit-order/DiscoverHeader.tsx
+++ src/features/legacy/limit-order/DiscoverHeader.tsx
@@ -15,9 +15,9 @@ const DiscoverHeader = () => {
         <Typography variant="h2" className="text-high-emphesis" weight={700}>
           {i18n._(t`Limit Orders`)}
         </Typography>
-        {/*<Typography variant="sm" weight={400}>*/}
-        {/*  {i18n._(t`Earn fees and rewards by depositing and staking your tokens to the platform.`)}*/}
-        {/*</Typography>*/}
+        <Typography variant="sm" weight={400}>
+          {i18n._(t`Place a limit order or check the status of your past orders`)}
+        </Typography>
       </div>
       <div className="flex gap-3">
         <Link href="/limit-order" passHref={true}>
