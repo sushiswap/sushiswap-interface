32157cdc64945a1e63a6781378cc94c232cd06fe
Chilli Chelli <ramin@sushi.com>
2022-02-03 18:51:40 +0100

    fix: query param formatting with undefined

--- src/functions/currency/wrappedCurrency.ts
+++ src/functions/currency/wrappedCurrency.ts
@@ -2,18 +2,15 @@ import { ChainId, Currency, NATIVE, WNATIVE } from '@sushiswap/core-sdk'
 
 export function unwrappedToken(currency: Currency): Currency {
   if (currency.isNative) return currency
-
-  // @ts-ignore TYPE NEEDS FIXING
   if (currency.chainId in ChainId && currency.equals(WNATIVE[currency.chainId])) return NATIVE[currency.chainId]
 
   return currency
 }
 
-export const isWrappedReturnNativeSymbol = (chainId: ChainId | undefined, address: string) => {
+export const isWrappedReturnNativeSymbol = (chainId: ChainId | undefined, address: string): string => {
   if (!chainId) return address
   if (address.toLowerCase() === WNATIVE[chainId].address.toLowerCase()) {
-    // @ts-ignore TYPE NEEDS FIXING
-    return NATIVE[chainId].symbol
+    return NATIVE[chainId].symbol || address
   }
 
   return address
