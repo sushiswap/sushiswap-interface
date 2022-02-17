d463b6e3d5554f3967ca1eb5b4d73b77f36c54a8
Matthew Lilley <hello@matthewlilley.com>
2022-02-17 17:22:03 +0000

    chore: rm console log

--- src/features/miso/AuctionStats/ChartCard.tsx
+++ src/features/miso/AuctionStats/ChartCard.tsx
@@ -36,8 +36,6 @@ export const ChartCard = ({ auction }: { auction: Auction }) => {
 
   useEffect(() => setSelectedBlock(commitments.length - 1), [commitments])
 
-  console.log(parsedAuctionCommitments)
-
   return (
     <div className="flex flex-col bg-[rgba(255,255,255,0.04)] border border-dark-900 rounded gap-5 shadow-2xl shadow-pink-red/5 h-full">
       <div className="flex justify-between gap-6 p-5">
