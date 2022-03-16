eae5429c7c191d55a61667df50f55be08b8ad864
sam bacha <sam@manifoldfinance.com>
2022-03-16 01:32:35 -0700

    fix(dashboard): defect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,25 @@
-import Container from 'app/components/Container';
-import { useEffect, useState } from 'react
-import { TitleAndMetaTags } from 'app/constants/TitleAndMetaTags';
-import Head from 'next/head';
-
+import Container from "app/components/Container";
+import { useEffect, useState } from "react";
+import { TitleAndMetaTags } from "app/constants/TitleAndMetaTags";
+import Head from "next/head";
 
 const Dashboard = () => {
   return (
-    <Container id="dashboard-page" className="py-4 md:py-8 lg:py-12" maxWidth="2xl">
-     
+    <Container
+      id="dashboard-page"
+      className="py-4 md:py-8 lg:py-12"
+      maxWidth="2xl"
+    >
       <TitleAndMetaTags title="Sushiswap" />
 
       <Head>
         <title>Dashboard | Sushi</title>
         <meta name="description" content="Sushi" />
-        <meta key="twitter:description" name="twitter:description" content="Sushi" />
+        <meta
+          key="twitter:description"
+          name="twitter:description"
+          content="Sushi"
+        />
         <meta key="og:description" property="og:description" content="Sushi" />
       </Head>
     </Container>
@@ -21,3 +27,4 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
+/** @exports Dashboard */
