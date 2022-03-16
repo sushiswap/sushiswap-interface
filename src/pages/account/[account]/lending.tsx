5114340ecbe5a1322d44c72b2d2ca385bdaa36f6
matthewlilley <hello@matthewlilley.com>
2022-03-16 06:54:04 +0000

    fix: account routing

--- src/pages/account/[account]/lending.tsx
+++ src/pages/account/[account]/lending.tsx
@@ -1,30 +1,19 @@
-import { t } from '@lingui/macro'
-import { useLingui } from '@lingui/react'
 import { KashiLendingList } from 'app/features/kashi/KashiLendingList'
 import ActionsModal from 'app/features/portfolio/ActionsModal'
 import HeaderDropdown from 'app/features/portfolio/HeaderDropdown'
 import { useAccountInUrl } from 'app/features/portfolio/useAccountInUrl'
 import TridentLayout, { TridentBody, TridentHeader } from 'app/layouts/Trident'
-import Head from 'next/head'
+import { NextSeo } from 'next-seo'
 import React from 'react'
 
 const Lending = () => {
-  const { i18n } = useLingui()
-
   const account = useAccountInUrl('/')
 
-  if (!account) return
+  if (!account) return <></>
 
   return (
     <>
-      <Head>
-        <title>{i18n._(t`Portfolio`)} | Sushi</title>
-        <meta
-          key="description"
-          name="description"
-          content="Get a summary of all of the balances in your portfolio on Sushi."
-        />
-      </Head>
+      <NextSeo title={`Kashi lending positions for account ${account}`} />
       <TridentHeader pattern="bg-chevron">
         <HeaderDropdown account={account} />
       </TridentHeader>
