77d0adad4b0c18c23aa1eee9f4bc0d49795a0e74
matthewlilley <hello@matthewlilley.com>
2022-03-19 01:13:53 +0000

    feat: improve account balance loading state

--- src/components/Header/Desktop.tsx
+++ src/components/Header/Desktop.tsx
@@ -10,6 +10,7 @@ import { useETHBalances } from 'app/state/wallet/hooks'
 import Image from 'next/image'
 import React, { FC } from 'react'
 
+import Dots from '../Dots'
 import Typography from '../Typography'
 import { NavigationItem } from './NavigationItem'
 
@@ -38,15 +39,37 @@ const Desktop: FC = () => {
               <div className="flex items-center justify-end gap-2">
                 <div className="flex items-center w-auto shadow select-none whitespace-nowrap">
                   <Web3Status />
-                  <Typography weight={700} variant="sm" className="px-2 py-5 font-bold">
-                    {userEthBalance?.toSignificant(4)} {NATIVE[chainId || 1].symbol}
-                  </Typography>
+
+                  {account && chainId && (
+                    <Typography weight={700} variant="sm" className="px-2 py-5 font-bold">
+                      {userEthBalance ? (
+                        `${userEthBalance?.toSignificant(4)} ${NATIVE[chainId].symbol}`
+                      ) : (
+                        <Dots>Loading {NATIVE[chainId].symbol}</Dots>
+                      )}
+                    </Typography>
+                  )}
                 </div>
                 {library && (library.provider.isMetaMask || isCoinbaseWallet) && (
                   <div className="hidden sm:inline-block">
                     <Web3Network />
                   </div>
                 )}
+                {/* <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+                  />
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                </svg> */}
               </div>
             </div>
           </Container>
