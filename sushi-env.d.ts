925b3e25c21fe9305975204814cb12a6be2deb9b
sam bacha <sam@manifoldfinance.com>
2022-03-21 16:23:13 -0700

    fix(types): use sushi env

--- sushi-env.d.ts
+++ sushi-env.d.ts
@@ -1,8 +1,10 @@
+/// <reference types="gtag.js" />
+
+declare module 'gtag.js'
+
 import { BigNumber, BigNumberish } from '@ethersproject/bignumber'
 
 import Fraction from './src/entities/bignumber/Fraction'
-
-declare module 'fortmatic'
 
 declare global {
   interface String {
@@ -21,6 +23,51 @@ declare global {
     web3?: Record<string, unknown>
   }
 }
+
+
+
+// Used by Ethers.js
+interface CustomError extends Error {
+  reason?: string; 
+}
+
+// Polyfill for ResizeObserver since type is missing from TS
+// https://github.com/que-etc/resize-observer-polyfill/blob/master/src/index.d.ts
+interface DOMRectReadOnly {
+readonly x: number;
+readonly y: number;
+readonly width: number;
+readonly height: number;
+readonly top: number;
+readonly right: number;
+readonly bottom: number;
+readonly left: number;
+}
+
+type ResizeObserverCallback = (entries: ResizeObserverEntry[], observer: ResizeObserver) => void;
+
+interface ResizeObserverEntry {
+readonly target: Element;
+readonly contentRect: DOMRectReadOnly;
+}
+
+interface ResizeObserver {
+observe(target: Element): void;
+unobserve(target: Element): void;
+disconnect(): void;
+}
+
+declare const ResizeObserver: {
+prototype: ResizeObserver;
+new (callback: ResizeObserverCallback): ResizeObserver;
+};
+
+interface ResizeObserver {
+observe(target: Element): void;
+unobserve(target: Element): void;
+disconnect(): void;
+}
+
 
 declare module 'content-hash' {
   declare function decode(x: string): string
