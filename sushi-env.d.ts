9b764e0e532cdb6a9b7ab88a43166021103d04bf
sam bacha <sam@manifoldfinance.com>
2022-03-21 15:05:53 -0700

    fix(dts): sushi env

--- sushi-env.d.ts
+++ sushi-env.d.ts
@@ -4,6 +4,48 @@ import Fraction from './src/entities/bignumber/Fraction'
 
 declare module 'fortmatic'
 
+// Used by Ethers.js
+interface CustomError extends Error {
+    reason?: string; 
+  }
+
+// Polyfill for ResizeObserver since type is missing from TS
+// https://github.com/que-etc/resize-observer-polyfill/blob/master/src/index.d.ts
+interface DOMRectReadOnly {
+  readonly x: number;
+  readonly y: number;
+  readonly width: number;
+  readonly height: number;
+  readonly top: number;
+  readonly right: number;
+  readonly bottom: number;
+  readonly left: number;
+}
+
+type ResizeObserverCallback = (entries: ResizeObserverEntry[], observer: ResizeObserver) => void;
+
+interface ResizeObserverEntry {
+  readonly target: Element;
+  readonly contentRect: DOMRectReadOnly;
+}
+
+interface ResizeObserver {
+  observe(target: Element): void;
+  unobserve(target: Element): void;
+  disconnect(): void;
+}
+
+declare const ResizeObserver: {
+  prototype: ResizeObserver;
+  new (callback: ResizeObserverCallback): ResizeObserver;
+};
+
+interface ResizeObserver {
+  observe(target: Element): void;
+  unobserve(target: Element): void;
+  disconnect(): void;
+}
+
 declare global {
   interface String {
     toBigNumber(decimals: number): BigNumber
