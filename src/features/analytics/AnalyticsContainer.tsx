6c16953178360b5180d9136a41e44333e59a0b4b
matthewlilley <hello@matthewlilley.com>
2022-03-21 11:51:32 +0000

    chore: improve analytics layout

--- src/features/analytics/AnalyticsContainer.tsx
+++ src/features/analytics/AnalyticsContainer.tsx
@@ -4,7 +4,6 @@ import Head from 'next/head'
 import { useRouter } from 'next/router'
 
 import Container from '../../components/Container'
-import Sidebar from '../../components/Sidebar'
 
 // @ts-ignore TYPE NEEDS FIXING
 export default function AnalyticsContainer({ children }): JSX.Element {
@@ -45,22 +44,14 @@ export default function AnalyticsContainer({ children }): JSX.Element {
   }
 
   return (
-    <>
+    <div className="relative w-full">
       <Head>
         <title>Sushi Analytics | Sushi</title>
         <meta name="description" content="SushiSwap Liquidity Pair (SLP) Analytics by Sushi" />
       </Head>
-
-      <Container
-        id="analytics"
-        maxWidth="full"
-        className="grid h-full grid-flow-col grid-cols-10 mx-auto lg:px-4 gap-9"
-      >
-        <div className="sticky top-0 hidden lg:block md:col-span-2 3xl:col-start-1 3xl:col-span-2">
-          <Sidebar items={items} />
-        </div>
-        <div className="col-span-10 lg:border-l lg:col-span-8 3xl:col-span-7 border-dark-700">{children}</div>
+      <Container id="analytics" maxWidth="7xl" className="mx-auto">
+        <div className="w-full border-dark-700">{children}</div>
       </Container>
-    </>
+    </div>
   )
 }
