9a0e8f935ef6504c488b54118bc18e782818a636
sam bacha <sam@manifoldfinance.com>
2022-03-21 19:20:58 -0700

    build(deps): enforce resolutions

--- src/services/graph/queries/masterchef.ts
+++ src/services/graph/queries/masterchef.ts
@@ -37,9 +37,8 @@ export const masterChefV1PairAddressesQuery = gql`
     $first: Int! = 1000
     $skip: Int! = 0
     $orderBy: String! = "id"
-    $orderDirection: String! = "desc"
-  ) # $where: Pool_filter! = { allocPoint_gt: 0, accSushiPerShare_gt: 0 }
-  {
+    $orderDirection: String! = "desc" # $where: Pool_filter! = { allocPoint_gt: 0, accSushiPerShare_gt: 0 }
+  ) {
     pools(first: $first, skip: $skip, orderBy: $orderBy, orderDirection: $orderDirection, where: $where) {
       id
       allocPoint
--- sushi-env.d.ts
+++ sushi-env.d.ts
@@ -24,50 +24,47 @@ declare global {
   }
 }
 
-
-
 // Used by Ethers.js
 interface CustomError extends Error {
-  reason?: string; 
+  reason?: string
 }
 
 // Polyfill for ResizeObserver since type is missing from TS
 // https://github.com/que-etc/resize-observer-polyfill/blob/master/src/index.d.ts
 interface DOMRectReadOnly {
-readonly x: number;
-readonly y: number;
-readonly width: number;
-readonly height: number;
-readonly top: number;
-readonly right: number;
-readonly bottom: number;
-readonly left: number;
+  readonly x: number
+  readonly y: number
+  readonly width: number
+  readonly height: number
+  readonly top: number
+  readonly right: number
+  readonly bottom: number
+  readonly left: number
 }
 
-type ResizeObserverCallback = (entries: ResizeObserverEntry[], observer: ResizeObserver) => void;
+type ResizeObserverCallback = (entries: ResizeObserverEntry[], observer: ResizeObserver) => void
 
 interface ResizeObserverEntry {
-readonly target: Element;
-readonly contentRect: DOMRectReadOnly;
+  readonly target: Element
+  readonly contentRect: DOMRectReadOnly
 }
 
 interface ResizeObserver {
-observe(target: Element): void;
-unobserve(target: Element): void;
-disconnect(): void;
+  observe(target: Element): void
+  unobserve(target: Element): void
+  disconnect(): void
 }
 
 declare const ResizeObserver: {
-prototype: ResizeObserver;
-new (callback: ResizeObserverCallback): ResizeObserver;
-};
-
-interface ResizeObserver {
-observe(target: Element): void;
-unobserve(target: Element): void;
-disconnect(): void;
+  prototype: ResizeObserver
+  new (callback: ResizeObserverCallback): ResizeObserver
 }
 
+interface ResizeObserver {
+  observe(target: Element): void
+  unobserve(target: Element): void
+  disconnect(): void
+}
 
 declare module 'content-hash' {
   declare function decode(x: string): string
