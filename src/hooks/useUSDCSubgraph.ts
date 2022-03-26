5065c5845d2bb83ec811dfc93efdbba1d9fb278e
LufyCZ <lufy@sushi.com>
2022-03-26 17:07:53 +0000

    feat: multiple usdc prices

--- src/hooks/useUSDCSubgraph.ts
+++ src/hooks/useUSDCSubgraph.ts
@@ -4,6 +4,81 @@ import { useActiveWeb3React } from 'app/services/web3'
 import { useMemo } from 'react'
 
 /**
+ * Returns the prices in USDC of the input currency
+ * @param currencies currencies to compute the USDC price of
+ * @param useTrident for possible compatibility reasons
+ */
+export function useUSDCPricesSubgraph(
+  currencies?: Currency[],
+  useTrident = false
+): { [address: string]: Price<Currency, Token> } | undefined {
+  const { chainId } = useActiveWeb3React()
+
+  const stablecoin = chainId ? CurrencyAmount.fromRawAmount(USD[chainId], 0).currency : undefined
+
+  const ethPrice = useEthPrice()
+  const tokensLegacy = useTokens({
+    chainId,
+    variables: { where: { id_in: currencies?.map((currency) => currency.wrapped.address.toLowerCase()) } },
+    shouldFetch: currencies && currencies?.length > 0,
+  }) as any[] | undefined
+  const { data: tokensTrident } = useTridentTokens({
+    chainId,
+    variables: {
+      where: { id_in: currencies?.map((currency) => currency.wrapped.address.toLowerCase()) },
+    },
+    shouldFetch: currencies && currencies?.length > 0,
+  })
+
+  return useMemo(() => {
+    if (!currencies || currencies.length === 0 || !stablecoin || !(tokensLegacy || tokensTrident)) {
+      return undefined
+    }
+
+    const prices: { [address: string]: Price<Currency, Token> } = {}
+
+    currencies.map((currency) => {
+      let price: number | undefined = undefined
+
+      const tokenLegacy = tokensLegacy?.find((token) => token.id === currency.wrapped.address.toLowerCase())
+      const tokenTrident = tokensTrident?.find((token) => token.id === currency.wrapped.address.toLowerCase())
+
+      // handle usdc
+      if (currency?.wrapped.equals(stablecoin)) {
+        return new Price(stablecoin, stablecoin, '1', '1')
+      }
+
+      if (tokenLegacy && tokenTrident) {
+        if (tokenLegacy.liquidity > tokenTrident.kpi.liquidity) {
+          price = ethPrice * tokenLegacy.derivedETH
+        } else {
+          price = tokenTrident.price.derivedUSD
+        }
+      } else if (ethPrice && tokenLegacy) {
+        price = ethPrice * tokenLegacy.derivedETH
+      } else if (tokenTrident) {
+        price = tokenTrident.price.derivedUSD
+      }
+
+      if (price !== undefined && price !== 0) {
+        const base = 10 ** (String(price).split('.')?.[1]?.length ?? 0)
+        const quote = Math.floor(price * base)
+        try {
+          prices[currency.wrapped.address] = new Price(
+            currency,
+            stablecoin,
+            (base * 10 ** currency.decimals) / 10 ** stablecoin.decimals,
+            quote
+          )
+        } catch {}
+      }
+    })
+
+    return prices
+  }, [currencies, stablecoin, tokensLegacy, tokensTrident, ethPrice])
+}
+
+/**
  * Returns the price in USDC of the input currency
  * @param currency currency to compute the USDC price of
  * @param useTrident for possible compatibility reasons
@@ -12,63 +87,9 @@ export default function useUSDCPriceSubgraph(
   currency?: Currency,
   useTrident = false
 ): Price<Currency, Token> | undefined {
-  const { chainId } = useActiveWeb3React()
+  const priceObject = useUSDCPricesSubgraph(currency ? [currency] : undefined, useTrident)
 
-  const stablecoin = chainId ? CurrencyAmount.fromRawAmount(USD[chainId], 0).currency : undefined
-
-  const ethPrice = useEthPrice()
-  const tokenLegacy = useTokens({
-    chainId,
-    variables: { first: 1, where: { id_in: [currency?.wrapped.address.toLowerCase()] } },
-    shouldFetch: !!currency?.wrapped.address,
-  })?.[0]
-  const { data: tokensTrident } = useTridentTokens({
-    chainId,
-    variables: {
-      first: 1,
-      where: { id_in: [currency?.wrapped.address.toLowerCase()] },
-      shouldFetch: !!currency?.wrapped.address,
-    },
-  })
-
-  const tokenTrident = tokensTrident?.[0]
-
-  return useMemo(() => {
-    if (!currency || !stablecoin || !(tokenLegacy || tokenTrident)) {
-      return undefined
-    }
-
-    let price: number | undefined = undefined
-
-    // handle usdc
-    if (currency?.wrapped.equals(stablecoin)) {
-      return new Price(stablecoin, stablecoin, '1', '1')
-    }
-
-    if (tokenLegacy && tokenTrident) {
-      if (tokenLegacy.liquidity > tokenTrident.kpi.liquidity) {
-        price = ethPrice * tokenLegacy.derivedETH
-      } else {
-        price = tokenTrident.price.derivedUSD
-      }
-    } else if (ethPrice && tokenLegacy) {
-      price = ethPrice * tokenLegacy.derivedETH
-    } else if (tokenTrident) {
-      price = tokenTrident.price.derivedUSD
-    }
-
-    if (price !== undefined && price !== 0) {
-      const base = 10 ** (String(price).split('.')?.[1]?.length ?? 0)
-      const quote = Math.floor(price * base)
-      try {
-        return new Price(currency, stablecoin, (base * 10 ** currency.decimals) / 10 ** stablecoin.decimals, quote)
-      } catch (e) {
-        console.log(currency.wrapped.name, price, base, quote, e)
-      }
-    }
-
-    return undefined
-  }, [currency, stablecoin, tokenLegacy, tokenTrident, ethPrice])
+  return priceObject ? Object.values(priceObject)[0] : undefined
 }
 
 export function useUSDCValueSubgraph(
