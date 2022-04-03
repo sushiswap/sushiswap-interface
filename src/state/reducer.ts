75b29d72e525e15bd29072313670adaffd2f809a
matthewlilley <hello@matthewlilley.com>
2022-04-03 22:02:38 +0100

    chore: remove dead reducer

--- src/state/reducer.ts
+++ src/state/reducer.ts
@@ -12,7 +12,6 @@ import tridentSwap from '../features/trident/swap/swapSlice'
 import application from './application/reducer'
 import burn from './burn/reducer'
 import create from './create/reducer'
-import web3Context from './global/web3ContextSlice'
 import inari from './inari/reducer'
 import limitOrder from './limit-order/reducer'
 import lists from './lists/reducer'
@@ -46,7 +45,6 @@ const reducer = combineReducers({
   tridentPools,
   tridentCreate,
   tridentMigrations,
-  web3Context,
 })
 
 export default reducer
