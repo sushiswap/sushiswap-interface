f6b1f4d7f67987d37d697b9c2693c48f2388a2f2
matthewlilley <hello@matthewlilley.com>
2022-04-05 06:23:50 +0100

    Revert "feat: chains"
    This reverts commit 3098b2973acc97b0b961d2ca314cb784e80684aa.

--- src/components/Web3Network/index.tsx
+++ src/components/Web3Network/index.tsx
@@ -1,100 +1,14 @@
 import { NETWORK_ICON } from 'app/config/networks'
-import { logEvent } from 'app/functions/analytics'
-import usePrevious from 'app/hooks/usePrevious'
-import NetworkModel, { SUPPORTED_NETWORKS } from 'app/modals/NetworkModal'
+import NetworkModel from 'app/modals/NetworkModal'
 import { useActiveWeb3React } from 'app/services/web3'
 import { useNetworkModalToggle } from 'app/state/application/hooks'
 import Image from 'next/image'
-import { useRouter } from 'next/router'
-import React, { useCallback, useEffect } from 'react'
-
-const switchToNetwork = async ({ provider, chainId }: any) => {
-  if (!provider.request) {
-    return
-  }
-  console.debug(`Switching to chain ${chainId}`)
-  const params = SUPPORTED_NETWORKS[chainId]
-  try {
-    await provider.request({
-      method: 'wallet_switchEthereumChain',
-      params: [{ chainId: `0x${chainId.toString(16)}` }],
-    })
-    logEvent('Chain', 'switch', params.chainName, chainId)
-  } catch (error) {
-    // @ts-ignore TYPE NEEDS FIXING
-    // This error code indicates that the chain has not been added to MetaMask.
-    if (error.code === 4902) {
-      await provider.request({
-        method: 'wallet_addEthereumChain',
-        params,
-      })
-      // metamask (only known implementer) automatically switches after a network is added
-      // the second call is done here because that behavior is not a part of the spec and cannot be relied upon in the future
-      // metamask's behavior when switching to the current network is just to return null (a no-op)
-      try {
-        await provider.request({
-          method: 'wallet_switchEthereumChain',
-          params: [{ chainId: `0x${chainId.toString(16)}` }],
-        })
-      } catch (error) {
-        console.debug('Added network but could not switch chains', error)
-      }
-    } else {
-      console.debug('Switch chain error', error)
-      throw error
-    }
-  }
-}
+import React from 'react'
 
 function Web3Network(): JSX.Element | null {
-  const { chainId, library } = useActiveWeb3React()
+  const { chainId } = useActiveWeb3React()
 
-  const router = useRouter()
   const toggleNetworkModal = useNetworkModalToggle()
-
-  const prevChainId = usePrevious(chainId)
-
-  const urlChainId = router.query.chainId
-
-  const handleChainSwitch = useCallback(
-    (targetChain: number) => {
-      if (!library?.provider) return
-      switchToNetwork({ provider: library.provider, chainId: targetChain })
-        .then(() => router.replace({ query: { ...router.query, chainId: targetChain } }))
-        .catch(() => {
-          if (chainId) {
-            router.replace({ query: { ...router.query, chainId } })
-          }
-        })
-    },
-    [library?.provider, router, chainId]
-  )
-
-  useEffect(() => {
-    if (!chainId || !prevChainId || !urlChainId) return
-
-    // when network change originates from wallet or dropdown selector, just update URL
-    if (chainId !== prevChainId) {
-      console.debug('network change from wallet or network modal', { urlChainId, chainId })
-
-      router.replace({ query: { ...router.query, chainId } })
-
-      console.debug('after network change from wallet or network modal', { urlChainId: router.query.chainId, chainId })
-
-      // otherwise assume network change originates from URL
-    } else if (urlChainId && chainId && Number(urlChainId) !== chainId) {
-      console.debug('network change from URL', { urlChainId, chainId })
-      handleChainSwitch(Number(urlChainId))
-    }
-  }, [chainId, urlChainId, prevChainId, router, handleChainSwitch])
-
-  // set chainId on initial load if not present
-  useEffect(() => {
-    if (chainId && !urlChainId) {
-      console.debug('Setting chain id on initial load because not present')
-      router.replace({ query: { ...router.query, chainId } })
-    }
-  }, [chainId, urlChainId, router])
 
   if (!chainId) return null
 
--- src/components/Web3ReactManager/index.tsx
+++ src/components/Web3ReactManager/index.tsx
@@ -9,7 +9,9 @@ import useEagerConnect from 'app/hooks/useEagerConnect'
 import useInactiveListener from 'app/hooks/useInactiveListener'
 import dynamic from 'next/dynamic'
 import { useRouter } from 'next/router'
-import React, { FC, useEffect } from 'react'
+import React, { FC, useEffect, useState } from 'react'
+
+import Loader from '../Loader'
 
 const GnosisManagerNoSSR = dynamic(() => import('./GnosisManager'), {
   ssr: false,
@@ -25,15 +27,7 @@ export const Web3ReactManager: FC = ({ children }) => {
 
   const router = useRouter()
 
-  // after eagerly trying injected, if the network connect ever isn't active or in an error state, activate itd
-  useEffect(() => {
-    if (triedEager && !networkActive && !networkError && !active) {
-      activateNetwork(network)
-    }
-  }, [triedEager, networkActive, networkError, activateNetwork, active])
-
-  // when there's no account connected, react to logins (broadly speaking) on the injected provider, if it exists
-  useInactiveListener(!triedEager)
+  console.log(router)
 
   useEffect(() => {
     if (chainId && featureEnabled(Feature.TRIDENT, chainId) && router.asPath === '/swap') {
@@ -41,8 +35,42 @@ export const Web3ReactManager: FC = ({ children }) => {
     }
   }, [chainId, router])
 
+  // useNetworkOrchistrator()
+
+  // after eagerly trying injected, if the network connect ever isn't active or in an error state, activate itd
+  useEffect(() => {
+    const activate = async () => {
+      if (triedEager && !networkActive && !networkError && !active) {
+        // const Cookies = (await import('js-cookie')).default
+        // network.changeChainId(Number(Cookies.get('chain-id')))
+        activateNetwork(network)
+      }
+    }
+    activate()
+  }, [triedEager, networkActive, networkError, activateNetwork, active])
+
+  // when there's no account connected, react to logins (broadly speaking) on the injected provider, if it exists
+  useInactiveListener(!triedEager)
+
+  // handle delayed loader state
+  const [showLoader, setShowLoader] = useState(false)
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShowLoader(true)
+    }, 600)
+
+    return () => {
+      clearTimeout(timeout)
+    }
+  }, [])
+
+  // on page load, do nothing until we've tried to connect to the injected connector
+  if (!triedEager) {
+    return null
+  }
+
   // if the account context isn't active, and there's an error on the network context, it's an irrecoverable error
-  if (triedEager && !active && networkError) {
+  if (!active && networkError) {
     return (
       <div className="flex items-center justify-center h-80">
         <div className="text-secondary">
@@ -50,6 +78,15 @@ export const Web3ReactManager: FC = ({ children }) => {
         </div>
       </div>
     )
+  }
+
+  // if neither context is active, spin
+  if (!active && !networkActive) {
+    return showLoader ? (
+      <div className="flex items-center justify-center h-80">
+        <Loader />
+      </div>
+    ) : null
   }
 
   return (
