85c7ad0948a59a1575d3262d62e6ad1a878bef01
matthewlilley <hello@matthewlilley.com>
2022-04-09 20:44:41 +0100

    fix: search result render

--- src/features/trident/pools/SearchCategoryLabel.tsx
+++ src/features/trident/pools/SearchCategoryLabel.tsx
@@ -10,10 +10,11 @@ export const SearchCategoryLabel: FC = () => {
   const { i18n } = useLingui()
   const { searchQuery } = useAppSelector(selectTridentPools)
 
+  console.log({ searchQuery })
   return (
     <div className="flex flex-row items-center justify-between px-2 py-2">
       <Typography variant="base" className="text-high-emphesis" weight={700}>
-        {searchQuery ? i18n._(t`Search results for '${searchQuery}'`) : i18n._(t`Top Liquidity Pools`)}
+        {searchQuery ? `${i18n._(t`Search results for`)} '${searchQuery}'` : i18n._(t`Top Liquidity Pools`)}
       </Typography>
       <div className="flex gap-1">
         <Image src={'https://app.sushi.com/images/rss.svg'} width={16} height={16} alt="rss icon" />
