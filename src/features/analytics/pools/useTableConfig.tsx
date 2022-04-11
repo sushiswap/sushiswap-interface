2aba6620b39264412a39d600d4e0a13663d189f1
matthewlilley <hello@matthewlilley.com>
2022-04-11 07:11:53 +0100

    feat: trending apy and prep for other kpis

--- src/features/analytics/pools/useTableConfig.tsx
+++ src/features/analytics/pools/useTableConfig.tsx
@@ -2,7 +2,7 @@ import { getAddress } from '@ethersproject/address'
 import { Token } from '@sushiswap/core-sdk'
 import { CurrencyLogoArray } from 'app/components/CurrencyLogo'
 import { formatNumber, getApy } from 'app/functions'
-import { useOneDayBlock, useOneWeekBlock, useSushiPairs } from 'app/services/graph'
+import { useOneDayBlock, useOneWeekBlock, useSushiPairs, useTwoDayBlock } from 'app/services/graph'
 import Image from 'next/image'
 import React, { useMemo } from 'react'
 
@@ -10,7 +10,7 @@ import { filterForSearchQuery } from './poolTableFilters'
 
 export const useTableConfig = (chainId: number) => {
   const block1d = useOneDayBlock({ chainId, shouldFetch: !!chainId })
-  const block2d = useOneDayBlock({ chainId, shouldFetch: !!chainId })
+  const block2d = useTwoDayBlock({ chainId, shouldFetch: !!chainId })
   const block1w = useOneWeekBlock({ chainId, shouldFetch: !!chainId })
 
   const pairs = useSushiPairs({ chainId })
@@ -26,28 +26,35 @@ export const useTableConfig = (chainId: number) => {
         ?.map((pair) => {
           // @ts-ignore TYPE NEEDS FIXING
           const pair1d = pairs1d?.find((p) => pair.id === p.id) ?? pair
-          const pair2d = pairs1d?.find((p) => pair.id === p.id) ?? pair
+          const pair2d = pairs2d?.find((p) => pair.id === p.id) ?? pair
           // @ts-ignore TYPE NEEDS FIXING
           const pair1w = pairs1w?.find((p) => pair.id === p.id) ?? pair1d
 
           const volume1d = pair.volumeUSD - pair1d.volumeUSD
-          const volume2d = pair.volumeUSD - pair2d.volumeUSD
           const volume1w = pair.volumeUSD - pair1w.volumeUSD
-          const volume1dChange = (volume1d / volume2d) * 100 - 100
+
+          const volume1dChange = pair.volumeUSD - pair1d.volumeUSD - (pair1d.volumeUSD - pair2d.volumeUSD)
+          const volume1dChangePercent =
+            ((pair.volumeUSD - pair1d.volumeUSD) / (pair1d.volumeUSD - pair2d.volumeUSD)) * 100 - 100
 
           const fees1d = volume1d * 0.003
           const fees1w = volume1w * 0.003
 
           const liquidity = pair.reserveUSD
-          const liquidity1dChange = pair.reserveUSD / pair1d.reserveUSD
+          const liquidity1d = pair1d.reserveUSD
 
-          const utilisation1d = (volume1d / pair?.reserveUSD) * 100
-          const utilisation2d = (volume2d / pair1d?.reserveUSD) * 100
+          const liquidity1dChange = pair.reserveUSD - pair1d.reserveUSD
+          const liquidity1dChangePercent = (pair.reserveUSD / pair1d.reserveUSD) * 100 - 100
+
+          const utilisation1d = ((pair.volumeUSD - pair1d.volumeUSD) / pair?.reserveUSD) * 100
+          const utilisation2d = ((pair1d.volumeUSD - pair2d.volumeUSD) / pair1d?.reserveUSD) * 100
           const utilisation1dChange = (utilisation1d / utilisation2d) * 100 - 100
 
           const tx1d = pair.txCount - pair1d.txCount
           const tx2d = pair1d.txCount - pair2d.txCount
           const tx1dChange = (tx1d / tx2d) * 100 - 100
+
+          const apy = getApy(volume1d, liquidity)
 
           return {
             pair: {
@@ -56,9 +63,12 @@ export const useTableConfig = (chainId: number) => {
               id: pair.id,
             },
             liquidity,
+            liquidity1d,
             liquidity1dChange,
+            liquidity1dChangePercent,
             volume1d,
             volume1dChange,
+            volume1dChangePercent,
             volume1w,
             fees1d,
             fees1w,
@@ -68,13 +78,13 @@ export const useTableConfig = (chainId: number) => {
             tx1d,
             tx2d,
             tx1dChange,
-            apy: getApy(volume1w, pair.reserveUSD),
+            apy,
           }
         })
         // @ts-ignore TYPE NEEDS FIXING
         .sort((a, b) => b.liquidityChangeNumber1d - a.liquidityChangeNumber1d)
     )
-  }, [pairs, pairs1d, pairs1w])
+  }, [pairs, pairs1d, pairs1w, pairs2d])
 
   const columns = useMemo(
     () => [
@@ -157,6 +167,42 @@ export const useTableConfig = (chainId: number) => {
         Header: 'APY',
         accessor: 'apy',
         minWidth: 150,
+        Cell: (props) => {
+          console.log({ props })
+          return (
+            <>
+              {props.value}{' '}
+              {props.value !== '-' &&
+                (props.row.original.utilisation1dChange > 0 ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-5 h-5 ml-2"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M12 7a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0V8.414l-4.293 4.293a1 1 0 01-1.414 0L8 10.414l-4.293 4.293a1 1 0 01-1.414-1.414l5-5a1 1 0 011.414 0L11 10.586 14.586 7H12z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-5 h-5 ml-2"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M12 13a1 1 0 100 2h5a1 1 0 001-1V9a1 1 0 10-2 0v2.586l-4.293-4.293a1 1 0 00-1.414 0L8 9.586 3.707 5.293a1 1 0 00-1.414 1.414l5 5a1 1 0 001.414 0L11 9.414 14.586 13H12z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                ))}
+            </>
+          )
+        },
       },
     ],
     [chainId]
