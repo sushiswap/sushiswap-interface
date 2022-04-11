7097c2c148e2af405f2eb5a9d44399214cd127b0
matthewlilley <hello@matthewlilley.com>
2022-04-11 07:11:27 +0100

    fix: select for analytics pools

--- src/features/analytics/pools/SearchCategoryLabel.tsx
+++ src/features/analytics/pools/SearchCategoryLabel.tsx
@@ -2,12 +2,12 @@ import { t } from '@lingui/macro'
 import { useLingui } from '@lingui/react'
 import Typography from 'app/components/Typography'
 import { useAppSelector } from 'app/state/hooks'
-import { selectTokens } from 'app/state/tokens'
+import { selectPools } from 'app/state/pools'
 import React, { FC } from 'react'
 
 export const SearchCategoryLabel: FC = () => {
   const { i18n } = useLingui()
-  const { searchQuery } = useAppSelector(selectTokens)
+  const { searchQuery } = useAppSelector(selectPools)
   return (
     <div className="flex flex-row items-center justify-between px-2 py-2">
       <Typography variant="base" className="text-high-emphesis" weight={700}>
