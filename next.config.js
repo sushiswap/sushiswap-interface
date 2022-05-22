27caf5b04823eb364a0747a986943a53945909d9
sam bacha <sam@manifoldfinance.com>
2022-05-22 12:16:19 -0700

    build(nextjs): log output

--- next.config.js
+++ next.config.js
@@ -145,4 +145,4 @@ const nextConfig = {
 module.exports = withSentryConfig(withBundleAnalyzer(nextConfig), sentryWebpackPluginOptions);
 
 // Don't delete this console log, useful to see the config in Vercel deployments
-// console.log('next.config.js', JSON.stringify(module.exports, null, 2))
+console.log('next.config.js', JSON.stringify(module.exports, null, 2))
