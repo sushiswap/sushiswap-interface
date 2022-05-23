611dfff934d5b6ce6885a14fe2bcf245f90cf6e1
Lint Action <lint-action@samuelmeuli.com>
2022-05-23 06:46:04 +0000

    style: code style issues with ESLint

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,12 @@
 /* eslint-disable @next/next/no-document-import-in-page */
 // pages/_document.js
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Head, Html, Main, NextScript } from 'next/document'
 
 const APP_NAME = 'Sushi'
 const APP_DESCRIPTION = 'Swap, yield, lend, borrow, leverage, limit, launch all on one community driven ecosystem'
 
-/** 
- * @export Document 
+/**
+ * @export Document
  * @summary `<Html>`, `<Head>`, `<Main>`, and `<NextScript>` are all required to be
  * used. You cannot use the normal `<html>`, `<head>`, et al.
  * Next.js injects additional attributes and/or content to make your
@@ -15,23 +15,18 @@ const APP_DESCRIPTION = 'Swap, yield, lend, borrow, leverage, limit, launch all 
  */
 
 export default function Document() {
-    return (
-      <Html lang="en">
+  return (
+    <Html lang="en">
       <Head>
-        <meta 
-            name="application-name" content={APP_NAME} 
-        />
-        <meta
-            name="ui-version"
-            content={`${process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA}`}
-        />
+        <meta name="application-name" content={APP_NAME} />
+        <meta name="ui-version" content={`${process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA}`} />
         <meta property="og:type" content="website" key="og:type" />
 
         <meta property="og:title" content="Sushi" key="og:title" />
         <meta
-        property="og:description"
-        content="Swap, yield, lend, borrow, leverage, limit, launch and more all on Sushiswap"
-        key="og:description"
+          property="og:description"
+          content="Swap, yield, lend, borrow, leverage, limit, launch and more all on Sushiswap"
+          key="og:description"
         />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
@@ -40,8 +35,6 @@ export default function Document() {
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="theme-color" content="#0993ec" />
-
-
 
         <link rel="apple-touch-icon" sizes="72x72" href="/icons/icon-72x72.png" />
         <link rel="apple-touch-icon" sizes="96x96" href="/icons/icon-96x96.png" />
@@ -60,5 +53,5 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
