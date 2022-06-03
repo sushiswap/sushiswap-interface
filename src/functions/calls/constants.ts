58666c9d7cc9de6fcbbb16960c7f7892386838b1
sam bacha <sam@manifoldfinance.com>
2022-06-03 04:54:53 -0700

    fix(constants): adjust min. call data
    add notes about post merge constants

--- src/functions/calls/constants.ts
+++ src/functions/calls/constants.ts
@@ -1,16 +1,27 @@
+
 import type { CallResult, CallState, ListenerOptions } from './types'
 
+/** @note EIP4488 Pisa post-merge */
+export const FUTURE_EIP4488 = 1_048_576
 export const DEFAULT_CALL_GAS_REQUIRED = 1_000_000
 export const DEFAULT_CHUNK_GAS_REQUIRED = 200_000
 export const CHUNK_GAS_LIMIT = 100_000_000
-export const CONSERVATIVE_BLOCK_GAS_LIMIT = 10_000_000 // conservative, hard-coded estimate of the current block gas limit
-
-// Const for hooks
+export const CONSERVATIVE_BLOCK_GAS_LIMIT = 10_000_000
+/** @note Minimum the gas limit may ever be, in Golang uint64 */
+export const MIN_GAS_LIMIT = 5000 
+/** @note Maximum the gas limit (2^63-1), in Golang uint64 */
+export const MAX_GAS_LIMIT = 0x7fffffffffffffff 
+/** 
+ * @summary Const for hooks 
+ * @export INVALID_RESULT
+ * @export NEVER_RELOAD
+ */
 export const INVALID_RESULT: CallResult = { valid: false, blockNumber: undefined, data: undefined }
 export const NEVER_RELOAD: ListenerOptions = {
   blocksPerFetch: Infinity,
 }
 
+/** @export INVALID_CALL_STATE */
 export const INVALID_CALL_STATE: CallState = {
   valid: false,
   result: undefined,
@@ -18,6 +29,8 @@ export const INVALID_CALL_STATE: CallState = {
   syncing: false,
   error: false,
 }
+
+/** @export LOADING_CALL_STATE */
 export const LOADING_CALL_STATE: CallState = {
   valid: true,
   result: undefined,
