d67a0e2550f45cb6fd0a2ea96eed58dfc9a8bef9
Lint Action <lint-action@samuelmeuli.com>
2022-06-04 13:53:16 +0000

    style: code style issues with ESLint

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -1,5 +1,5 @@
-import NextErrorComponent from 'next/error'
 import * as Sentry from '@sentry/nextjs'
+import NextErrorComponent from 'next/error'
 // @ts-ignore
 const MyError = ({ statusCode, hasGetInitialPropsRun, err }) => {
   if (!hasGetInitialPropsRun && err) {
