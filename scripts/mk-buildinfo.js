9256ddd5ae1bdeee7ff3e097dc651da1912d8c91
sam bacha <sam@manifoldfinance.com>
2022-06-07 01:48:26 -0700

    chore(cleanup): prep for merge

---
+++ scripts/mk-buildinfo.js
@@ -0,0 +1,36 @@
+const path = require('path')
+const fs = require('fs')
+const fetch = require('node-fetch')
+
+// @note this should be `NEXT_PUBLIC_COMMIT_SHA` for nextjs
+const commit = process.env.COMMIT_SHA
+
+async function getCommit() {
+  if (!commit) return `No COMMIT_SHA environment variable set.`
+  try {
+    const res = await fetch(`https://api.github.com/repos/${github_org}/${github_repo}/commits/${commit}`)
+    const data = await res.json()
+    return {
+      isDeployCommit: commit === 'HEAD' ? 'Unknown' : true,
+      sha: data.sha,
+      author: data.commit.author.name,
+      date: data.commit.author.date,
+      message: data.commit.message,
+      link: data.html_url,
+    }
+  } catch (error) {
+    return `Unable to get git commit info: ${error.message}`
+  }
+}
+
+async function go() {
+  const buildInfo = {
+    buildTime: Date.now(),
+    commit: await getCommit(),
+  }
+
+  // @note this is different for nextjs, should be in dir: `.next/~`
+  fs.writeFileSync(path.join(__dirname, '../public/build/info.json'), JSON.stringify(buildInfo, null, 2))
+  console.log('build info generated', buildInfo)
+}
+go()
