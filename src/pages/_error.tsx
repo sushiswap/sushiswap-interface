ebef22ab61393d0574f7346c032353f6ce3f2753
matthewlilley <hello@matthewlilley.com>
2022-06-08 15:48:39 +0100

    fix: compilation

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -1,7 +1,7 @@
+import * as Sentry from '@sentry/nextjs'
 import NextErrorComponent from 'next/error'
 
-import * as Sentry from '@sentry/nextjs'
-
+// @ts-ignore
 const MyError = ({ statusCode, hasGetInitialPropsRun, err }) => {
   if (!hasGetInitialPropsRun && err) {
     // getInitialProps is not called in case of
@@ -14,7 +14,9 @@ const MyError = ({ statusCode, hasGetInitialPropsRun, err }) => {
   return <NextErrorComponent statusCode={statusCode} />
 }
 
+// @ts-ignore
 MyError.getInitialProps = async ({ res, err }) => {
+  // @ts-ignore
   const errorInitialProps = await NextErrorComponent.getInitialProps({
     res,
     err,
@@ -22,6 +24,7 @@ MyError.getInitialProps = async ({ res, err }) => {
 
   // Workaround for https://github.com/vercel/next.js/issues/8592, mark when
   // getInitialProps has run
+  // @ts-ignore
   errorInitialProps.hasGetInitialPropsRun = true
 
   // Running on the server, the response object (`res`) is available.
