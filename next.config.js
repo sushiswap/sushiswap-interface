95d1d5b23656b5bb71316aad52237c97f451dde9
Matthew Lilley <hello@matthewlilley.com>
2024-04-10 06:41:59 +0100

    chore: wildcard analytics path

--- next.config.js
+++ next.config.js
@@ -108,7 +108,7 @@ const nextConfig = {
         permanent: true,
       },
       {
-        source: '/analytics/:path*',
+        source: '/analytics/*',
         destination: 'https://www.sushi.com/analytics',
         permanent: true,
       }
